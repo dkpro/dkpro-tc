9a5d1cf5cea1e0d0e80c3c44c1aa1d997d792ee3
oliver.ferschke@gmail.com <oliver.ferschke@gmail.com@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2013-05-16 14:09:26 +0000

    Moved reader, reader tests and reader test resources from io to experiment module.

---
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.experiments.twentynewsgroups/src/test/java/de/tudarmstadt/ukp/dkpro/tc/experiments/twentynewsgroups/io/TwentyNewsgroupCorpusReaderTest.java
@@ -0,0 +1,48 @@
+package de.tudarmstadt.ukp.dkpro.tc.experiments.twentynewsgroups.io;
+
+import static org.junit.Assert.assertEquals;
+import static org.uimafit.factory.CollectionReaderFactory.createCollectionReader;
+
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+import org.uimafit.pipeline.JCasIterable;
+import org.uimafit.util.JCasUtil;
+
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.dkpro.tc.type.TextClassificationOutcome;
+
+public class TwentyNewsgroupCorpusReaderTest
+{
+
+    @Test
+    public void testTwentyNewsgroupsCorpusReader()
+        throws Exception
+    {
+        CollectionReader reader = createCollectionReader(
+                TwentyNewsgroupCorpusReader.class,
+                TwentyNewsgroupCorpusReader.PARAM_PATH, "classpath:/data/",
+                TwentyNewsgroupCorpusReader.PARAM_PATTERNS, new String[] {
+                    TwentyNewsgroupCorpusReader.INCLUDE_PREFIX + "*/*.txt" });
+
+        int i=0;
+        for (JCas jcas : new JCasIterable(reader)) {
+            DocumentMetaData md = DocumentMetaData.get(jcas);
+            dumpMetaData(md);
+            i++;
+
+            for (TextClassificationOutcome outcome : JCasUtil.select(jcas, TextClassificationOutcome.class)) {
+                System.out.println(outcome);
+            }
+        }
+        assertEquals(12, i);
+    }
+
+    private void dumpMetaData(final DocumentMetaData aMetaData)
+    {
+        System.out.println("Collection ID: "+aMetaData.getCollectionId());
+        System.out.println("ID           : "+aMetaData.getDocumentId());
+        System.out.println("Base URI     : "+aMetaData.getDocumentBaseUri());
+        System.out.println("URI          : "+aMetaData.getDocumentUri());
+    }
+}
