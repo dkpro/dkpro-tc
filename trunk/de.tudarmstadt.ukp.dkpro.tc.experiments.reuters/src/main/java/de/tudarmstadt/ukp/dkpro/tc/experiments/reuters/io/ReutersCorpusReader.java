afc80938c4aa0dd6c95c92fe89687c6a8fb9af3d
oliver.ferschke@gmail.com <oliver.ferschke@gmail.com@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2013-05-17 14:02:38 +0000

    Loading json config file directly via new File() and not via Classloader - this caused problems on jenkins because of whitespaces. Convertin URL to URI instead of URL.getFile() to avoid the same problem mentioned above.

--- trunk/de.tudarmstadt.ukp.dkpro.tc.experiments.reuters/src/main/java/de/tudarmstadt/ukp/dkpro/tc/experiments/reuters/io/ReutersCorpusReader.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.experiments.reuters/src/main/java/de/tudarmstadt/ukp/dkpro/tc/experiments/reuters/io/ReutersCorpusReader.java
@@ -2,6 +2,7 @@ package de.tudarmstadt.ukp.dkpro.tc.experiments.reuters.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,14 +27,14 @@ import de.tudarmstadt.ukp.dkpro.tc.type.TextClassificationOutcome;
 public class ReutersCorpusReader
     extends TextReader
 {
-    
+
     /**
-     * Path to the file containing the gold standard labels. 
+     * Path to the file containing the gold standard labels.
      */
     public static final String PARAM_GOLD_LABEL_FILE = "GoldLabelFile";
     @ConfigurationParameter(name = PARAM_GOLD_LABEL_FILE, mandatory = true)
     private String goldLabelFile;
-    
+
     private Map<String, List<String>> goldLabelMap;
 
     @Override
@@ -41,30 +42,33 @@ public class ReutersCorpusReader
         throws ResourceInitializationException
     {
         super.initialize(context);
-        
+
         goldLabelMap = new HashMap<String, List<String>>();
-        
+
         try {
             URL resourceUrl = ResourceUtils.resolveLocation(goldLabelFile, this, context);
-            
-            for (String line : FileUtils.readLines(new File(resourceUrl.getFile()))) {
+
+            for (String line : FileUtils.readLines(new File(resourceUrl.toURI()))) {
                 String[] parts = line.split(" ");
-                
+
                 if (parts.length < 2) {
                     throw new IOException("Wrong file format in line: " + line);
                 }
                 String fileId = parts[0].split("/")[1];
-                
+
                 List<String> labels = new ArrayList<String>();
                 for (int i=1; i<parts.length; i++) {
                     labels.add(parts[i]);
                 }
-                
+
                 goldLabelMap.put(fileId, labels);
             }
         }
         catch (IOException e) {
             throw new ResourceInitializationException(e);
+        }
+        catch (URISyntaxException ex) {
+            throw new ResourceInitializationException(ex);
         }
     }
 
@@ -73,7 +77,7 @@ public class ReutersCorpusReader
         throws IOException, CollectionException
     {
         super.getNext(aCAS);
-        
+
         JCas jcas;
         try {
             jcas = aCAS.getJCas();
@@ -88,7 +92,7 @@ public class ReutersCorpusReader
         if (!goldLabelMap.containsKey(titleWithoutExtension)) {
             throw new IOException("No gold label for document: " + dmd.getDocumentTitle());
         }
-        
+
         for (String label : goldLabelMap.get(titleWithoutExtension)) {
             TextClassificationOutcome outcome = new TextClassificationOutcome(jcas);
             outcome.setOutcome(label);
