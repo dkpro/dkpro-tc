e810dfd651cbc1e0ab03136bad0f82eb873c1a59
daxenberger@ukp.informatik.tu-darmstadt.de <daxenberger@ukp.informatik.tu-darmstadt.de@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2013-05-24 07:23:57 +0000

    Added necessary imports.

--- trunk/de.tudarmstadt.ukp.dkpro.tc.experiments.twentynewsgroups/src/main/groovy/de/tudarmstadt/ukp/dkpro/tc/experiments/twentynewsgroups/TwentyNewsgroupsGroovyExtendedExperiment.groovy
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.experiments.twentynewsgroups/src/main/groovy/de/tudarmstadt/ukp/dkpro/tc/experiments/twentynewsgroups/TwentyNewsgroupsGroovyExtendedExperiment.groovy
@@ -1,12 +1,19 @@
 package de.tudarmstadt.ukp.dkpro.tc.experiments.twentynewsgroups;
 
+import static org.uimafit.factory.AnalysisEngineFactory.createAggregateDescription
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription
+import static org.uimafit.factory.CollectionReaderFactory.createDescription
+
 import org.apache.uima.analysis_engine.AnalysisEngineDescription
 import org.apache.uima.collection.CollectionReaderDescription
 import org.apache.uima.resource.ResourceInitializationException
 
+import weka.classifiers.bayes.NaiveBayes
 import weka.classifiers.functions.SMO
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter
 import de.tudarmstadt.ukp.dkpro.lab.Lab
+import de.tudarmstadt.ukp.dkpro.lab.task.Dimension
 import de.tudarmstadt.ukp.dkpro.lab.task.impl.BatchTask
 import de.tudarmstadt.ukp.dkpro.lab.task.impl.BatchTask.ExecutionPolicy
 import de.tudarmstadt.ukp.dkpro.tc.core.extractor.SingleLabelInstanceExtractor
@@ -27,55 +34,57 @@ import de.tudarmstadt.ukp.dkpro.tc.weka.task.TestTask
 
 /**
  * Groovy-Version of the TwentyNewsgroupsExperiment
- * 
- * The TwentyNewsgroupsGroovyExtendedExperiment does the same as TwentyNewsgroupsGroovyExperiment, 
+ *
+ * The TwentyNewsgroupsGroovyExtendedExperiment does the same as TwentyNewsgroupsGroovyExperiment,
  * but it manually sets up the sub-tasks and builds a generic batch task.
- * 
- * In TwentyNewsgroupsGroovyExperiment, this is done automatically in the BatchTaskCV and BatchTaskTrainTest, 
+ *
+ * In TwentyNewsgroupsGroovyExperiment, this is done automatically in the BatchTaskCV and BatchTaskTrainTest,
  * which is more convenient, but less flexible.
- * 
+ *
  * @author Oliver Ferschke
  */
 public class TwentyNewsgroupsGroovyExtendedExperiment {
 
-	// === PARAMETERS===========================================================
+    // === PARAMETERS===========================================================
 
-	def corpusFilePathTrain = "src/main/resources/data/bydate-train";
-	def corpusFilePathTest  ="src/main/resources/data/bydate-test";
+    def corpusFilePathTrain = "src/main/resources/data/bydate-train";
+    def corpusFilePathTest  ="src/main/resources/data/bydate-test";
     def languageCode = "en";
 
-	// === DIMENSIONS===========================================================
+    // === DIMENSIONS===========================================================
 
     def dimFolds = Dimension.create("folds" , 2);
     def dimTopNgramsK = Dimension.create("topNgramsK" , [500, 1000] as int[] );
     def dimToLowerCase = Dimension.create("toLowerCase", true);
     def dimMultiLabel = Dimension.create("multiLabel", false);
 
-	//UIMA parameters for FE configuration
-	def dimPipelineParameters = Dimension.create(
-        "pipelineParameters",
+    //UIMA parameters for FE configuration
+    def dimPipelineParameters = Dimension.create(
+    "pipelineParameters",
+    [
+        NGramFeatureExtractor.PARAM_NGRAM_MIN_N,
+        1,
+        NGramFeatureExtractor.PARAM_NGRAM_MAX_N,
+        3
+    ]);
+
+
+    def dimClassificationArgs =
+    Dimension.create("classificationArguments",
+    [
+        [NaiveBayes.class.name].toArray(),
+        [SMO.class.name].toArray()
+    ] as Object[]
+    );
+
+    def dimFeatureSets = Dimension.create(
+    "featureSet",
+    [
         [
-            NGramFeatureExtractor.PARAM_NGRAM_MIN_N, 1,
-            NGramFeatureExtractor.PARAM_NGRAM_MAX_N, 3
-        ]);
-
-
-	def dimClassificationArgs =
-        Dimension.create("classificationArguments",
-            [
-                [NaiveBayes.class.name].toArray(),
-                [SMO.class.name].toArray()
-            ] as Object[]
-     );
-
-	def dimFeatureSets = Dimension.create(
-        "featureSet",
-        [
-            [
-                NrOfTokensFeatureExtractor.class.name,
-                NGramFeatureExtractor.class.name
-            ].toArray()
-        ] as Object[]
+            NrOfTokensFeatureExtractor.class.name,
+            NGramFeatureExtractor.class.name
+        ].toArray()
+    ] as Object[]
     );
 
 
@@ -87,156 +96,188 @@ public class TwentyNewsgroupsGroovyExtendedExperiment {
      *
      * @throws Exception
      */
-	protected void runCrossValidation() throws Exception
-	{
-		/*
-		 * Define (instantiate) tasks
-		 */
+    protected void runCrossValidation() throws Exception
+    {
+        /*
+         * Define (instantiate) tasks
+         */
 
-		PreprocessTask preprocessTask = [
-			reader:getReaderDesc(corpusFilePathTrain,languageCode),
-			aggregate:getPreprocessing(),
-			type: "Preprocessing-TwentyNewsgroupsCV"
-		];
+        PreprocessTask preprocessTask = [
+            reader:getReaderDesc(corpusFilePathTrain,languageCode),
+            aggregate:getPreprocessing(),
+            type: "Preprocessing-TwentyNewsgroupsCV"
+        ];
 
-		MetaInfoTask metaTask = [type: "MetaInfoTask-TwentyNewsgroupsCV"];
+        MetaInfoTask metaTask = [type: "MetaInfoTask-TwentyNewsgroupsCV"];
 
-		ExtractFeaturesTask featureExtractionTask = [
-			addInstanceId: false,
-			instanceExtractor:SingleLabelInstanceExtractor.class,
-			type: "FeatureExtraction-TwentyNewsgroupsCV"
-		];
+        ExtractFeaturesTask featureExtractionTask = [
+            addInstanceId: false,
+            instanceExtractor:SingleLabelInstanceExtractor.class,
+            type: "FeatureExtraction-TwentyNewsgroupsCV"
+        ];
 
-		CrossValidationTask cvTask = [
-			type: "CVTask-TwentyNewsgroupsCV",
-			reports: [CVReport]];
-
-
-		/*
-		 * Wire tasks
-		 */
-		metaTask.addImportLatest(MetaInfoTask.INPUT_KEY, PreprocessTask.OUTPUT_KEY, preprocessTask.getType());
-		featureExtractionTask.addImportLatest(MetaInfoTask.INPUT_KEY, PreprocessTask.OUTPUT_KEY, preprocessTask.getType());
-		featureExtractionTask.addImportLatest(MetaInfoTask.META_KEY, MetaInfoTask.META_KEY, metaTask.getType());
-		cvTask.addImportLatest(MetaInfoTask.META_KEY, MetaInfoTask.META_KEY, metaTask.getType());
-		cvTask.addImportLatest(CrossValidationTask.INPUT_KEY, ExtractFeaturesTask.OUTPUT_KEY, featureExtractionTask.getType());
+        CrossValidationTask cvTask = [
+            type: "CVTask-TwentyNewsgroupsCV",
+            reports: [CVReport]];
 
 
-		/*
-		 *	Wrap wired tasks in batch task
-		 */
-
-		BatchTask batchTask = [
-			type: "Evaluation-TwentyNewsgroups-CV",
-			parameterSpace : [dimFolds, dimTopNgramsK, dimToLowerCase, dimMultiLabel, dimClassificationArgs, dimFeatureSets, dimPipelineParameters],
-			tasks:           [preprocessTask, metaTask, featureExtractionTask, cvTask],
-			executionPolicy: ExecutionPolicy.RUN_AGAIN,
-			reports:         [CVBatchReport]
-		];
-
-		/*
-		 * Run
-		 */
-		Lab.getInstance().run(batchTask);
-	}
-
-	/**
-	 * TrainTest Setting
-	 *
-	 * @throws Exception
-	 */
-	protected void runTrainTest() throws Exception
-	{
-		/*
-		 * Define (instantiate) tasks
-		 */
-
-		PreprocessTask preprocessTaskTrain = [
-			reader:getReaderDesc(corpusFilePathTrain, languageCode),
-			aggregate:getPreprocessing(),
-			type: "Preprocessing-TwentyNewsgroups-Train"
-		];
-
-		PreprocessTask preprocessTaskTest = [
-			reader:getReaderDesc(corpusFilePathTest, languageCode),
-			aggregate:getPreprocessing(),
-			type: "Preprocessing-TwentyNewsgroups-Test"
-		];
-
-		MetaInfoTask metaTask = [type: "MetaInfoTask-TwentyNewsgroups-TrainTest"];
-
-		ExtractFeaturesTask featuresTrainTask = [
-			addInstanceId: true,
-			instanceExtractor:SingleLabelInstanceExtractor.class,
-			type: "FeatureExtraction-TwentyNewsgroups-Train"
-		];
-
-		ExtractFeaturesTask featuresTestTask = [
-			addInstanceId: true,
-			instanceExtractor:SingleLabelInstanceExtractor.class,
-			type: "FeatureExtraction-TwentyNewsgroups-Test"
-		];
-
-		TestTask testTask = [
-			type:"TestTask.TwentyNewsgroups",
-			reports: [ TrainTestReport, OutcomeIDReport]
-		];
+        /*
+         * Wire tasks
+         */
+        metaTask.addImportLatest(MetaInfoTask.INPUT_KEY, PreprocessTask.OUTPUT_KEY, preprocessTask.getType());
+        featureExtractionTask.addImportLatest(MetaInfoTask.INPUT_KEY, PreprocessTask.OUTPUT_KEY, preprocessTask.getType());
+        featureExtractionTask.addImportLatest(MetaInfoTask.META_KEY, MetaInfoTask.META_KEY, metaTask.getType());
+        cvTask.addImportLatest(MetaInfoTask.META_KEY, MetaInfoTask.META_KEY, metaTask.getType());
+        cvTask.addImportLatest(CrossValidationTask.INPUT_KEY, ExtractFeaturesTask.OUTPUT_KEY, featureExtractionTask.getType());
 
 
-		/*
-		 * Wire tasks
-		 */
-		metaTask.addImportLatest(MetaInfoTask.INPUT_KEY, PreprocessTask.OUTPUT_KEY, preprocessTaskTrain.getType());
-		featuresTrainTask.addImportLatest(MetaInfoTask.INPUT_KEY, PreprocessTask.OUTPUT_KEY, preprocessTaskTrain.getType());
-		featuresTrainTask.addImportLatest(MetaInfoTask.META_KEY, MetaInfoTask.META_KEY, metaTask.getType());
-		featuresTestTask.addImportLatest(MetaInfoTask.INPUT_KEY, PreprocessTask.OUTPUT_KEY, preprocessTaskTest.getType());
-		featuresTestTask.addImportLatest(MetaInfoTask.META_KEY, MetaInfoTask.META_KEY, metaTask.getType());
-		testTask.addImportLatest(MetaInfoTask.META_KEY, MetaInfoTask.META_KEY, metaTask.getType());
-		testTask.addImportLatest(TestTask.INPUT_KEY_TRAIN, ExtractFeaturesTask.OUTPUT_KEY, featuresTrainTask.getType());
-		testTask.addImportLatest(TestTask.INPUT_KEY_TEST, ExtractFeaturesTask.OUTPUT_KEY, featuresTestTask.getType());
+        /*
+         *	Wrap wired tasks in batch task
+         */
+
+        BatchTask batchTask = [
+            type: "Evaluation-TwentyNewsgroups-CV",
+            parameterSpace : [
+                dimFolds,
+                dimTopNgramsK,
+                dimToLowerCase,
+                dimMultiLabel,
+                dimClassificationArgs,
+                dimFeatureSets,
+                dimPipelineParameters
+            ],
+            tasks:           [
+                preprocessTask,
+                metaTask,
+                featureExtractionTask,
+                cvTask
+            ],
+            executionPolicy: ExecutionPolicy.RUN_AGAIN,
+            reports:         [CVBatchReport]];
+
+        /*
+         * Run
+         */
+        Lab.getInstance().run(batchTask);
+    }
+
+    /**
+     * TrainTest Setting
+     *
+     * @throws Exception
+     */
+    protected void runTrainTest() throws Exception
+    {
+        /*
+         * Define (instantiate) tasks
+         */
+
+        PreprocessTask preprocessTaskTrain = [
+            reader:getReaderDesc(corpusFilePathTrain, languageCode),
+            aggregate:getPreprocessing(),
+            type: "Preprocessing-TwentyNewsgroups-Train"
+        ];
+
+        PreprocessTask preprocessTaskTest = [
+            reader:getReaderDesc(corpusFilePathTest, languageCode),
+            aggregate:getPreprocessing(),
+            type: "Preprocessing-TwentyNewsgroups-Test"
+        ];
+
+        MetaInfoTask metaTask = [type: "MetaInfoTask-TwentyNewsgroups-TrainTest"];
+
+        ExtractFeaturesTask featuresTrainTask = [
+            addInstanceId: true,
+            instanceExtractor:SingleLabelInstanceExtractor.class,
+            type: "FeatureExtraction-TwentyNewsgroups-Train"
+        ];
+
+        ExtractFeaturesTask featuresTestTask = [
+            addInstanceId: true,
+            instanceExtractor:SingleLabelInstanceExtractor.class,
+            type: "FeatureExtraction-TwentyNewsgroups-Test"
+        ];
+
+        TestTask testTask = [
+            type:"TestTask.TwentyNewsgroups",
+            reports: [
+                TrainTestReport,
+                OutcomeIDReport]
+        ];
 
 
-		/*
-		 *	Wrap wired tasks in batch task
-		 */
-
-		BatchTask batchTask = [
-			type: "Evaluation-TwentyNewsgroups-TrainTest",
-			parameterSpace : [ dimFolds, dimTopNgramsK, dimToLowerCase, dimMultiLabel, dimClassificationArgs, dimFeatureSets, dimPipelineParameters],
-			tasks:           [ preprocessTaskTrain, preprocessTaskTest, metaTask, featuresTrainTask, featuresTestTask, testTask],
-			executionPolicy: ExecutionPolicy.RUN_AGAIN,
-			reports:         [ BatchTrainTestReport, BatchOutcomeIDReport]
-		];
-
-		// Run
-		Lab.getInstance().run(batchTask);
-	}
+        /*
+         * Wire tasks
+         */
+        metaTask.addImportLatest(MetaInfoTask.INPUT_KEY, PreprocessTask.OUTPUT_KEY, preprocessTaskTrain.getType());
+        featuresTrainTask.addImportLatest(MetaInfoTask.INPUT_KEY, PreprocessTask.OUTPUT_KEY, preprocessTaskTrain.getType());
+        featuresTrainTask.addImportLatest(MetaInfoTask.META_KEY, MetaInfoTask.META_KEY, metaTask.getType());
+        featuresTestTask.addImportLatest(MetaInfoTask.INPUT_KEY, PreprocessTask.OUTPUT_KEY, preprocessTaskTest.getType());
+        featuresTestTask.addImportLatest(MetaInfoTask.META_KEY, MetaInfoTask.META_KEY, metaTask.getType());
+        testTask.addImportLatest(MetaInfoTask.META_KEY, MetaInfoTask.META_KEY, metaTask.getType());
+        testTask.addImportLatest(TestTask.INPUT_KEY_TRAIN, ExtractFeaturesTask.OUTPUT_KEY, featuresTrainTask.getType());
+        testTask.addImportLatest(TestTask.INPUT_KEY_TEST, ExtractFeaturesTask.OUTPUT_KEY, featuresTestTask.getType());
 
 
-	private CollectionReaderDescription getReaderDesc(String corpusFilePath, String language)
-			throws ResourceInitializationException, IOException
-	{
-		return createDescription(
-			TwentyNewsgroupsCorpusReader,
-			TwentyNewsgroupsCorpusReader.PARAM_PATH, corpusFilePath,
-            TwentyNewsgroupsCorpusReader.PARAM_LANGUAGE, language,
-			TwentyNewsgroupsCorpusReader.PARAM_PATTERNS, [TwentyNewsgroupsCorpusReader.INCLUDE_PREFIX + "*/*.txt"]
-		);
-	}
+        /*
+         *	Wrap wired tasks in batch task
+         */
 
-	private AnalysisEngineDescription getPreprocessing()
-			throws ResourceInitializationException
-	{
-		return createAggregateDescription(
-			createPrimitiveDescription(BreakIteratorSegmenter),
-			createPrimitiveDescription(OpenNlpPosTagger)
-		);
-	}
+        BatchTask batchTask = [
+            type: "Evaluation-TwentyNewsgroups-TrainTest",
+            parameterSpace : [
+                dimFolds,
+                dimTopNgramsK,
+                dimToLowerCase,
+                dimMultiLabel,
+                dimClassificationArgs,
+                dimFeatureSets,
+                dimPipelineParameters
+            ],
+            tasks:           [
+                preprocessTaskTrain,
+                preprocessTaskTest,
+                metaTask,
+                featuresTrainTask,
+                featuresTestTask,
+                testTask
+            ],
+            executionPolicy: ExecutionPolicy.RUN_AGAIN,
+            reports:         [
+                BatchTrainTestReport,
+                BatchOutcomeIDReport]
+        ];
 
-	public static void main(String[] args)
-	{
-		new TwentyNewsgroupsGroovyExtendedExperiment().runCrossValidation();
-		new TwentyNewsgroupsGroovyExtendedExperiment().runTrainTest();
-	}
+        // Run
+        Lab.getInstance().run(batchTask);
+    }
+
+
+    private CollectionReaderDescription getReaderDesc(String corpusFilePath, String language)
+            throws ResourceInitializationException, IOException
+    {
+        return createDescription(
+        TwentyNewsgroupsCorpusReader,
+        TwentyNewsgroupsCorpusReader.PARAM_PATH, corpusFilePath,
+        TwentyNewsgroupsCorpusReader.PARAM_LANGUAGE, language,
+        TwentyNewsgroupsCorpusReader.PARAM_PATTERNS, [
+            TwentyNewsgroupsCorpusReader.INCLUDE_PREFIX + "*/*.txt"]
+        );
+    }
+
+    private AnalysisEngineDescription getPreprocessing()
+    throws ResourceInitializationException
+    {
+        return createAggregateDescription(
+        createPrimitiveDescription(BreakIteratorSegmenter),
+        createPrimitiveDescription(OpenNlpPosTagger)
+        );
+    }
+
+    public static void main(String[] args)
+    {
+        new TwentyNewsgroupsGroovyExtendedExperiment().runCrossValidation();
+        new TwentyNewsgroupsGroovyExtendedExperiment().runTrainTest();
+    }
 
 }
