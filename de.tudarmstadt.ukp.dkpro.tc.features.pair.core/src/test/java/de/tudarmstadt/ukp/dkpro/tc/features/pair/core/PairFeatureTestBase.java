b8f87ece1366f1866d64748e86998445713869a1
Nicolai Erbs <nico@erbs.eu>
2013-06-03 15:57:32 +0000

    Added tests for pair features

---
+++ de.tudarmstadt.ukp.dkpro.tc.features.pair.core/src/test/java/de/tudarmstadt/ukp/dkpro/tc/features/pair/core/PairFeatureTestBase.java
@@ -0,0 +1,31 @@
+package de.tudarmstadt.ukp.dkpro.tc.features.pair.core;
+
+import java.util.List;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
+import de.tudarmstadt.ukp.dkpro.tc.exception.TextClassificationException;
+
+public class PairFeatureTestBase {
+	
+	public List<Feature> runExtractor(AnalysisEngine engine, PairFeatureExtractor extractor) throws ResourceInitializationException, TextClassificationException, AnalysisEngineProcessException{
+
+		JCas jcas1 = engine.newJCas();
+        jcas1.setDocumentLanguage("en");
+        jcas1.setDocumentText("This is the text of view 1");
+        engine.process(jcas1);
+        
+        JCas jcas2 = engine.newJCas();
+        jcas2.setDocumentLanguage("en");
+        jcas2.setDocumentText("This is the text of view 2");
+        engine.process(jcas2);
+        
+        return extractor.extract(jcas1, jcas2);
+
+	}
+
+}
