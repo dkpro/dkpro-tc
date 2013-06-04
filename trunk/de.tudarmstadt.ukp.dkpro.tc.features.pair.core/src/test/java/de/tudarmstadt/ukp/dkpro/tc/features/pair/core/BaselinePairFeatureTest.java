28886efd0f2883ecf8a9edd8cba74c2946fb5b08
nico.erbs@googlemail.com <nico.erbs@googlemail.com@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2013-06-04 12:15:45 +0000

    Added JUnit test for BaselinePairFeature

---
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.features.pair.core/src/test/java/de/tudarmstadt/ukp/dkpro/tc/features/pair/core/BaselinePairFeatureTest.java
@@ -0,0 +1,41 @@
+package de.tudarmstadt.ukp.dkpro.tc.features.pair.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.uimafit.factory.AnalysisEngineFactory.createAggregateDescription;
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitive;
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
+
+import static de.tudarmstadt.ukp.dkpro.tc.features.pair.core.FeatureTestUtil.assertFeature;
+
+import java.util.List;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
+
+public class BaselinePairFeatureTest extends PairFeatureTestBase
+{
+	
+    @Test
+    public void baselinePairFeatureExtractorTest()
+        throws Exception
+    {
+        AnalysisEngineDescription desc = createAggregateDescription(
+                createPrimitiveDescription(BreakIteratorSegmenter.class)
+                
+        );
+        AnalysisEngine engine = createPrimitive(desc);
+
+        BaselinePairFeatureExtractor extractor = new BaselinePairFeatureExtractor();
+        List<Feature> features = runExtractor(engine, extractor);
+
+        assertEquals(1, features.size());
+        
+        for (Feature feature : features) {
+            assertFeature("Baseline", 0, feature);
+        }
+    }
+}
---
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.features.pair.core/src/test/java/de/tudarmstadt/ukp/dkpro/tc/features/pair/core/FeatureTestUtil.java
@@ -0,0 +1,19 @@
+package de.tudarmstadt.ukp.dkpro.tc.features.pair.core;
+
+import junit.framework.Assert;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
+
+public class FeatureTestUtil
+{
+    public static void assertFeature(String expectedName, Object expectedValue, Feature actualFeature) {
+        Assert.assertNotNull(actualFeature);
+        Assert.assertEquals(expectedName, actualFeature.getName());
+        Assert.assertEquals(expectedValue, actualFeature.getValue());
+    }
+
+    public static void assertFeature(String expectedName, double expectedValue, Feature actualFeature, double epsilon) {
+        Assert.assertNotNull(actualFeature);
+        Assert.assertEquals(expectedName, actualFeature.getName());
+        Assert.assertEquals(expectedValue, (Double) actualFeature.getValue(), epsilon);
+    }
+}
