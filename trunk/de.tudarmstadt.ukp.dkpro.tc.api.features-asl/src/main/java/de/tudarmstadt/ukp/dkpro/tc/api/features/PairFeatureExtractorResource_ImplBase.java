4337207f0471a45a002fabb706a3cb5d7de42900
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2013-06-10 15:24:15 +0000

    Testing feature extractors as external resources.

---
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.api.features-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/api/features/PairFeatureExtractorResource_ImplBase.java
@@ -0,0 +1,11 @@
+package de.tudarmstadt.ukp.dkpro.tc.api.features;
+
+import org.apache.uima.fit.component.Resource_ImplBase;
+
+public abstract class PairFeatureExtractorResource_ImplBase
+    extends Resource_ImplBase
+    implements PairFeatureExtractor
+{
+    
+
+}
