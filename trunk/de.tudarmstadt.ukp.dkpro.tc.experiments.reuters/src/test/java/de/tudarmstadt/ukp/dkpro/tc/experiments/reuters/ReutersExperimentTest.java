f76dcc8a19cebbb08705f1a1d872467682a6c53d
daxenberger@ukp.informatik.tu-darmstadt.de <daxenberger@ukp.informatik.tu-darmstadt.de@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2013-06-27 14:58:22 +0000

    Fixed test (will break the experiment ;-)

--- trunk/de.tudarmstadt.ukp.dkpro.tc.experiments.reuters/src/test/java/de/tudarmstadt/ukp/dkpro/tc/experiments/reuters/ReutersExperimentTest.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.experiments.reuters/src/test/java/de/tudarmstadt/ukp/dkpro/tc/experiments/reuters/ReutersExperimentTest.java
@@ -10,12 +10,11 @@ import org.junit.rules.TestName;
 import de.tudarmstadt.ukp.dkpro.lab.task.ParameterSpace;
 
 /**
- * This is not exactly a unit test (yet).
- * It just ensures that the experiments run without throwing any exception.
- * Additional unit tests should test the inner workings of the experiments
- *
+ * This is not exactly a unit test (yet). It just ensures that the experiments run without throwing
+ * any exception. Additional unit tests should test the inner workings of the experiments
+ * 
  * @author Oliver Ferschke
- *
+ * 
  */
 public class ReutersExperimentTest
 {
@@ -23,26 +22,29 @@ public class ReutersExperimentTest
     ParameterSpace pSpace;
 
     @Before
-    public void setup() throws Exception
+    public void setup()
+        throws Exception
     {
-            String path = "target/repository/"+getClass().getSimpleName()+"/"+name.getMethodName();
-            System.setProperty("DKPRO_HOME", new File(path).getAbsolutePath());
+        String path = "target/repository/" + getClass().getSimpleName() + "/"
+                + name.getMethodName();
+        System.setProperty("DKPRO_HOME", new File(path).getAbsolutePath());
 
-            experiment = new ReutersTextClassification();
-            pSpace=experiment.setup();
+        experiment = new ReutersTextClassification();
+        pSpace = experiment.setup();
     }
 
-
     @Test
-    public void testJavaCrossValidation() throws Exception
+    public void testJavaCrossValidation()
+        throws Exception
     {
         experiment.runCrossValidation(pSpace);
     }
 
     @Test
-    public void testJavaTrainTest() throws Exception
+    public void testJavaTrainTest()
+        throws Exception
     {
-        experiment.runCrossValidation(pSpace);
+        experiment.runTrainTest(pSpace);
     }
 
     @Rule
@@ -51,6 +53,6 @@ public class ReutersExperimentTest
     @Before
     public void printSeparator()
     {
-            System.out.println("\n=== " + name.getMethodName() + " =====================");
+        System.out.println("\n=== " + name.getMethodName() + " =====================");
     }
 }
