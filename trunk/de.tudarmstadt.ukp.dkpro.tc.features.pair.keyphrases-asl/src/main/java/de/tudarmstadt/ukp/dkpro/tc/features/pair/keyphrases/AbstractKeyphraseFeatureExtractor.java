17f9c54ea241c3f9f2fbb95e0ffbf40cf60e8250
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2013-07-31 13:40:10 +0000

    Trying to fix features.pair.keyphrases

--- trunk/de.tudarmstadt.ukp.dkpro.tc.features.pair.keyphrases-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/pair/keyphrases/AbstractKeyphraseFeatureExtractor.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.features.pair.keyphrases-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/pair/keyphrases/AbstractKeyphraseFeatureExtractor.java
@@ -3,7 +3,14 @@ package de.tudarmstadt.ukp.dkpro.tc.features.pair.keyphrases;
 
 import java.util.List;
 
-public abstract class AbstractKeyphraseFeatureExtractor implements PairFeature {
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+
+import de.tudarmstadt.ukp.dkpro.keyphrases.core.evaluator.util.EvaluatorUtils;
+import de.tudarmstadt.ukp.dkpro.keyphrases.core.type.Keyphrase;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.PairFeatureExtractor;
+
+public abstract class AbstractKeyphraseFeatureExtractor implements PairFeatureExtractor {
 	
 	protected List<Keyphrase> getKeyphrases(JCas jcas, int number){
 		List<Keyphrase> keyphrases = 
