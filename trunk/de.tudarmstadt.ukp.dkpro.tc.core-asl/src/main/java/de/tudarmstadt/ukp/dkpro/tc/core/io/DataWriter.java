0cd7aad0ef3d781cf548d3fb4a93b629080fd621
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2013-09-17 18:16:10 +0000

    Moved DataWriter interface to io package.

--- trunk/de.tudarmstadt.ukp.dkpro.tc.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/io/DataWriter.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/io/DataWriter.java
@@ -1,9 +1,15 @@
-package de.tudarmstadt.ukp.dkpro.tc.core.extractor;
+package de.tudarmstadt.ukp.dkpro.tc.core.io;
 
 import java.io.File;
 
 import de.tudarmstadt.ukp.dkpro.tc.api.features.InstanceList;
 
+/**
+ * Interface for data writers that write instances in the representation format used by machine learning tools.
+ * 
+ * @author zesch
+ *
+ */
 public interface DataWriter
 {
     public void write(File outputDirectory, InstanceList instanceList, boolean useDenseInstances, boolean isRegressionExperiment)
