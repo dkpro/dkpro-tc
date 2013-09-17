7940910fe67a517f690c279e3e6563480ed96358
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2013-09-17 18:40:09 +0000

    Added documentation.

--- trunk/de.tudarmstadt.ukp.dkpro.tc.api-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/io/TCReaderSingleLabel.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.api-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/io/TCReaderSingleLabel.java
@@ -3,6 +3,12 @@ package de.tudarmstadt.ukp.dkpro.tc.io;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.jcas.JCas;
 
+/**
+ * Interface that should be implemented by readers for single label setups.
+ * 
+ * @author zesch
+ *
+ */
 public interface TCReaderSingleLabel
 {
     public String getTextClassificationOutcome(JCas jcas) throws CollectionException;
