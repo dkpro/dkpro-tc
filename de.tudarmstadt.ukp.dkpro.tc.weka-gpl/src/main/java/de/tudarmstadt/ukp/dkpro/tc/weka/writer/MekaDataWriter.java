e100b357f082391f88bdbf26797787386272959b
Torsten Zesch <torsten.zesch@gmail.com>
2013-09-17 18:16:10 +0000

    Moved DataWriter interface to io package.

--- de.tudarmstadt.ukp.dkpro.tc.weka-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/weka/writer/MekaDataWriter.java
+++ de.tudarmstadt.ukp.dkpro.tc.weka-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/weka/writer/MekaDataWriter.java
@@ -3,7 +3,7 @@ package de.tudarmstadt.ukp.dkpro.tc.weka.writer;
 import java.io.File;
 
 import de.tudarmstadt.ukp.dkpro.tc.api.features.InstanceList;
-import de.tudarmstadt.ukp.dkpro.tc.core.extractor.DataWriter;
+import de.tudarmstadt.ukp.dkpro.tc.core.io.DataWriter;
 import de.tudarmstadt.ukp.dkpro.tc.weka.util.WekaUtils;
 
 public class MekaDataWriter
