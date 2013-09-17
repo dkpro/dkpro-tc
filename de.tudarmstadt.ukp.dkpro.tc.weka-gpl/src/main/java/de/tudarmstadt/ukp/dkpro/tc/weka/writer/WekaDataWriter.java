0cd27769f8a66be6821328d46ac67c890e1a1bf1
Torsten Zesch <torsten.zesch@gmail.com>
2013-09-17 18:38:32 +0000

    Moved magic string to constants.

--- de.tudarmstadt.ukp.dkpro.tc.weka-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/weka/writer/WekaDataWriter.java
+++ de.tudarmstadt.ukp.dkpro.tc.weka-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/weka/writer/WekaDataWriter.java
@@ -3,21 +3,25 @@ package de.tudarmstadt.ukp.dkpro.tc.weka.writer;
 import java.io.File;
 
 import de.tudarmstadt.ukp.dkpro.tc.api.features.InstanceList;
+import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
 import de.tudarmstadt.ukp.dkpro.tc.core.io.DataWriter;
 import de.tudarmstadt.ukp.dkpro.tc.weka.util.WekaUtils;
 
+/**
+ * {@link DataWriter} for the Meka machine learning tool.
+ * 
+ * @author zesch
+ *
+ */
 public class WekaDataWriter
-    implements DataWriter
+    implements DataWriter, Constants
 {
-
-    // FIXME should this name be fixed somewhere else?
-    public static final String arffFileName = "training-data.arff.gz";
     
     @Override
     public void write(File outputDirectory, InstanceList instanceList, boolean useDenseInstances, boolean isRegressionExperiment)
             throws Exception
     {
-        WekaUtils.instanceListToArffFile(new File(outputDirectory, arffFileName), instanceList, useDenseInstances, isRegressionExperiment);
+        WekaUtils.instanceListToArffFile(new File(outputDirectory, ARFF_FILENAME), instanceList, useDenseInstances, isRegressionExperiment);
     }
 
 }
