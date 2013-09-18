2ba9e57a9fa6c5479c22585c73a44f07bd195035
Johannes Daxenberger <daxenberger@ukp.informatik.tu-darmstadt.de>
2013-09-18 17:58:47 +0000

    new constants

--- de.tudarmstadt.ukp.dkpro.tc.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/Constants.java
+++ de.tudarmstadt.ukp.dkpro.tc.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/Constants.java
@@ -6,7 +6,7 @@ import org.apache.uima.cas.CAS;
  * Basic constants that are used throughout the project
  * 
  * @author zesch
- *
+ * 
  */
 public interface Constants
 {
@@ -17,44 +17,48 @@ public interface Constants
     public static String INITIAL_VIEW = CAS.NAME_DEFAULT_SOFA;
     public static String PART_ONE = "PART_ONE";
     public static String PART_TWO = "PART_TWO";
-    
+
     /*
      * Instance storage
      */
 
     public static final String CLASS_ATTRIBUTE_PREFIX = "__";
     public static final String CLASS_ATTRIBUTE_NAME = "outcome";
-    
+
     /*
      * Discriminators
      */
-    
+
     public static final String DIM_READER_TRAIN = "readerTrain";
     public static final String DIM_READER_TRAIN_PARAMS = "readerTrainParams";
     public static final String DIM_READER_TEST = "readerTest";
     public static final String DIM_READER_TEST_PARAMS = "readerTestParams";
-    
+
     public static final String DIM_FEATURE_SET = "featureSet";
-    
+
     public static final String DIM_PIPELINE_PARAMS = "pipelineParameters";
-    
+
     public static final String DIM_CLASSIFICATION_ARGS = "classificationArguments";
-    
+
     public static final String DIM_IS_REGRESSION = "isRegressionExperiment";
     public static final String DIM_MULTI_LABEL = "multiLabel";
+    public static final String DIM_BIPARTITION_THRESHOLD = "threshold";
     public static final String DIM_LOWER_CASE = "lowerCase";
 
-    
+    public static final String DIM_DATA_WRITER = "dataWriter";
+
     /*
      * Mainly for reports
      */
     public static final String EVAL_FILE_NAME = "evalulation_results";
     public static final String SUFFIX_EXCEL = ".xls";
     public static final String SUFFIX_CSV = ".csv";
-    
+
     /*
-     * Weka 
+     * Weka
      */
     public static final String ARFF_FILENAME = "training-data.arff.gz";
+    public static final String MEKA_DATA_WRITER_NAME = "de.tudarmstadt.ukp.dkpro.tc.weka.writer.MekaDataWriter";
+    public static final String WEKA_DATA_WRITER_NAME = "de.tudarmstadt.ukp.dkpro.tc.weka.writer.WekaDataWriter";
 
 }
