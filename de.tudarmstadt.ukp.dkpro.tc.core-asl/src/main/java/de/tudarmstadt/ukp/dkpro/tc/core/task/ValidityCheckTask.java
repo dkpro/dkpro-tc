a68c114dca925ab025c34d98050b7d78a064e43a
Johannes Daxenberger <daxenberger@ukp.informatik.tu-darmstadt.de>
2013-09-18 17:58:13 +0000

    Adding new validity checks (classification unit and multi-label)

--- de.tudarmstadt.ukp.dkpro.tc.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/task/ValidityCheckTask.java
+++ de.tudarmstadt.ukp.dkpro.tc.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/task/ValidityCheckTask.java
@@ -20,11 +20,12 @@ import de.tudarmstadt.ukp.dkpro.lab.uima.task.impl.UimaTaskBase;
 import de.tudarmstadt.ukp.dkpro.tc.core.task.uima.ValidityCheckConnector;
 
 /**
- * Checks that everything has been configured properly and throws more meaningful exception otherwise than would have been thrown downstream.
- * This should be the first task in the TC pipeline.
+ * Checks that everything has been configured properly and throws more meaningful exception
+ * otherwise than would have been thrown downstream. This should be the first task in the TC
+ * pipeline.
  * 
  * @author zesch
- *
+ * 
  */
 public class ValidityCheckTask
     extends UimaTaskBase
@@ -33,15 +34,18 @@ public class ValidityCheckTask
 
     @Discriminator
     protected Class<? extends CollectionReader> readerTrain;
-
     @Discriminator
     protected List<Object> readerTrainParams;
-
     @Discriminator
     protected List<Object> pipelineParameters;
-    
     @Discriminator
     private boolean isRegressionExperiment = false;
+    @Discriminator
+    private boolean multiLabel = false;
+    @Discriminator
+    private String dataWriter;
+    @Discriminator
+    private boolean isUnitClassification = false;
 
     @Override
     public CollectionReaderDescription getCollectionReaderDescription(TaskContext aContext)
@@ -56,20 +60,31 @@ public class ValidityCheckTask
     @Override
     public AnalysisEngineDescription getAnalysisEngineDescription(TaskContext aContext)
         throws ResourceInitializationException, IOException
-    {    
-        
+    {
+        // check mandatory dimensions
+        if (dataWriter == null) {
+            throw new ResourceInitializationException("You need to set the dataWriter dimension.",
+                    null);
+        }
+
         // make a dummy folder so that the lab can create an import on it
         File file = new File(aContext.getStorageLocation(DUMMY_KEY, AccessMode.READWRITE)
                 .getPath());
         file.mkdir();
-        
+
         List<Object> parameters = new ArrayList<Object>();
         if (pipelineParameters != null) {
             parameters.addAll(pipelineParameters);
         }
         parameters.add(ValidityCheckConnector.PARAM_IS_REGRESSION);
         parameters.add(isRegressionExperiment);
-        
+        parameters.add(ValidityCheckConnector.PARAM_IS_MULTILABEL);
+        parameters.add(multiLabel);
+        parameters.add(ValidityCheckConnector.PARAM_DATA_WRITER);
+        parameters.add(dataWriter);
+        parameters.add(ValidityCheckConnector.PARAM_IS_UNIT_CLASSIFICATION);
+        parameters.add(isUnitClassification);
+
         return createEngineDescription(ValidityCheckConnector.class, parameters.toArray());
     }
 }
