e717700d4de741ba6687fb8e43aa1c06bd0502a8
Artem Vovk <vovk.artem@gmail.com>
2013-10-02 10:43:10 +0000

    Read resource in more proper way (use InputStream instead of File)

--- de.tudarmstadt.ukp.dkpro.tc.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/ExperimentStarter.java
+++ de.tudarmstadt.ukp.dkpro.tc.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/ExperimentStarter.java
@@ -3,9 +3,10 @@ package de.tudarmstadt.ukp.dkpro.tc.core;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyObject;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.StringWriter;
+
+import org.apache.commons.io.IOUtils;
 
 /**
  * Java wrapper class for the execution of Groovy scripts. It is needed to avoid the pre-compilation
@@ -21,22 +22,19 @@ public class ExperimentStarter
      * 
      * @param pathToScript
      *            path to Groovy script.
-     * 
      */
     public static void start(String pathToScript)
         throws InstantiationException, IllegalAccessException, IOException
     {
-        try {
-            ClassLoader parent = ExperimentStarter.class.getClassLoader();
-            GroovyClassLoader loader = new GroovyClassLoader(parent);
-            Class<?> groovyClass = loader.parseClass(new File(parent.getResource(pathToScript)
-                    .toURI()));
-            GroovyObject groovyObject = (GroovyObject) groovyClass.newInstance();
-            Object[] a = {};
-            groovyObject.invokeMethod("run", a);
-        }
-        catch (URISyntaxException e) {
-            throw new IOException("Wrong path to the script file", e);
-        }
+        ClassLoader parent = ExperimentStarter.class.getClassLoader();
+        GroovyClassLoader loader = new GroovyClassLoader(parent);
+
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(parent.getResourceAsStream(pathToScript), writer, "UTF-8");
+        Class<?> groovyClass = loader.parseClass(writer.toString());
+        GroovyObject groovyObject = (GroovyObject) groovyClass.newInstance();
+        Object[] a = {};
+        groovyObject.invokeMethod("run", a);
+
     }
 }
