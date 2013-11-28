d0a0f8725043cc838be627e36b560fd5f365794d
Johannes Daxenberger <daxenberger@ukp.informatik.tu-darmstadt.de>
2013-11-28 10:35:15 +0000

    Adding methods to convert a TC instance into Weka/Meka instances, based upon a given set of features/attributes; along with tests.

--- de.tudarmstadt.ukp.dkpro.tc.weka-gpl/src/test/java/de/tudarmstadt/ukp/dkpro/tc/weka/util/WekaUtilTest.java
+++ de.tudarmstadt.ukp.dkpro.tc.weka-gpl/src/test/java/de/tudarmstadt/ukp/dkpro/tc/weka/util/WekaUtilTest.java
@@ -1,10 +1,16 @@
 package de.tudarmstadt.ukp.dkpro.tc.weka.util;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
+import weka.core.Attribute;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.Instance;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.InstanceList;
@@ -14,9 +20,9 @@ public class WekaUtilTest
 
     @Test
     public void instanceToArffTest()
-            throws Exception
+        throws Exception
     {
-                
+
         Instance i1 = new Instance();
         i1.addFeature(new Feature("feature1", 2));
         i1.addFeature(new Feature("feature2", 2));
@@ -40,22 +46,21 @@ public class WekaUtilTest
         iList.addInstance(i2);
         iList.addInstance(i3);
 
-        
         File outfile = new File("target/test/out.txt");
         outfile.mkdirs();
         outfile.createNewFile();
         outfile.deleteOnExit();
-        
+
         WekaUtils.instanceListToArffFile(outfile, iList);
-        
+
         System.out.println(FileUtils.readFileToString(outfile));
     }
-    
+
     @Test
     public void instanceToArffTest_multiLabel()
-            throws Exception
+        throws Exception
     {
-                
+
         Instance i1 = new Instance();
         i1.addFeature(new Feature("feature1", 2));
         i1.addFeature(new Feature("feature2", 2));
@@ -79,14 +84,110 @@ public class WekaUtilTest
         iList.addInstance(i2);
         iList.addInstance(i3);
 
-        
         File outfile = new File("target/test/out.txt");
         outfile.mkdirs();
         outfile.createNewFile();
         outfile.deleteOnExit();
-        
+
         WekaUtils.instanceListToArffFile(outfile, iList);
-        
+
         System.out.println(FileUtils.readFileToString(outfile));
+    }
+
+    @Test
+    public void tcInstanceToWekaInstanceTest()
+        throws Exception
+    {
+        List<String> outcomeValues = Arrays.asList(new String[] { "outc_1", "outc_2", "outc_3" });
+
+        Instance i1 = new Instance();
+        i1.addFeature(new Feature("feature1", 2));
+        i1.addFeature(new Feature("feature2", 2));
+        i1.addFeature(new Feature("feature3_{{", "a"));
+
+        Instance i2 = new Instance();
+        i2.addFeature(new Feature("feature1", 1));
+        i2.addFeature(new Feature("feature4", "val_1"));
+        i2.addFeature(new Feature("feature3_{{", "b"));
+
+        List<Attribute> attributes = new ArrayList<Attribute>();
+        attributes.add(new Attribute("feature5"));
+        attributes.add(new Attribute("feature2"));
+        attributes.add(new Attribute("feature4", Arrays.asList(new String[] { "val_1", "val_2" })));
+        attributes.add(new Attribute("feature1"));
+        attributes.add(new Attribute("outcome", outcomeValues));
+
+        weka.core.Instance wekaInstance1 = WekaUtils.tcInstanceToWekaInstance(i1, attributes,
+                outcomeValues, false);
+        weka.core.Instance wekaInstance2 = WekaUtils.tcInstanceToWekaInstance(i2, attributes,
+                outcomeValues, false);
+
+        assertEquals(true, wekaInstance1.equalHeaders(wekaInstance2));
+        assertEquals(5, wekaInstance1.numAttributes());
+
+        wekaInstance1.dataset().add(wekaInstance1);
+        wekaInstance2.dataset().add(wekaInstance2);
+        System.out.println(wekaInstance1.dataset() + "\n");
+        System.out.println(wekaInstance2.dataset() + "\n");
+    }
+
+    @Test
+    public void tcInstanceToMekaInstanceTest()
+        throws Exception
+    {
+        List<String> outcomeValues = Arrays.asList(new String[] { "outc_1", "outc_2", "outc_3" });
+
+        Instance i1 = new Instance();
+        i1.addFeature(new Feature("feature1", 2));
+        i1.addFeature(new Feature("feature2", 2));
+        i1.addFeature(new Feature("feature3_{{", "a"));
+
+        Instance i2 = new Instance();
+        i2.addFeature(new Feature("feature1", 1));
+        i2.addFeature(new Feature("feature4", "val_1"));
+        i2.addFeature(new Feature("feature3_{{", "b"));
+
+        List<Attribute> attributes = new ArrayList<Attribute>();
+        attributes.add(new Attribute("outc_1", Arrays.asList(new String[] { "0", "1" })));
+        attributes.add(new Attribute("outc_2", Arrays.asList(new String[] { "0", "1" })));
+        attributes.add(new Attribute("outc_3", Arrays.asList(new String[] { "0", "1" })));
+        attributes.add(new Attribute("feature5"));
+        attributes.add(new Attribute("feature2"));
+        attributes.add(new Attribute("feature4", Arrays.asList(new String[] { "val_1", "val_2" })));
+        attributes.add(new Attribute("feature1"));
+
+        weka.core.Instance wekaInstance1 = WekaUtils.tcInstanceToMekaInstance(i1, attributes,
+                outcomeValues);
+        weka.core.Instance wekaInstance2 = WekaUtils.tcInstanceToMekaInstance(i2, attributes,
+                outcomeValues);
+
+        assertEquals(true, wekaInstance1.equalHeaders(wekaInstance2));
+        assertEquals(7, wekaInstance1.numAttributes());
+
+        wekaInstance1.dataset().add(wekaInstance1);
+        wekaInstance2.dataset().add(wekaInstance2);
+        System.out.println(wekaInstance1.dataset() + "\n");
+        System.out.println(wekaInstance2.dataset() + "\n");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tcInstanceToWekaInstanceFailTest()
+        throws Exception
+    {
+        List<String> outcomeValues = Arrays.asList(new String[] { "outc_1", "outc_2", "outc_3" });
+
+        Instance i1 = new Instance();
+        i1.addFeature(new Feature("feature1", 2));
+        i1.addFeature(new Feature("feature4", "val_1"));
+        i1.addFeature(new Feature("feature3_{{", "a"));
+
+        List<Attribute> attributes = new ArrayList<Attribute>();
+        attributes.add(new Attribute("feature2"));
+        attributes.add(new Attribute("feature4", Arrays.asList(new String[] { "val_4", "val_2" })));
+        attributes.add(new Attribute("outcome", outcomeValues));
+
+        @SuppressWarnings("unused")
+        weka.core.Instance wekaInstance1 = WekaUtils.tcInstanceToWekaInstance(i1, attributes,
+                outcomeValues, false);
     }
 }
