275d111d2076212fdc436bf4c91b9bd48bf58bc6
Torsten Zesch <torsten.zesch@gmail.com>
2013-12-10 12:13:48 +0000

    Constants instead of magic strings for metric names in reports.

---
+++ de.tudarmstadt.ukp.dkpro.tc.weka-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/weka/report/ReportConstants.java
@@ -0,0 +1,23 @@
+package de.tudarmstadt.ukp.dkpro.tc.weka.report;
+
+public class ReportConstants
+{
+    // accuracy
+    public static final String CORRECT = "correct";
+    public static final String INCORRECT = "incorrect";
+    public static final String PCT_CORRECT = "pctCorrect";
+    public static final String PCT_INCORRECT = "pctIncorrect";
+    public static final String PCT_UNCLASSIFIED = "pctUnclassified";
+    
+    // P/R/F
+    public static final String PRECISION = "precision";
+    public static final String RECALL = "recall";
+    public static final String FMEASURE = "fmeasure";
+    
+    // regression
+    public static final String CORRELATION = "Pearson correlation";
+    public static final String MEAN_ABSOLUTE_ERROR = "mean absolute error";
+    public static final String RELATIVE_ABSOLUTE_ERROR = "relative absolute error";
+    public static final String ROOT_MEAN_SQUARED_ERROR = "root mean squared error";
+    public static final String ROOT_RELATIVE_SQUARED_ERROR = "root relative squared error";
+}
