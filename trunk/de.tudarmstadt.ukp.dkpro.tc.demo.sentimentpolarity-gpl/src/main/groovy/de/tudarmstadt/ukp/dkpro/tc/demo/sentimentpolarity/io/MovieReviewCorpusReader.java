22749b192784907afd39b72d79fda7d1b818d346
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2013-12-17 20:38:13 +0000

    Abstract base readers for single-label and multi-label that reduce code duplication. (and might show the direction why I introduced the single/multi label interfaces :)

--- trunk/de.tudarmstadt.ukp.dkpro.tc.demo.sentimentpolarity-gpl/src/main/groovy/de/tudarmstadt/ukp/dkpro/tc/demo/sentimentpolarity/io/MovieReviewCorpusReader.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.demo.sentimentpolarity-gpl/src/main/groovy/de/tudarmstadt/ukp/dkpro/tc/demo/sentimentpolarity/io/MovieReviewCorpusReader.java
@@ -1,50 +1,25 @@
 package de.tudarmstadt.ukp.dkpro.tc.demo.sentimentpolarity.io;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.apache.uima.cas.CAS;
-import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.jcas.JCas;
 
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
-import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
-import de.tudarmstadt.ukp.dkpro.tc.io.TCReaderSingleLabel;
-import de.tudarmstadt.ukp.dkpro.tc.type.TextClassificationOutcome;
+import de.tudarmstadt.ukp.dkpro.tc.core.io.SingleLabelReaderBase;
 
 public class MovieReviewCorpusReader
-    extends TextReader
-    implements TCReaderSingleLabel
+    extends SingleLabelReaderBase
 {
-
-    @Override
-    public void getNext(CAS aCAS)
-        throws IOException, CollectionException
-    {
-        super.getNext(aCAS);
-        
-        JCas jcas;
-        try {
-            jcas = aCAS.getJCas();
-        }
-        catch (CASException e) {
-            throw new CollectionException();
-        }
-
-        TextClassificationOutcome outcome = new TextClassificationOutcome(jcas);
-        outcome.setOutcome(getTextClassificationOutcome(jcas));
-        outcome.addToIndexes();
-    }
-
     @Override
     public String getTextClassificationOutcome(JCas jcas)
             throws CollectionException
     {
         try {
-            return new File(new URI(DocumentMetaData.get(jcas).getDocumentUri()).getPath()).getParentFile().getName();
+            String uriString = DocumentMetaData.get(jcas).getDocumentUri();
+            return new File(new URI(uriString).getPath()).getParentFile().getName();
         }
         catch (URISyntaxException e) {
             throw new CollectionException(e);
