1f78fa7b9e416c27aa34477d6581faeb9edf99ce
Torsten Zesch <torsten.zesch@gmail.com>
2014-01-19 20:50:49 +0000

    New lucene-based version of the ngram FE and meta-collector. Does not yet give the same results as the old one. Needs to be investigated :)

---
+++ de.tudarmstadt.ukp.dkpro.tc.features.ngram-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/LuceneNGramFeatureExtractor.java
@@ -0,0 +1,69 @@
+package de.tudarmstadt.ukp.dkpro.tc.features.ngram;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.misc.HighFreqTerms;
+import org.apache.lucene.misc.TermStats;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.descriptor.TypeCapability;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Level;
+
+import de.tudarmstadt.ukp.dkpro.tc.api.features.ClassificationUnitFeatureExtractor;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.meta.MetaCollector;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.meta.MetaDependent;
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.meta.LuceneNGramMetaCollector;
+
+@TypeCapability(inputs = { "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence",
+        "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token" })
+public class LuceneNGramFeatureExtractor
+    extends NGramFeatureExtractorBase
+    implements MetaDependent, ClassificationUnitFeatureExtractor
+{
+    
+    public static final String LUCENE_NGRAM_FIELD = "ngram";
+
+    public static final String PARAM_LUCENE_DIR = "luceneDir";
+    @ConfigurationParameter(name = PARAM_LUCENE_DIR, mandatory = true)
+    private File luceneDir;
+    
+    @Override
+    public List<Class<? extends MetaCollector>> getMetaCollectorClasses()
+    {
+        List<Class<? extends MetaCollector>> metaCollectorClasses = new ArrayList<Class<? extends MetaCollector>>();
+        metaCollectorClasses.add(LuceneNGramMetaCollector.class);
+
+        return metaCollectorClasses;
+    }
+
+    @Override
+    protected Set<String> getTopNgrams()
+        throws ResourceInitializationException
+    {       
+
+        Set<String> topNGrams = new HashSet<String>();
+
+        IndexReader reader;
+        try {
+            reader = DirectoryReader.open(FSDirectory.open(luceneDir));
+            for (TermStats termStat : HighFreqTerms.getHighFreqTerms(reader, ngramUseTopK, LUCENE_NGRAM_FIELD)) {
+                topNGrams.add(termStat.termtext.utf8ToString());
+            }
+
+        }
+        catch (Exception e) {
+            throw new ResourceInitializationException(e);
+        }
+        
+        getLogger().log(Level.INFO, "+++ TAKING " + topNGrams.size() + " NGRAMS");
+
+        return topNGrams;
+    }
+}
