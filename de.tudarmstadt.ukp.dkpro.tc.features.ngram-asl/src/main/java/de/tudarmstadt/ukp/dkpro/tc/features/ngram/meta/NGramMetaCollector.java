f4d506024210bbb034e8d0fdf31a9e6d222c9f93
Torsten Zesch <torsten.zesch@gmail.com>
2014-01-19 17:38:51 +0000

    Read stopwords once in initialize() instead of for each document in process()

--- de.tudarmstadt.ukp.dkpro.tc.features.ngram-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/meta/NGramMetaCollector.java
+++ de.tudarmstadt.ukp.dkpro.tc.features.ngram-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/meta/NGramMetaCollector.java
@@ -9,9 +9,11 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
 
 import de.tudarmstadt.ukp.dkpro.core.api.frequency.util.FrequencyDistribution;
 import de.tudarmstadt.ukp.dkpro.core.api.resources.ResourceUtils;
@@ -38,24 +40,35 @@ public class NGramMetaCollector
     @ConfigurationParameter(name = NGramFeatureExtractor.PARAM_NGRAM_LOWER_CASE, mandatory = false)
     private boolean ngramLowerCase = true;
 
+    private  Set<String> stopwords;
+    @Override
+    public void initialize(UimaContext context)
+        throws ResourceInitializationException
+    {
+        super.initialize(context);
+        
+        stopwords = new HashSet<String>();
+
+        if (ngramStopwordsFile != null && !ngramStopwordsFile.isEmpty()) {
+            try {
+                URL stopUrl = ResourceUtils.resolveLocation(ngramStopwordsFile, null);
+                InputStream is = stopUrl.openStream();
+                stopwords.addAll(IOUtils.readLines(is, "UTF-8"));
+            }
+            catch (Exception e) {
+                throw new ResourceInitializationException(e);
+            }
+        }
+    }
+    
     @Override
     public void process(JCas jcas)
         throws AnalysisEngineProcessException
     {
         FrequencyDistribution<String> documentNGrams = null;
-        if (ngramStopwordsFile != null && !ngramStopwordsFile.isEmpty()) {
-            try {
-                URL stopUrl = ResourceUtils.resolveLocation(ngramStopwordsFile, null);
-                InputStream is = stopUrl.openStream();
-                Set<String> stopwords = new HashSet<String>();
-                stopwords.addAll(IOUtils.readLines(is, "UTF-8"));
-                documentNGrams = NGramUtils.getDocumentNgrams(jcas, ngramLowerCase, ngramMinN,
+        if (stopwords.size() > 0) {
+            documentNGrams = NGramUtils.getDocumentNgrams(jcas, ngramLowerCase, ngramMinN,
                         ngramMaxN, stopwords);
-
-            }
-            catch (Exception e) {
-                throw new AnalysisEngineProcessException(e);
-            }
         }
         else {
             documentNGrams = NGramUtils.getDocumentNgrams(jcas, ngramLowerCase, ngramMinN,
