7c7e70ba95e41679526243d96e39a35cbbfef41e
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-02-12 18:02:23 +0000

    Better test.

--- trunk/de.tudarmstadt.ukp.dkpro.tc.features.pair.core-asl/src/test/java/de/tudarmstadt/ukp/dkpro/tc/features/pair/core/ngram/PairNgramFeatureExtractorTest.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.features.pair.core-asl/src/test/java/de/tudarmstadt/ukp/dkpro/tc/features/pair/core/ngram/PairNgramFeatureExtractorTest.java
@@ -1,31 +1,31 @@
 package de.tudarmstadt.ukp.dkpro.tc.features.pair.core.ngram;
 
-import java.io.BufferedReader;
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
-import org.apache.uima.fit.pipeline.JCasIterable;
-import org.apache.uima.jcas.JCas;
+import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import com.google.gson.Gson;
+
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureStore;
+import de.tudarmstadt.ukp.dkpro.tc.core.io.JsonDataWriter;
 import de.tudarmstadt.ukp.dkpro.tc.core.util.TaskUtils;
 import de.tudarmstadt.ukp.dkpro.tc.features.pair.core.ngram.meta.LuceneNGramPairMetaCollector;
 import de.tudarmstadt.ukp.dkpro.tc.features.pair.core.ngram.meta.TestPairReader;
-import de.tudarmstadt.ukp.dkpro.tc.weka.writer.WekaDataWriter;
+import de.tudarmstadt.ukp.dkpro.tc.fstore.simple.SimpleFeatureStore;
 
 
 public class PairNgramFeatureExtractorTest
@@ -34,7 +34,7 @@ public class PairNgramFeatureExtractorTest
     public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void pairNgramFullPipelineTest()
+    public void giveMeBetterName()
             throws Exception
     {
         File lucenePath = folder.newFolder();
@@ -43,9 +43,11 @@ public class PairNgramFeatureExtractorTest
         Object[] parameters = new Object[] {
                 LuceneNGramPairFeatureExtractor.PARAM_NGRAM_MIN_N_VIEW1, 1,
                 LuceneNGramPairFeatureExtractor.PARAM_NGRAM_MIN_N_VIEW2, 1,
-                LuceneNGramPairFeatureExtractor.PARAM_USE_VIEW1_NGRAMS_AS_FEATURES, "true",
-                LuceneNGramPairFeatureExtractor.PARAM_USE_VIEW2_NGRAMS_AS_FEATURES, "true",
-                LuceneNGramPairFeatureExtractor.PARAM_USE_VIEWBLIND_NGRAMS_AS_FEATURES, "true",
+                LuceneNGramPairFeatureExtractor.PARAM_NGRAM_MAX_N_VIEW1, 3,
+                LuceneNGramPairFeatureExtractor.PARAM_NGRAM_MAX_N_VIEW2, 3,
+                LuceneNGramPairFeatureExtractor.PARAM_USE_VIEW1_NGRAMS_AS_FEATURES, true,
+                LuceneNGramPairFeatureExtractor.PARAM_USE_VIEW2_NGRAMS_AS_FEATURES, true,
+                LuceneNGramPairFeatureExtractor.PARAM_USE_VIEWBLIND_NGRAMS_AS_FEATURES, true,
                 LuceneNGramPairFeatureExtractor.PARAM_LUCENE_DIR, lucenePath
         };
         List<Object> parameterList = new ArrayList<Object>(Arrays.asList(parameters));
@@ -65,35 +67,23 @@ public class PairNgramFeatureExtractorTest
         AnalysisEngineDescription featExtractorConnector = TaskUtils.getFeatureExtractorConnector(
                 parameterList,
                 outputPath.getAbsolutePath(),
-                WekaDataWriter.class.getName(),
+                JsonDataWriter.class.getName(),
                 false,
                 false,
                 LuceneNGramPairFeatureExtractor.class.getName()
         );
 
         // run meta collector
-        for (JCas jcas : new JCasIterable(reader, segmenter, metaCollector)) {
-        }
-        
-        // run FE
-        for (JCas jcas : new JCasIterable(reader, segmenter, featExtractorConnector)) {
-        }
-        
-        for (File file : FileUtils.listFiles(outputPath, new String[] { "arff.gz"}, true)) {
-            System.out.println(getGzippedContents(file));
-        }
-    }
-    
-    private static String getGzippedContents(File file) 
-            throws IOException
-    {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(file)), "UTF-8"));
-        String inputLine;
-        while ((inputLine = reader.readLine()) != null) {
-            sb.append(inputLine + "\n");
-        }
-        reader.close();
-        return sb.toString();
+        SimplePipeline.runPipeline(reader, segmenter, metaCollector);
+
+        // run FE(s)
+        SimplePipeline.runPipeline(reader, segmenter, featExtractorConnector);
+
+        Gson gson = new Gson();
+        FeatureStore fs = gson.fromJson(FileUtils.readFileToString(new File(outputPath, JsonDataWriter.JSON_FILE_NAME)), SimpleFeatureStore.class);
+        assertEquals(1, fs.getNumberOfInstances());
+        assertEquals(1, fs.getUniqueOutcomes().size());
+
+        System.out.println(FileUtils.readFileToString(new File(outputPath, JsonDataWriter.JSON_FILE_NAME)));
     }
 }
