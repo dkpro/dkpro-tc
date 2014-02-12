64df541f0ddd6a2e4057e996b28acc6b3f7e8f16
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-02-12 17:20:32 +0000

    Added DumpDataWriter and JsonDataWriter
    Fixes issue 90

---
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/io/DumpDataWriter.java
@@ -0,0 +1,40 @@
+package de.tudarmstadt.ukp.dkpro.tc.core.io;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+
+import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureStore;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.Instance;
+import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
+
+/**
+ * Dumps the feature store to a human readable file and also to the console.
+ * Mainly used for testing purposes.
+ * 
+ * @author zesch
+ *
+ */
+public class DumpDataWriter
+    implements DataWriter, Constants
+{
+        
+    public static final String DUMP_FILE_NAME = "fs.ser";
+    
+    @Override
+    public void write(File outputDirectory, FeatureStore featureStore, boolean useDenseInstances, boolean isRegressionExperiment)
+            throws Exception
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append(featureStore.getNumberOfInstances());
+        sb.append("\n");
+        sb.append(StringUtils.join(featureStore.getUniqueOutcomes(), ", "));
+        sb.append("\n");
+        for (Instance instance : featureStore.getInstances()) {
+            sb.append(instance.toString());
+        }
+        System.out.println(sb.toString());
+        FileUtils.writeStringToFile(new File(outputDirectory, DUMP_FILE_NAME), sb.toString());
+    }
+}
---
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.core-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/io/JsonDataWriter.java
@@ -0,0 +1,32 @@
+package de.tudarmstadt.ukp.dkpro.tc.core.io;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+
+import com.google.gson.Gson;
+
+import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureStore;
+import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
+
+/**
+ * Writes the feature store to a JSON file.
+ * Mainly used for testing purposes.
+ * 
+ * @author zesch
+ *
+ */
+public class JsonDataWriter
+    implements DataWriter, Constants
+{
+    public static final String JSON_FILE_NAME = "fs.json";
+        
+    private Gson gson = new Gson();
+    
+    @Override
+    public void write(File outputDirectory, FeatureStore featureStore, boolean useDenseInstances, boolean isRegressionExperiment)
+            throws Exception
+    {
+        FileUtils.writeStringToFile(new File(outputDirectory, JSON_FILE_NAME), gson.toJson(featureStore));
+    }
+}
