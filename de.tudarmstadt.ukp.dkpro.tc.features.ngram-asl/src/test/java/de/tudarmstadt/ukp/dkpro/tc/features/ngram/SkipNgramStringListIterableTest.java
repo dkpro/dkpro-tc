d86ba08e6e460c3bd469a97d3029d7ef1bef1558
Emily Jamison <EmilyKJamison@gmail.com>
2014-02-17 13:35:30 +0000

    Added test to demonstrate that maybe skip ngrams should have a skip size limit.
    Update issue 89
    Status: Started
    
    Thanks for the new algorithm.
    I noticed the new algorithm doesn't limit the size of the skip, other than the size of the Sentence.  This means that skip bigrams from a sentence with 40 tokens will include a bigram with token 1 and token 40.
    
    I see two problems with this approach: 
    1. It will generate an enormous number of skip ngrams.
    2. This isn't how I understand skip ngrams to be implemented in literature such as Guthrie et al (2006), who impose an upper limit on the number of tokens skipped.
    
    I am committing a unit test to illustrate this issue.

--- de.tudarmstadt.ukp.dkpro.tc.features.ngram-asl/src/test/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/SkipNgramStringListIterableTest.java
+++ de.tudarmstadt.ukp.dkpro.tc.features.ngram-asl/src/test/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/SkipNgramStringListIterableTest.java
@@ -3,6 +3,7 @@ package de.tudarmstadt.ukp.dkpro.tc.features.ngram;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -70,5 +71,22 @@ public class SkipNgramStringListIterableTest
             i++;
         }
         assertEquals(20, i);
+    }
+    @Test
+    public void ngramTest_size2_tooBig(){
+
+        String[] tokens = "A B C D E F G H I J K L M N O P".split(" ");
+        
+        for (List<String> ngram : new SkipNgramStringListIterable(tokens, 2, 2)) {
+        	List<String> aAndP = new ArrayList<String>();
+        	aAndP.add("A");
+        	aAndP.add("P");
+        	if(ngram.equals(aAndP)){
+        		System.out.println(ngram);
+        		System.out.println("Tokens = " + StringUtils.join(tokens));
+        		System.out.println("Do we really want A and P together in an ngram?");
+        		System.out.println("Guthrie et al provide an adjustable limit of 1- or 2-skips.");
+        	}
+        }
     }
 }
