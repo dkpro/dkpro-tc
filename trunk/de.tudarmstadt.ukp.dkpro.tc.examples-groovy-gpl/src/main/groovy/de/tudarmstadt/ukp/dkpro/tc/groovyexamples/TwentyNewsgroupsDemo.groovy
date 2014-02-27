8a8f675de33cbe9f2cf6aac3140ed5e66bb08aaa
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-02-27 20:09:04 +0000

    Issue 88: Own modules for Java and Groovy example projects.

---
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.examples-groovy-gpl/src/main/groovy/de/tudarmstadt/ukp/dkpro/tc/groovyexamples/TwentyNewsgroupsDemo.groovy
@@ -0,0 +1,209 @@
+package de.tudarmstadt.ukp.dkpro.tc.groovyexamples;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription
+import org.apache.uima.resource.ResourceInitializationException
+
+import weka.classifiers.bayes.NaiveBayes
+import weka.classifiers.functions.SMO
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter
+import de.tudarmstadt.ukp.dkpro.lab.Lab
+import de.tudarmstadt.ukp.dkpro.lab.task.Dimension
+import de.tudarmstadt.ukp.dkpro.lab.task.impl.BatchTask.ExecutionPolicy
+import de.tudarmstadt.ukp.dkpro.tc.core.Constants
+import de.tudarmstadt.ukp.dkpro.tc.examples.io.TwentyNewsgroupsCorpusReader
+import de.tudarmstadt.ukp.dkpro.tc.features.length.NrOfTokensDFE
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.LuceneNGramDFE
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchCrossValidationReport
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchOutcomeIDReport
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchTrainTestReport
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.ClassificationReport
+import de.tudarmstadt.ukp.dkpro.tc.weka.task.BatchTaskCrossValidation
+import de.tudarmstadt.ukp.dkpro.tc.weka.task.BatchTaskTrainTest
+import de.tudarmstadt.ukp.dkpro.tc.weka.writer.WekaDataWriter
+
+/**
+ * Groovy-Version of the TwentyNewsgroupsExperiment
+ *
+ * The TwentyNewsgroupsGroovyExperiment does the same as TwentyNewsgroupsGroovyExtendedExperiment,
+ * but it uses the {@link BatchTaskCrossValidation} and {@link BatchTaskTrainTest} to automatically wire the standard tasks for
+ * a basic CV and TrainTest setup. This is more convenient, but less flexible.
+ *
+ * If you need to define a more complex experiment setup, look at TwentyNewsgroupsGroovyExtendedExperiment
+ *
+ * @author Oliver Ferschke
+ */
+public class TwentyNewsgroupsDemo implements Constants {
+
+    // === PARAMETERS===========================================================
+
+    def experimentName = "TwentyNewsgroups";
+    def corpusFilePathTrain = "src/main/resources/data/twentynewsgroups/bydate-train";
+    def corpusFilePathTest  ="src/main/resources/data/twentynewsgroups/bydate-test";
+    def languageCode = "en";
+    def numFolds = 2;
+
+    // === DIMENSIONS===========================================================
+
+    def dimReaders = Dimension.createBundle("readers", [
+        readerTest: TwentyNewsgroupsCorpusReader.class,
+        readerTestParams: [
+            TwentyNewsgroupsCorpusReader.PARAM_SOURCE_LOCATION,
+            corpusFilePathTest,
+            TwentyNewsgroupsCorpusReader.PARAM_LANGUAGE,
+            languageCode,
+            TwentyNewsgroupsCorpusReader.PARAM_PATTERNS,
+            TwentyNewsgroupsCorpusReader.INCLUDE_PREFIX + "*/*.txt"
+        ],
+        readerTrain: TwentyNewsgroupsCorpusReader.class,
+        readerTrainParams: [
+            TwentyNewsgroupsCorpusReader.PARAM_SOURCE_LOCATION,
+            corpusFilePathTrain,
+            TwentyNewsgroupsCorpusReader.PARAM_LANGUAGE,
+            languageCode,
+            TwentyNewsgroupsCorpusReader.PARAM_PATTERNS,
+            TwentyNewsgroupsCorpusReader.INCLUDE_PREFIX + "*/*.txt"
+        ]
+    ]);
+
+    def dimLearningMode = Dimension.create(DIM_LEARNING_MODE, LM_SINGLE_LABEL);
+    def dimFeatureMode = Dimension.create(DIM_FEATURE_MODE, FM_DOCUMENT);
+    def dimDataWriter = Dimension.create(DIM_DATA_WRITER, WekaDataWriter.class.name);
+
+    def dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
+    [NaiveBayes.class.name],
+    [SMO.class.name]);
+
+    def dimFeatureSets = Dimension.create(
+    DIM_FEATURE_SET,
+    [
+        NrOfTokensDFE.class.name,
+        LuceneNGramDFE.class.name
+        //        NGramFeatureExtractor.class.name
+
+    ]
+    );
+
+    //    def dimPipelineParameters = Dimension.create(
+    //    DIM_PIPELINE_PARAMS,
+    //    [
+    //        NGramFeatureExtractor.PARAM_NGRAM_USE_TOP_K,
+    //        "50",
+    //        NGramFeatureExtractor.PARAM_NGRAM_MIN_N,
+    //        1,
+    //        NGramFeatureExtractor.PARAM_NGRAM_MAX_N,
+    //        3
+    //    ],
+    //    [
+    //        NGramFeatureExtractor.PARAM_NGRAM_USE_TOP_K,
+    //        "100",
+    //        NGramFeatureExtractor.PARAM_NGRAM_MIN_N,
+    //        1,
+    //        NGramFeatureExtractor.PARAM_NGRAM_MAX_N,
+    //        3
+    //    ]
+    //    );
+
+    def dimPipelineParameters = Dimension.create(
+    DIM_PIPELINE_PARAMS,
+    [
+        LuceneNGramDFE.PARAM_NGRAM_USE_TOP_K,
+        "50",
+        LuceneNGramDFE.PARAM_NGRAM_MIN_N,
+        1,
+        LuceneNGramDFE.PARAM_NGRAM_MAX_N,
+        3
+    ],
+    [
+        LuceneNGramDFE.PARAM_NGRAM_USE_TOP_K,
+        "100",
+        LuceneNGramDFE.PARAM_NGRAM_MIN_N,
+        1,
+        LuceneNGramDFE.PARAM_NGRAM_MAX_N,
+        3
+    ]
+    );
+
+    // === Experiments =========================================================
+
+    /**
+     * Crossvalidation setting
+     *
+     * @throws Exception
+     */
+    protected void runCrossValidation() throws Exception
+    {
+
+        BatchTaskCrossValidation batchTask = [
+            experimentName: experimentName + "-CV-Groovy",
+            // we need to explicitly set the name of the batch task, as the constructor of the groovy setup must be zero-arg
+            type: "Evaluation-"+ experimentName +"-CV-Groovy",
+            aggregate:	getPreprocessing(),
+            innerReport: ClassificationReport.class,
+            parameterSpace : [
+                dimReaders,
+                dimLearningMode,
+                dimFeatureMode,
+                dimDataWriter,
+                dimClassificationArgs,
+                dimFeatureSets,
+                dimPipelineParameters
+            ],
+            executionPolicy: ExecutionPolicy.RUN_AGAIN,
+            reports:         [BatchCrossValidationReport],
+            numFolds: numFolds];
+
+        Lab.getInstance().run(batchTask);
+    }
+    /**
+     * TrainTest Setting
+     *
+     * @throws Exception
+     */
+    protected void runTrainTest() throws Exception
+    {
+
+        BatchTaskTrainTest batchTask = [
+            experimentName: experimentName + "-TrainTest-Groovy",
+            // we need to explicitly set the name of the batch task, as the constructor of the groovy setup must be zero-arg
+            type: "Evaluation-"+ experimentName +"-TrainTest-Groovy",
+            aggregate:	getPreprocessing(),
+            innerReport: ClassificationReport.class,
+            parameterSpace : [
+                dimReaders,
+                dimLearningMode,
+                dimFeatureMode,
+                dimDataWriter,
+                dimClassificationArgs,
+                dimFeatureSets,
+                dimPipelineParameters
+            ],
+            executionPolicy: ExecutionPolicy.RUN_AGAIN,
+            reports:         [
+                BatchTrainTestReport,
+                BatchOutcomeIDReport]
+        ];
+
+        // Run
+        Lab.getInstance().run(batchTask);
+    }
+
+    private AnalysisEngineDescription getPreprocessing()
+    throws ResourceInitializationException
+    {
+        return createEngineDescription(
+        createEngineDescription(BreakIteratorSegmenter.class),
+        createEngineDescription(OpenNlpPosTagger.class)
+        );
+    }
+
+    public static void main(String[] args)
+    {
+        new TwentyNewsgroupsDemo().runTrainTest();
+        new TwentyNewsgroupsDemo().runCrossValidation();
+    }
+
+}
---
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.examples-groovy-gpl/src/test/groovy/de/tudarmstadt/ukp/dkpro/tc/groovyexamples/TwentyNewsgroupsDemoTest.java
@@ -0,0 +1,66 @@
+package de.tudarmstadt.ukp.dkpro.tc.groovyexamples;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+/**
+ * This is not exactly a unit test (yet). It just ensures that the experiments run without throwing
+ * any exception. Additional unit tests should test the inner workings of the experiments
+ * 
+ * @author Oliver Ferschke
+ * 
+ */
+public class TwentyNewsgroupsDemoTest
+{
+    TwentyNewsgroupsDemo groovyExperiment;
+    TwentyNewsgroupsDemoExtended groovyExtendedExperiment;
+
+    @Before
+    public void setup()
+        throws Exception
+    {
+        String path = "target/repository/" + getClass().getSimpleName() + "/"
+                + name.getMethodName();
+        System.setProperty("DKPRO_HOME", new File(path).getAbsolutePath());
+
+        groovyExperiment = new TwentyNewsgroupsDemo();
+        groovyExtendedExperiment = new TwentyNewsgroupsDemoExtended();
+    }
+
+    @Test
+    public void testGroovyCrossValidation()
+        throws Exception
+    {
+        // Groovy setup with automatic task wiring
+        groovyExperiment.runCrossValidation();
+    }
+
+    @Test
+    public void testGroovyTrainTest()
+        throws Exception
+    {
+        // Groovy setup with automatic task wiring
+        groovyExperiment.runTrainTest();
+    }
+
+    @Test
+    public void testGroovyExtendedTrainTest()
+        throws Exception
+    {
+        // Groovy setup with manual task wiring
+        groovyExtendedExperiment.runTrainTest();
+    }
+
+    @Rule
+    public TestName name = new TestName();
+
+    @Before
+    public void printSeparator()
+    {
+        System.out.println("\n=== " + name.getMethodName() + " =====================");
+    }
+}
