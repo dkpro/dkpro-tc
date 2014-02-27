36b04982fb80b3cd12869bde462821226bcd2f49
Torsten Zesch <torsten.zesch@gmail.com>
2014-02-27 20:09:04 +0000

    Issue 88: Own modules for Java and Groovy example projects.

---
+++ de.tudarmstadt.ukp.dkpro.tc.examples-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/examples/TwentyNewsgroupsDemo.java
@@ -0,0 +1,156 @@
+package de.tudarmstadt.ukp.dkpro.tc.examples;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.SMO;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.tudarmstadt.ukp.dkpro.lab.Lab;
+import de.tudarmstadt.ukp.dkpro.lab.task.Dimension;
+import de.tudarmstadt.ukp.dkpro.lab.task.ParameterSpace;
+import de.tudarmstadt.ukp.dkpro.lab.task.impl.BatchTask.ExecutionPolicy;
+import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
+import de.tudarmstadt.ukp.dkpro.tc.examples.io.TwentyNewsgroupsCorpusReader;
+import de.tudarmstadt.ukp.dkpro.tc.features.length.NrOfTokensDFE;
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.FrequencyDistributionNGramDFE;
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.base.FrequencyDistributionNGramFeatureExtractorBase;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchCrossValidationReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchOutcomeIDReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchTrainTestReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.ClassificationReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.task.BatchTaskCrossValidation;
+import de.tudarmstadt.ukp.dkpro.tc.weka.task.BatchTaskTrainTest;
+import de.tudarmstadt.ukp.dkpro.tc.weka.writer.WekaDataWriter;
+
+/**
+ * This a pure Java-based experiment setup of the TwentyNewsgroupsExperiment.
+ * 
+ * Defining the parameters directly in this class makes on-the-fly changes more difficult when the
+ * experiment is run on a server.
+ * 
+ * For these cases, the self-sufficient Groovy versions are more suitable, since their source code
+ * can be changed and then executed without pre-compilation.
+ */
+public class TwentyNewsgroupsDemo
+    implements Constants
+{
+    public static final String LANGUAGE_CODE = "en";
+
+    public static int NUM_FOLDS = 3;
+
+    public static final String corpusFilePathTrain = "src/main/resources/data/twentynewsgroups/bydate-train";
+    public static final String corpusFilePathTest = "src/main/resources/data/twentynewsgroups/bydate-test";
+
+    public static void main(String[] args)
+        throws Exception
+    {
+        ParameterSpace pSpace = getParameterSpace();
+
+        TwentyNewsgroupsDemo experiment = new TwentyNewsgroupsDemo();
+        experiment.runCrossValidation(pSpace);
+        experiment.runTrainTest(pSpace);
+    }
+
+    public static ParameterSpace getParameterSpace()
+    {
+        // configure training and test data reader dimension
+        Map<String, Object> dimReaders = new HashMap<String, Object>();
+        dimReaders.put(DIM_READER_TRAIN, TwentyNewsgroupsCorpusReader.class);
+        dimReaders.put(
+                DIM_READER_TRAIN_PARAMS,
+                Arrays.asList(new Object[] { TwentyNewsgroupsCorpusReader.PARAM_SOURCE_LOCATION,
+                        corpusFilePathTrain, TwentyNewsgroupsCorpusReader.PARAM_LANGUAGE,
+                        LANGUAGE_CODE, TwentyNewsgroupsCorpusReader.PARAM_PATTERNS,
+                        TwentyNewsgroupsCorpusReader.INCLUDE_PREFIX + "*/*.txt" }));
+        dimReaders.put(DIM_READER_TEST, TwentyNewsgroupsCorpusReader.class);
+        dimReaders.put(
+                DIM_READER_TEST_PARAMS,
+                Arrays.asList(new Object[] { TwentyNewsgroupsCorpusReader.PARAM_SOURCE_LOCATION,
+                        corpusFilePathTest, TwentyNewsgroupsCorpusReader.PARAM_LANGUAGE,
+                        LANGUAGE_CODE, TwentyNewsgroupsCorpusReader.PARAM_PATTERNS,
+                        TwentyNewsgroupsCorpusReader.INCLUDE_PREFIX + "*/*.txt" }));
+
+        @SuppressWarnings("unchecked")
+        Dimension<List<String>> dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
+                Arrays.asList(new String[] { SMO.class.getName() }),
+                Arrays.asList(new String[] { NaiveBayes.class.getName() }));
+
+        @SuppressWarnings("unchecked")
+        Dimension<List<Object>> dimPipelineParameters = Dimension.create(
+                DIM_PIPELINE_PARAMS,
+                Arrays.asList(new Object[] {
+                        FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_USE_TOP_K,
+                        "500", FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MIN_N, 1,
+                        FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MAX_N, 3 }),
+                Arrays.asList(new Object[] {
+                        FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_USE_TOP_K,
+                        "1000", FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MIN_N,
+                        1, FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MAX_N, 3 }));
+
+        @SuppressWarnings("unchecked")
+        Dimension<List<String>> dimFeatureSets = Dimension.create(
+                DIM_FEATURE_SET,
+                Arrays.asList(new String[] { NrOfTokensDFE.class.getName(),
+                        FrequencyDistributionNGramDFE.class.getName() }));
+
+        ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle("readers", dimReaders),
+                Dimension.create(DIM_DATA_WRITER, WekaDataWriter.class.getName()),
+                Dimension.create(DIM_LEARNING_MODE, LM_SINGLE_LABEL), Dimension.create(
+                        DIM_FEATURE_MODE, FM_DOCUMENT), dimPipelineParameters, dimFeatureSets,
+                dimClassificationArgs);
+
+        return pSpace;
+    }
+
+    // ##### CV #####
+    protected void runCrossValidation(ParameterSpace pSpace)
+        throws Exception
+    {
+
+        BatchTaskCrossValidation batch = new BatchTaskCrossValidation("TwentyNewsgroupsCV",
+                getPreprocessing(), NUM_FOLDS);
+        batch.setInnerReport(ClassificationReport.class);
+        batch.setParameterSpace(pSpace);
+        batch.setExecutionPolicy(ExecutionPolicy.RUN_AGAIN);
+        batch.addReport(BatchCrossValidationReport.class);
+
+        // Run
+        Lab.getInstance().run(batch);
+    }
+
+    // ##### TRAIN-TEST #####
+    protected void runTrainTest(ParameterSpace pSpace)
+        throws Exception
+    {
+
+        BatchTaskTrainTest batch = new BatchTaskTrainTest("TwentyNewsgroupsTrainTest",
+                getPreprocessing());
+        batch.setInnerReport(ClassificationReport.class);
+        batch.setParameterSpace(pSpace);
+        batch.setExecutionPolicy(ExecutionPolicy.RUN_AGAIN);
+        batch.addReport(BatchTrainTestReport.class);
+        batch.addReport(BatchOutcomeIDReport.class);
+
+        // Run
+        Lab.getInstance().run(batch);
+    }
+
+    protected AnalysisEngineDescription getPreprocessing()
+        throws ResourceInitializationException
+    {
+
+        return createEngineDescription(
+                createEngineDescription(BreakIteratorSegmenter.class),
+                createEngineDescription(OpenNlpPosTagger.class, OpenNlpPosTagger.PARAM_LANGUAGE,
+                        LANGUAGE_CODE));
+    }
+}
---
+++ de.tudarmstadt.ukp.dkpro.tc.examples-gpl/src/test/java/de/tudarmstadt/ukp/dkpro/tc/examples/TwentyNewsgroupsDemoTest.java
@@ -0,0 +1,60 @@
+package de.tudarmstadt.ukp.dkpro.tc.examples;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import de.tudarmstadt.ukp.dkpro.lab.task.ParameterSpace;
+
+/**
+ * This is not exactly a unit test (yet). It just ensures that the experiments run without throwing
+ * any exception. Additional unit tests should test the inner workings of the experiments
+ * 
+ * @author Oliver Ferschke
+ * 
+ */
+public class TwentyNewsgroupsDemoTest
+{
+    TwentyNewsgroupsDemo javaExperiment;
+    ParameterSpace pSpace;
+
+    @Before
+    public void setup()
+        throws Exception
+    {
+        String path = "target/repository/" + getClass().getSimpleName() + "/"
+                + name.getMethodName();
+        System.setProperty("DKPRO_HOME", new File(path).getAbsolutePath());
+
+        javaExperiment = new TwentyNewsgroupsDemo();
+        pSpace = TwentyNewsgroupsDemo.getParameterSpace();
+    }
+
+    @Test
+    public void testJavaCrossValidation()
+        throws Exception
+    {
+        // Java setup with automatic task wiring
+        javaExperiment.runCrossValidation(pSpace);
+    }
+
+    @Test
+    public void testJavaTrainTest()
+        throws Exception
+    {
+        // Java setup with automatic task wiring
+        javaExperiment.runTrainTest(pSpace);
+    }
+
+    @Rule
+    public TestName name = new TestName();
+
+    @Before
+    public void printSeparator()
+    {
+        System.out.println("\n=== " + name.getMethodName() + " =====================");
+    }
+}
