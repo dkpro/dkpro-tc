e3e1a0945e850eb94972c2554ce7a1e75359d244
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-03-04 09:42:32 +0000

    We should not need the other meta collector.

--- trunk/de.tudarmstadt.ukp.dkpro.tc.features.ngram-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/base/LucenePOSNGramFeatureExtractorBase.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.features.ngram-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/base/LucenePOSNGramFeatureExtractorBase.java
@@ -9,7 +9,6 @@ import org.apache.uima.fit.descriptor.TypeCapability;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.meta.MetaCollector;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.meta.MetaDependent;
 import de.tudarmstadt.ukp.dkpro.tc.features.ngram.meta.LucenePOSNGramMetaCollector;
-import de.tudarmstadt.ukp.dkpro.tc.features.ngram.meta.POSNGramMetaCollector;
 
 @TypeCapability(inputs = { "de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS" })
 public class LucenePOSNGramFeatureExtractorBase
@@ -39,8 +38,6 @@ public class LucenePOSNGramFeatureExtractorBase
     {
         List<Class<? extends MetaCollector>> metaCollectorClasses = new ArrayList<Class<? extends MetaCollector>>();
         metaCollectorClasses.add(LucenePOSNGramMetaCollector.class);
-        metaCollectorClasses.add(POSNGramMetaCollector.class);
-
         
         return metaCollectorClasses;
     }
