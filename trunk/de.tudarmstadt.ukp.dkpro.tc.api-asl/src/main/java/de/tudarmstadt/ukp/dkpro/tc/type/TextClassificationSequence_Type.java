d4d8eb3f020752bebfb7bfbf429963f00e3df4a9
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-03-22 14:34:34 +0000

    New annotation types.

--- trunk/de.tudarmstadt.ukp.dkpro.tc.api-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/type/TextClassificationSequence_Type.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.api-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/type/TextClassificationSequence_Type.java
@@ -1,20 +1,20 @@
 
-/* First created by JCasGen Wed Aug 28 16:30:21 CEST 2013 */
+/* First created by JCasGen Sat Mar 22 12:19:05 CET 2014 */
 package de.tudarmstadt.ukp.dkpro.tc.type;
 
-import org.apache.uima.cas.FeatureStructure;
-import org.apache.uima.cas.Type;
-import org.apache.uima.cas.impl.CASImpl;
-import org.apache.uima.cas.impl.FSGenerator;
-import org.apache.uima.cas.impl.TypeImpl;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
 import org.apache.uima.jcas.tcas.Annotation_Type;
 
 /** 
  * Updated by JCasGen Sat Mar 22 12:19:05 CET 2014
  * @generated */
-public class TextClassificationUnit_Type extends Annotation_Type {
+public class TextClassificationSequence_Type extends Annotation_Type {
   /** @generated */
   @Override
   protected FSGenerator getFSGenerator() {return fsGenerator;}
@@ -22,31 +22,31 @@ public class TextClassificationUnit_Type extends Annotation_Type {
   private final FSGenerator fsGenerator = 
     new FSGenerator() {
       public FeatureStructure createFS(int addr, CASImpl cas) {
-  			 if (TextClassificationUnit_Type.this.useExistingInstance) {
+  			 if (TextClassificationSequence_Type.this.useExistingInstance) {
   			   // Return eq fs instance if already created
-  		     FeatureStructure fs = TextClassificationUnit_Type.this.jcas.getJfsFromCaddr(addr);
+  		     FeatureStructure fs = TextClassificationSequence_Type.this.jcas.getJfsFromCaddr(addr);
   		     if (null == fs) {
-  		       fs = new TextClassificationUnit(addr, TextClassificationUnit_Type.this);
-  			   TextClassificationUnit_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  		       fs = new TextClassificationSequence(addr, TextClassificationSequence_Type.this);
+  			   TextClassificationSequence_Type.this.jcas.putJfsFromCaddr(addr, fs);
   			   return fs;
   		     }
   		     return fs;
-        } else return new TextClassificationUnit(addr, TextClassificationUnit_Type.this);
+        } else return new TextClassificationSequence(addr, TextClassificationSequence_Type.this);
   	  }
     };
   /** @generated */
   @SuppressWarnings ("hiding")
-  public final static int typeIndexID = TextClassificationUnit.typeIndexID;
+  public final static int typeIndexID = TextClassificationSequence.typeIndexID;
   /** @generated 
      @modifiable */
   @SuppressWarnings ("hiding")
-  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("de.tudarmstadt.ukp.dkpro.tc.type.TextClassificationUnit");
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("de.tudarmstadt.ukp.dkpro.tc.type.TextClassificationSequence");
 
 
 
   /** initialize variables to correspond with Cas Type and Features
 	* @generated */
-  public TextClassificationUnit_Type(JCas jcas, Type casType) {
+  public TextClassificationSequence_Type(JCas jcas, Type casType) {
     super(jcas, casType);
     casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
 
