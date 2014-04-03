d16e3f8a781380ce103a2c3f38d5f449f0f4e502
daxenberger@ukp.informatik.tu-darmstadt.de <daxenberger@ukp.informatik.tu-darmstadt.de@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-04-03 12:51:09 +0000

    Making the BrownUnitPosDemo independent from the BrownPosDemo (Sequence labeling), since they use different machine learning frameworks.

--- trunk/de.tudarmstadt.ukp.dkpro.tc.examples-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/examples/single/unit/BrownUnitPosDemo.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.examples-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/examples/single/unit/BrownUnitPosDemo.java
@@ -1,26 +1,108 @@
 package de.tudarmstadt.ukp.dkpro.tc.examples.single.unit;
 
+import static de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase.INCLUDE_PREFIX;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.fit.component.NoOpAnnotator;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.SMO;
+import de.tudarmstadt.ukp.dkpro.lab.Lab;
+import de.tudarmstadt.ukp.dkpro.lab.task.Dimension;
 import de.tudarmstadt.ukp.dkpro.lab.task.ParameterSpace;
+import de.tudarmstadt.ukp.dkpro.lab.task.impl.BatchTask.ExecutionPolicy;
 import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
-import de.tudarmstadt.ukp.dkpro.tc.examples.single.sequence.BrownPosDemo;
+import de.tudarmstadt.ukp.dkpro.tc.examples.io.BrownCorpusReader;
+import de.tudarmstadt.ukp.dkpro.tc.features.length.NrOfTokensUFE;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchCrossValidationReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.ClassificationReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.task.BatchTaskCrossValidation;
+import de.tudarmstadt.ukp.dkpro.tc.weka.writer.WekaDataWriter;
 
 /**
- * This is an example for POS tagging as unit classification.
- * Each POS is treated as a classification unit, but unlike sequence tagging the decision for each POS is taken independently.
- * This will usually give worse results, so this is only to showcase the concept.
+ * This is an example for POS tagging as unit classification. Each POS is treated as a
+ * classification unit, but unlike sequence tagging the decision for each POS is taken
+ * independently. This will usually give worse results, so this is only to showcase the concept.
  * 
- * We are re-using the sequence tagging setup.
  */
 public class BrownUnitPosDemo
-    extends BrownPosDemo
+    implements Constants
 {
+
+    public static final String LANGUAGE_CODE = "en";
+
+    public static int NUM_FOLDS = 2;
+
+    public static final String corpusFilePathTrain = "src/main/resources/data/brown_tei/";
 
     public static void main(String[] args)
         throws Exception
     {
-        ParameterSpace pSpace = getParameterSpace(Constants.FM_UNIT, Constants.LM_SINGLE_LABEL);
+        new BrownUnitPosDemo().runCrossValidation(getParameterSpace());
+    }
 
-        BrownUnitPosDemo experiment = new BrownUnitPosDemo();
-        experiment.runCrossValidation(pSpace);
+    // ##### CV #####
+    protected void runCrossValidation(ParameterSpace pSpace)
+        throws Exception
+    {
+
+        BatchTaskCrossValidation batch = new BatchTaskCrossValidation("BrownPosDemoCV",
+                getPreprocessing(), NUM_FOLDS);
+        batch.setInnerReport(ClassificationReport.class);
+        batch.setParameterSpace(pSpace);
+        batch.setExecutionPolicy(ExecutionPolicy.RUN_AGAIN);
+        batch.addReport(BatchCrossValidationReport.class);
+        // batch.addReport(BatchRuntimeReport.class);
+
+        // Run
+        Lab.getInstance().run(batch);
+    }
+
+    public static ParameterSpace getParameterSpace()
+    {
+        // configure training and test data reader dimension
+        Map<String, Object> dimReaders = new HashMap<String, Object>();
+        dimReaders.put(DIM_READER_TRAIN, BrownCorpusReader.class);
+        dimReaders.put(
+                DIM_READER_TRAIN_PARAMS,
+                Arrays.asList(new Object[] { BrownCorpusReader.PARAM_LANGUAGE, "en",
+                        BrownCorpusReader.PARAM_SOURCE_LOCATION, corpusFilePathTrain,
+                        BrownCorpusReader.PARAM_PATTERNS,
+                        new String[] { INCLUDE_PREFIX + "*.xml", INCLUDE_PREFIX + "*.xml.gz" } }));
+
+        @SuppressWarnings("unchecked")
+        Dimension<List<String>> dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
+                Arrays.asList(new String[] { SMO.class.getName() }),
+                Arrays.asList(new String[] { NaiveBayes.class.getName() }));
+
+        @SuppressWarnings("unchecked")
+        Dimension<List<Object>> dimPipelineParameters = Dimension.create(DIM_PIPELINE_PARAMS,
+                Arrays.asList(new Object[] { "something", "something" }),
+                Arrays.asList(new Object[] { "something2", "something2" }));
+
+        @SuppressWarnings("unchecked")
+        Dimension<List<String>> dimFeatureSets = Dimension.create(DIM_FEATURE_SET,
+                Arrays.asList(new String[] { NrOfTokensUFE.class.getName() }));
+
+        ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle("readers", dimReaders),
+                Dimension.create(DIM_DATA_WRITER, WekaDataWriter.class.getName()),
+                Dimension.create(DIM_LEARNING_MODE, LM_SINGLE_LABEL), Dimension.create(
+                        DIM_FEATURE_MODE, FM_UNIT), dimPipelineParameters, dimFeatureSets,
+                dimClassificationArgs);
+
+        return pSpace;
+    }
+
+    protected AnalysisEngineDescription getPreprocessing()
+        throws ResourceInitializationException
+    {
+        return createEngineDescription(createEngineDescription(NoOpAnnotator.class));
     }
 }
