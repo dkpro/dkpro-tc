1ab251d64cb54f993041b96217b01177d55d0657
daxenberger@ukp.informatik.tu-darmstadt.de <daxenberger@ukp.informatik.tu-darmstadt.de@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-05-27 08:41:56 +0000

    Missing values.

--- trunk/de.tudarmstadt.ukp.dkpro.tc.features-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/length/NrOfCharsUFE.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.features-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/length/NrOfCharsUFE.java
@@ -4,23 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.uima.fit.descriptor.TypeCapability;
-import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.tc.api.exception.TextClassificationException;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.ClassificationUnitFeatureExtractor;
-import de.tudarmstadt.ukp.dkpro.tc.api.features.DocumentFeatureExtractor;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
 import de.tudarmstadt.ukp.dkpro.tc.api.type.TextClassificationUnit;
 
 /**
- * Extracts the number of characters in the document, per sentence, and per token
+ * Extracts the number of characters in the unit (basically, its length).
  */
-@TypeCapability(inputs = { "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence",
-        "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token" })
+@TypeCapability(inputs = { "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token" })
 public class NrOfCharsUFE
     extends FeatureExtractorResource_ImplBase
     implements ClassificationUnitFeatureExtractor
--- trunk/de.tudarmstadt.ukp.dkpro.tc.features-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/style/InitialCharacterUpperCaseUFE.java
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.features-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/style/InitialCharacterUpperCaseUFE.java
@@ -3,21 +3,16 @@ package de.tudarmstadt.ukp.dkpro.tc.features.style;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.uima.fit.descriptor.TypeCapability;
-import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.tc.api.exception.TextClassificationException;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.ClassificationUnitFeatureExtractor;
-import de.tudarmstadt.ukp.dkpro.tc.api.features.DocumentFeatureExtractor;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
 import de.tudarmstadt.ukp.dkpro.tc.api.type.TextClassificationUnit;
 
 /**
- * Extracts information if the first character of the classification unit is uppercase or doesn't
+ * Extracts whether the first character of the classification unit is upper-case or not.
  * 
  * @author Andriy Nadolskyy
  * @author daxenberger
@@ -36,16 +31,15 @@ public class InitialCharacterUpperCaseUFE
         throws TextClassificationException
     {
         String token = classificationUnit.getCoveredText();
-        
+
         List<Feature> featList = new ArrayList<Feature>();
         boolean bool = false;
-        if (Character.isUpperCase(token.charAt(0))){
-        	featList.add(new Feature(INITIAL_CH_UPPER_CASE, !bool));
+        if (Character.isUpperCase(token.charAt(0))) {
+            featList.add(new Feature(INITIAL_CH_UPPER_CASE, !bool));
         }
-        else{
-        	featList.add(new Feature(INITIAL_CH_UPPER_CASE, bool));
+        else {
+            featList.add(new Feature(INITIAL_CH_UPPER_CASE, bool));
         }
-        		
         return featList;
     }
 }
