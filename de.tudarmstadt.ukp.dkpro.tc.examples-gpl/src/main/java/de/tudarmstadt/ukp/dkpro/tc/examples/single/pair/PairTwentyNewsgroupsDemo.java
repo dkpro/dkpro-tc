c1e35066560a7622a0e8e3e68969aae230c518c3
Johannes Daxenberger <daxenberger@ukp.informatik.tu-darmstadt.de>
2014-06-04 16:18:55 +0000

    Adding Java versions for demos already present as Groovy version.
    Update issue 88.

---
+++ de.tudarmstadt.ukp.dkpro.tc.examples-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/examples/single/pair/PairTwentyNewsgroupsDemo.java
@@ -0,0 +1,134 @@
+package de.tudarmstadt.ukp.dkpro.tc.examples.single.pair;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import weka.classifiers.functions.SMO;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordNamedEntityRecognizer;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
+import de.tudarmstadt.ukp.dkpro.lab.Lab;
+import de.tudarmstadt.ukp.dkpro.lab.task.Dimension;
+import de.tudarmstadt.ukp.dkpro.lab.task.ParameterSpace;
+import de.tudarmstadt.ukp.dkpro.lab.task.impl.BatchTask.ExecutionPolicy;
+import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
+import de.tudarmstadt.ukp.dkpro.tc.examples.io.PairTwentyNewsgroupsReader;
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.base.FrequencyDistributionNGramFeatureExtractorBase;
+import de.tudarmstadt.ukp.dkpro.tc.features.pair.core.length.DiffNrOfTokensPairFeatureExtractor;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchOutcomeIDReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchTrainTestReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.task.BatchTaskTrainTest;
+import de.tudarmstadt.ukp.dkpro.tc.weka.writer.WekaDataWriter;
+
+/**
+ * PairTwentyNewsgroupsExperiment, using Java
+ * 
+ * The PairTwentyNewsgroupsExperiment takes pairs of news files and trains/tests a binary classifier
+ * to learn if the files in the pair are from the same newsgroup. The pairs are listed in a tsv
+ * file: see the files in src/main/resources/lists/ as examples.
+ * <p>
+ * PairTwentyNewsgroupsExperiment uses similar architecture as TwentyNewsgroupsGroovyExperiment (
+ * {@link BatchTaskTrainTest}) to automatically wire the standard tasks for a basic TrainTest setup.
+ * To remind the user to be careful of information leak when training and testing on pairs of data
+ * from similar sources, we do not provide a demo Cross Validation setup here. (Our sample train and
+ * test datasets are from separate newsgroups.) Please see TwentyNewsgroupsGroovyExperiment for a
+ * demo implementing a CV experiment.
+ * 
+ * 
+ * @author Emily Jamison
+ */
+public class PairTwentyNewsgroupsDemo
+    implements Constants
+{
+
+    public static final String experimentName = "PairTwentyNewsgroupsExperiment";
+    public static final String languageCode = "en";
+    public static final String listFilePathTrain = "src/main/resources/data/twentynewsgroups/pairs/pairslist.train";
+    public static final String listFilePathTest = "src/main/resources/data/twentynewsgroups/pairs/pairslist.test";
+
+    public static void main(String[] args)
+        throws Exception
+    {
+        ParameterSpace pSpace = getParameterSpace();
+        PairTwentyNewsgroupsDemo experiment = new PairTwentyNewsgroupsDemo();
+        experiment.runTrainTest(pSpace);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static ParameterSpace getParameterSpace()
+    {
+        // configure training and test data reader dimension
+        // train/test will use both, while cross-validation will only use the train part
+        Map<String, Object> dimReaders = new HashMap<String, Object>();
+        dimReaders.put(DIM_READER_TRAIN, PairTwentyNewsgroupsReader.class);
+        dimReaders.put(DIM_READER_TRAIN_PARAMS,
+                Arrays.asList(PairTwentyNewsgroupsReader.PARAM_LISTFILE,
+                        listFilePathTrain,
+                        PairTwentyNewsgroupsReader.PARAM_LANGUAGE_CODE,
+                        languageCode));
+        dimReaders.put(DIM_READER_TEST, PairTwentyNewsgroupsReader.class);
+        dimReaders.put(DIM_READER_TEST_PARAMS,
+                Arrays.asList(PairTwentyNewsgroupsReader.PARAM_LISTFILE,
+                        listFilePathTest,
+                        PairTwentyNewsgroupsReader.PARAM_LANGUAGE_CODE,
+                        languageCode));
+
+        Dimension<List<String>> dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
+                Arrays.asList(new String[] { SMO.class.getName() }));
+
+        Dimension<List<Object>> dimPipelineParameters = Dimension.create(
+                DIM_PIPELINE_PARAMS,
+                Arrays.asList(new Object[] {
+                        FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_USE_TOP_K,
+                        "500", FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MIN_N, 1,
+                        FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MAX_N, 3 }),
+                Arrays.asList(new Object[] {
+                        FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_USE_TOP_K,
+                        "1000", FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MIN_N,
+                        1, FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MAX_N, 3 }));
+
+        // This feature is sensible and fast, but gives bad results on the demo data
+        Dimension<List<String>> dimFeatureSets = Dimension.create(
+                DIM_FEATURE_SET,
+                Arrays.asList(new String[] { DiffNrOfTokensPairFeatureExtractor.class.getName() }));
+
+        ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle("readers", dimReaders),
+                Dimension.create(DIM_DATA_WRITER, WekaDataWriter.class.getName()),
+                Dimension.create(DIM_LEARNING_MODE, LM_SINGLE_LABEL), Dimension.create(
+                        DIM_FEATURE_MODE, FM_PAIR), dimPipelineParameters, dimFeatureSets,
+                dimClassificationArgs);
+
+        return pSpace;
+    }
+
+    // ##### TRAIN-TEST #####
+    protected void runTrainTest(ParameterSpace pSpace)
+        throws Exception
+    {
+
+        BatchTaskTrainTest batch = new BatchTaskTrainTest("TwentyNewsgroupsTrainTest",
+                getPreprocessing());
+        batch.setParameterSpace(pSpace);
+        batch.setExecutionPolicy(ExecutionPolicy.RUN_AGAIN);
+        batch.addReport(BatchTrainTestReport.class);
+        batch.addReport(BatchOutcomeIDReport.class);
+
+        // Run
+        Lab.getInstance().run(batch);
+    }
+
+    protected AnalysisEngineDescription getPreprocessing()
+        throws ResourceInitializationException
+    {
+        return createEngineDescription(
+                createEngineDescription(StanfordSegmenter.class),
+                createEngineDescription(StanfordNamedEntityRecognizer.class,
+                        StanfordNamedEntityRecognizer.PARAM_VARIANT, "all.3class.distsim.crf"));
+    }
+}
