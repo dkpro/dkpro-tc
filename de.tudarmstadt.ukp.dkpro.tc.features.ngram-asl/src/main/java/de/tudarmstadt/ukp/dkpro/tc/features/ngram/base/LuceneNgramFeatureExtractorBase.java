acdc8f7508c0f938a72209c8741eb75a81402085
Johannes Daxenberger <daxenberger@ukp.informatik.tu-darmstadt.de>
2014-06-05 14:39:04 +0000

    Adding a test for LucenePOSNGramFeatureExtractor.
    Update issue 137
    The class hierarchy of the LuceneNgram Feature Extractors was inconsistent. I changed it to be consistent with LucenePOSNGram FeatureExtractors etc. 
    LucenePOSNGramFeatureExtractor has been working fine as shown in the corresponding test, but you need to set different parameters to configure it (as also shown in the test). I guess, this was causing the unexpected results here.

--- de.tudarmstadt.ukp.dkpro.tc.features.ngram-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/base/LuceneNgramFeatureExtractorBase.java
+++ de.tudarmstadt.ukp.dkpro.tc.features.ngram-asl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/base/LuceneNgramFeatureExtractorBase.java
@@ -1,36 +1,14 @@
 package de.tudarmstadt.ukp.dkpro.tc.features.ngram.base;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.Fields;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.MultiFields;
-import org.apache.lucene.index.Terms;
-import org.apache.lucene.index.TermsEnum;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.BytesRef;
-import org.apache.uima.fit.descriptor.ConfigurationParameter;
-import org.apache.uima.resource.ResourceInitializationException;
-import org.apache.uima.util.Level;
-
-import com.google.common.collect.MinMaxPriorityQueue;
-
-import de.tudarmstadt.ukp.dkpro.core.api.frequency.util.FrequencyDistribution;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.meta.MetaCollector;
 import de.tudarmstadt.ukp.dkpro.tc.features.ngram.meta.LuceneNGramMetaCollector;
-import de.tudarmstadt.ukp.dkpro.tc.features.ngram.util.TermFreqTuple;
 
 public abstract class LuceneNgramFeatureExtractorBase
-    extends NGramFeatureExtractorBase
+    extends LuceneFeatureExtractorBase
 {
-    public static final String PARAM_LUCENE_DIR = "luceneDir";
-    @ConfigurationParameter(name = PARAM_LUCENE_DIR, mandatory = true)
-    protected File luceneDir;
-
-    public static final String LUCENE_NGRAM_FIELD = "ngram";
 
     @Override
     public List<Class<? extends MetaCollector>> getMetaCollectorClasses()
@@ -57,47 +35,5 @@ public abstract class LuceneNgramFeatureExtractorBase
     protected int getTopN()
     {
         return ngramUseTopK;
-    }
-
-    @Override
-    protected FrequencyDistribution<String> getTopNgrams()
-        throws ResourceInitializationException
-    {
-
-        FrequencyDistribution<String> topNGrams = new FrequencyDistribution<String>();
-
-        MinMaxPriorityQueue<TermFreqTuple> topN = MinMaxPriorityQueue.maximumSize(getTopN())
-                .create();
-
-        IndexReader reader;
-        try {
-            reader = DirectoryReader.open(FSDirectory.open(luceneDir));
-            Fields fields = MultiFields.getFields(reader);
-            if (fields != null) {
-                Terms terms = fields.terms(getFieldName());
-                if (terms != null) {
-                    TermsEnum termsEnum = terms.iterator(null);
-                    BytesRef text = null;
-                    while ((text = termsEnum.next()) != null) {
-                        String term = text.utf8ToString();
-                        long freq = termsEnum.totalTermFreq();
-                        topN.add(new TermFreqTuple(term, freq));
-                    }
-                }
-            }
-        }
-        catch (Exception e) {
-            throw new ResourceInitializationException(e);
-        }
-
-        int size = topN.size();
-        for (int i = 0; i < size; i++) {
-            TermFreqTuple tuple = topN.poll();
-            topNGrams.addSample(tuple.getTerm(), tuple.getFreq());
-        }
-
-        getLogger().log(Level.INFO, "+++ TAKING " + topNGrams.getB() + " NGRAMS");
-
-        return topNGrams;
     }
 }
---
+++ de.tudarmstadt.ukp.dkpro.tc.features.ngram-asl/src/test/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/LucenePOSNGramFeatureExtractorTest.java
@@ -0,0 +1,99 @@
+package de.tudarmstadt.ukp.dkpro.tc.features.ngram;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.google.gson.Gson;
+
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureStore;
+import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
+import de.tudarmstadt.ukp.dkpro.tc.core.io.JsonDataWriter;
+import de.tudarmstadt.ukp.dkpro.tc.core.util.TaskUtils;
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.io.TestReaderSingleLabel;
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.meta.LucenePOSNGramMetaCollector;
+import de.tudarmstadt.ukp.dkpro.tc.fstore.simple.SimpleFeatureStore;
+
+public class LucenePOSNGramFeatureExtractorTest
+{
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Before
+    public void setupLogging()
+    {
+        System.setProperty("org.apache.uima.logger.class",
+                "org.apache.uima.util.impl.Log4jLogger_impl");
+    }
+
+    @Test
+    public void luceneNGramFeatureExtractorTest()
+        throws Exception
+    {
+
+        File luceneFolder = folder.newFolder();
+        File outputPath = folder.newFolder();
+
+        Object[] parameters = new Object[] { LucenePOSNGramDFE.PARAM_POS_NGRAM_USE_TOP_K, 5,
+                LucenePOSNGramDFE.PARAM_LUCENE_DIR, luceneFolder };
+        List<Object> parameterList = new ArrayList<Object>(Arrays.asList(parameters));
+
+        CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(
+                TestReaderSingleLabel.class, TestReaderSingleLabel.PARAM_SOURCE_LOCATION,
+                "src/test/resources/ngrams/*.txt");
+
+        AnalysisEngineDescription segmenter = AnalysisEngineFactory
+                .createEngineDescription(BreakIteratorSegmenter.class);
+
+        AnalysisEngineDescription posTagger = AnalysisEngineFactory
+                .createEngineDescription(OpenNlpPosTagger.class, OpenNlpPosTagger.PARAM_LANGUAGE,
+                        "en");
+
+        AnalysisEngineDescription metaCollector = AnalysisEngineFactory.createEngineDescription(
+                LucenePOSNGramMetaCollector.class, parameterList.toArray());
+
+        AnalysisEngineDescription featExtractorConnector = TaskUtils.getFeatureExtractorConnector(
+                parameterList, outputPath.getAbsolutePath(), JsonDataWriter.class.getName(),
+                Constants.LM_SINGLE_LABEL, Constants.FM_DOCUMENT, false, false,
+                LucenePOSNGramDFE.class.getName());
+
+        // run meta collector
+        SimplePipeline.runPipeline(reader, segmenter, posTagger, metaCollector);
+
+        // run FE(s)
+        SimplePipeline.runPipeline(reader, segmenter, posTagger, featExtractorConnector);
+
+        Gson gson = new Gson();
+        FeatureStore fs = gson.fromJson(
+                FileUtils.readFileToString(new File(outputPath, JsonDataWriter.JSON_FILE_NAME)),
+                SimpleFeatureStore.class);
+        assertEquals(4, fs.getNumberOfInstances());
+        assertEquals(1, fs.getUniqueOutcomes().size());
+
+        Set<String> featureNames = new HashSet<String>(fs.getFeatureNames());
+        assertEquals(5, featureNames.size());
+        assertTrue(featureNames.contains("posngram_CARD"));
+        assertTrue(featureNames.contains("posngram_V"));
+        assertTrue(featureNames.contains("posngram_CARD_CARD"));
+
+    }
+}
