46c6a10d3d5ae5e8318cc1e9ad3ff8eaab3c8969
Nadolskyy@gmail.com <Nadolskyy@gmail.com@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-06-12 16:02:19 +0000

    example reader and data which will be described on wiki page "Creating custom data readers".

---
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.examples-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/examples/io/SimpleDkproTCReader.java
@@ -0,0 +1,137 @@
+package de.tudarmstadt.ukp.dkpro.tc.examples.io;
+
+import static org.apache.commons.io.IOUtils.closeQuietly;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.uima.UimaContext;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import de.tudarmstadt.ukp.dkpro.core.api.io.JCasResourceCollectionReader_ImplBase;
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
+import de.tudarmstadt.ukp.dkpro.core.api.resources.ResourceUtils;
+import de.tudarmstadt.ukp.dkpro.tc.api.io.TCReaderSingleLabel;
+import de.tudarmstadt.ukp.dkpro.tc.api.type.TextClassificationOutcome;
+
+/**
+ * Show case for a simple DKPro TC reader
+ *
+ */
+public class SimpleDkproTCReader
+    extends JCasResourceCollectionReader_ImplBase
+	implements TCReaderSingleLabel
+
+{
+    /**
+     * Character encoding of the input data.
+     */
+    public static final String PARAM_ENCODING = ComponentParameters.PARAM_SOURCE_ENCODING;
+    @ConfigurationParameter(name = PARAM_ENCODING, mandatory = true, defaultValue = "UTF-8")
+    private String encoding;
+    
+    /**
+     * Path to the file containing the gold standard labels.
+     */
+    public static final String PARAM_GOLD_LABEL_FILE = "GoldLabelFile";
+    @ConfigurationParameter(name = PARAM_GOLD_LABEL_FILE, mandatory = true)
+    private String goldLabelFile;
+    
+    private List<String> golds;
+
+    private List<String> texts;
+    private int offset;
+    
+    @Override
+    public void initialize(UimaContext context)
+        throws ResourceInitializationException
+    {
+        super.initialize(context);
+        
+        // read file with gold labels
+        golds = new ArrayList<String>();
+        try {
+            URL resourceUrl = ResourceUtils.resolveLocation(goldLabelFile, this, context);
+            for (String label : FileUtils.readLines(new File(resourceUrl.toURI()))) {
+                golds.add(label);
+            }
+        }
+        catch (IOException e) {
+            throw new ResourceInitializationException(e);
+        }
+        catch (URISyntaxException ex) {
+            throw new ResourceInitializationException(ex);
+        } 
+        
+        // read file with instances
+        offset = 0;
+        Resource res = nextFile();
+        BufferedReader reader = null;
+        texts = new ArrayList<String>();
+        try {
+            reader = new BufferedReader(new InputStreamReader(res.getInputStream(), encoding));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+            	texts.add(line);
+            }
+        } catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        finally {
+            closeQuietly(reader);
+        }
+    }
+    
+    @Override
+    public boolean hasNext()
+        throws IOException, CollectionException
+    {
+        return offset < texts.size();
+    }
+  
+    
+    @Override
+    public void getNext(JCas aJCas)
+        throws IOException, CollectionException
+    {
+        // as we are creating more than one CAS out of a single file, we need to have different
+        // document titles and URIs for each CAS
+        // otherwise, serialized CASes will be overwritten
+        DocumentMetaData dmd = DocumentMetaData.create(aJCas);
+        dmd.setDocumentTitle(dmd.getDocumentTitle() + "-" + offset);
+        dmd.setDocumentUri(dmd.getDocumentUri() + "-" + offset);
+        dmd.setDocumentId(String.valueOf(offset));
+    	
+    	aJCas.setDocumentText(texts.get(offset));
+        
+        TextClassificationOutcome outcome = new TextClassificationOutcome(aJCas);
+        outcome.setOutcome(getTextClassificationOutcome(aJCas));
+        outcome.addToIndexes();
+	
+        offset++;
+    }
+
+
+	@Override
+	public String getTextClassificationOutcome(JCas jcas) 
+			throws CollectionException
+	{
+		return golds.get(offset);
+	}
+
+}
---
+++ trunk/de.tudarmstadt.ukp.dkpro.tc.examples-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/tc/examples/single/document/SimpleDkproTCReaderDemo.java
@@ -0,0 +1,112 @@
+package de.tudarmstadt.ukp.dkpro.tc.examples.single.document;
+
+import static de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase.INCLUDE_PREFIX;
+import static java.util.Arrays.asList;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import weka.classifiers.bayes.NaiveBayes;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.tudarmstadt.ukp.dkpro.lab.Lab;
+import de.tudarmstadt.ukp.dkpro.lab.task.Dimension;
+import de.tudarmstadt.ukp.dkpro.lab.task.ParameterSpace;
+import de.tudarmstadt.ukp.dkpro.lab.task.impl.BatchTask.ExecutionPolicy;
+import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
+import de.tudarmstadt.ukp.dkpro.tc.examples.io.SimpleDkproTCReader;
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.LuceneNGramDFE;
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.base.FrequencyDistributionNGramFeatureExtractorBase;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchCrossValidationReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.BatchRuntimeReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.ClassificationReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.task.BatchTaskCrossValidation;
+import de.tudarmstadt.ukp.dkpro.tc.weka.writer.WekaDataWriter;
+
+/**
+ * This demo shows how to use a simple self written dkpro-tc reader
+ */
+
+public class SimpleDkproTCReaderDemo implements Constants {
+	public static final String LANGUAGE_CODE = "en";
+	public static final int NUM_FOLDS = 2;
+	public static final String FILEPATH_TRAIN = "src/main/resources/data/simple_reader/train";
+	public static final String FILEPATH_GOLD_LABELS = "src/main/resources/data/simple_reader/gold_labels.txt";
+
+	public static void main(String[] args) throws Exception {
+		SimpleDkproTCReaderDemo demo = new SimpleDkproTCReaderDemo();
+		demo.runCrossValidation(getParameterSpace());
+	}
+
+	// ##### CV #####
+	protected void runCrossValidation(ParameterSpace pSpace) throws Exception {
+		BatchTaskCrossValidation batch = new BatchTaskCrossValidation(
+				"SimpleReaderDemoCV", getPreprocessing(), NUM_FOLDS);
+		batch.addInnerReport(ClassificationReport.class);
+		batch.setParameterSpace(pSpace);
+		batch.setExecutionPolicy(ExecutionPolicy.RUN_AGAIN);
+		batch.addReport(BatchCrossValidationReport.class);
+		batch.addReport(BatchRuntimeReport.class);
+
+		// Run
+		Lab.getInstance().run(batch);
+	}
+
+	public static ParameterSpace getParameterSpace() {
+		Map<String, Object> dimReaders = new HashMap<String, Object>();
+		dimReaders.put(DIM_READER_TRAIN, SimpleDkproTCReader.class);
+		dimReaders.put(
+				DIM_READER_TRAIN_PARAMS,
+				Arrays.asList(new Object[] {
+						SimpleDkproTCReader.PARAM_LANGUAGE, LANGUAGE_CODE,
+						SimpleDkproTCReader.PARAM_GOLD_LABEL_FILE,
+						FILEPATH_GOLD_LABELS,
+						SimpleDkproTCReader.PARAM_SOURCE_LOCATION,
+						FILEPATH_TRAIN, SimpleDkproTCReader.PARAM_PATTERNS,
+						INCLUDE_PREFIX + "*.txt" }));
+
+		@SuppressWarnings("unchecked")
+		Dimension<List<String>> dimClassificationArgs = Dimension.create(
+				DIM_CLASSIFICATION_ARGS,
+				Arrays.asList(new String[] { NaiveBayes.class.getName() }));
+
+		@SuppressWarnings("unchecked")
+		Dimension<List<String>> dimFeatureSets = Dimension.create(
+				DIM_FEATURE_SET,
+				asList(new String[] { LuceneNGramDFE.class.getName() }));
+
+		// parameters to configure feature extractors
+		@SuppressWarnings("unchecked")
+		Dimension<List<Object>> dimPipelineParameters = Dimension
+				.create(DIM_PIPELINE_PARAMS,
+						asList(new Object[] {
+								FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_USE_TOP_K,
+								"100",
+								FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MIN_N,
+								1,
+								FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MAX_N,
+								3 }));
+
+		@SuppressWarnings("unchecked")
+		ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle(
+				"readers", dimReaders), Dimension.create(DIM_DATA_WRITER,
+				WekaDataWriter.class.getName()), Dimension.create(
+				DIM_LEARNING_MODE, LM_SINGLE_LABEL), Dimension.create(
+				DIM_FEATURE_MODE, FM_DOCUMENT), dimPipelineParameters,
+				dimFeatureSets, dimClassificationArgs);
+
+		return pSpace;
+	}
+	
+	protected AnalysisEngineDescription getPreprocessing()
+			throws ResourceInitializationException {
+		return createEngineDescription(createEngineDescription(
+				BreakIteratorSegmenter.class,
+				BreakIteratorSegmenter.PARAM_LANGUAGE, LANGUAGE_CODE));
+	}
+}
