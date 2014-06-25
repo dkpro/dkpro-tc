97fc6667665e9b0130079d902b118a2c08b633b5
Richard Eckart de Castilho <richard.eckart@gmail.com>
2014-06-25 14:18:47 +0000

    Issue 156. Add missing license headers
    Update issue 156
    - Activate rat check profile
    - Added ASL headers in mallet module

--- dkpro-tc-mallet/src/main/java/de/tudarmstadt/ukp/dkpro/tc/mallet/util/PerClassEvaluator.java
+++ dkpro-tc-mallet/src/main/java/de/tudarmstadt/ukp/dkpro/tc/mallet/util/PerClassEvaluator.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.tc.mallet.util;
 
 import java.text.DecimalFormat;
@@ -14,7 +31,6 @@ import cc.mallet.types.InstanceList;
 import cc.mallet.types.MatrixOps;
 import cc.mallet.types.Sequence;
 import cc.mallet.util.MalletLogger;
-import de.tudarmstadt.ukp.dkpro.tc.mallet.task.TestTask;
 
 /**
  * Determines the precision, recall and F1 on a per-class basis.
@@ -105,7 +121,8 @@ public class PerClassEvaluator extends TransducerEvaluator {
 		this (new InstanceList[] {i1, i2}, new String[] {d1, d2});
 	}
 
-	public void evaluateInstanceList (TransducerTrainer tt, InstanceList data, String description)
+	@Override
+    public void evaluateInstanceList (TransducerTrainer tt, InstanceList data, String description)
 	{
 		Transducer model = tt.getTransducer();
 		Alphabet dict = model.getInputPipe().getTargetAlphabet();
