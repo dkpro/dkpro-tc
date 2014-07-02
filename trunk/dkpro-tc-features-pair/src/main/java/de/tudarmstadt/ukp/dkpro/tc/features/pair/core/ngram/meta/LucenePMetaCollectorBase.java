cf9173222bb81117232dc295f71f1eaa84152868
daxenberger@ukp.informatik.tu-darmstadt.de <daxenberger@ukp.informatik.tu-darmstadt.de@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-07-02 12:41:46 +0000

    use Constants class

--- trunk/dkpro-tc-features-pair/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/pair/core/ngram/meta/LucenePMetaCollectorBase.java
+++ trunk/dkpro-tc-features-pair/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/pair/core/ngram/meta/LucenePMetaCollectorBase.java
@@ -26,11 +26,12 @@ import org.apache.uima.jcas.JCas;
 
 import de.tudarmstadt.ukp.dkpro.core.api.frequency.util.FrequencyDistribution;
 import de.tudarmstadt.ukp.dkpro.tc.api.exception.TextClassificationException;
-import de.tudarmstadt.ukp.dkpro.tc.core.io.AbstractPairReader;
+import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
 import de.tudarmstadt.ukp.dkpro.tc.features.ngram.meta.LuceneBasedMetaCollector;
 
 public abstract class LucenePMetaCollectorBase
     extends LuceneBasedMetaCollector
+    implements Constants
 {
     @Override
     public void process(JCas jcas)
@@ -38,16 +39,16 @@ public abstract class LucenePMetaCollectorBase
     {
         JCas view1;
         JCas view2;
-        try{
-            view1 = jcas.getView(AbstractPairReader.PART_ONE);
-            view2 = jcas.getView(AbstractPairReader.PART_TWO);
+        try {
+            view1 = jcas.getView(PART_ONE);
+            view2 = jcas.getView(PART_TWO);
         }
         catch (Exception e) {
             throw new AnalysisEngineProcessException(e);
         }
-        
+
         initializeDocument(jcas);
-        
+
         List<JCas> jcases = new ArrayList<JCas>();
         jcases.add(view1);
         jcases.add(view2);
@@ -55,40 +56,41 @@ public abstract class LucenePMetaCollectorBase
         FrequencyDistribution<String> view1NGrams;
         FrequencyDistribution<String> view2NGrams;
         FrequencyDistribution<String> documentNGrams;
-        try{
+        try {
             view1NGrams = getNgramsFDView1(view1);
             view2NGrams = getNgramsFDView2(view2);
             documentNGrams = getNgramsFD(jcases);
-        }catch (TextClassificationException e){
+        }
+        catch (TextClassificationException e) {
             throw new AnalysisEngineProcessException(e);
         }
 
-
         for (String ngram : documentNGrams.getKeys()) {
-            for (int i=0;i<documentNGrams.getCount(ngram);i++){
-                addField(jcas, getFieldName(), ngram); 
+            for (int i = 0; i < documentNGrams.getCount(ngram); i++) {
+                addField(jcas, getFieldName(), ngram);
             }
         }
         for (String ngram : view1NGrams.getKeys()) {
-            for (int i=0;i<view1NGrams.getCount(ngram);i++){
-                addField(jcas, getFieldNameView1(), ngram); 
+            for (int i = 0; i < view1NGrams.getCount(ngram); i++) {
+                addField(jcas, getFieldNameView1(), ngram);
             }
         }
         for (String ngram : view2NGrams.getKeys()) {
-            for (int i=0;i<view2NGrams.getCount(ngram);i++){
-                addField(jcas, getFieldNameView2(), ngram); 
+            for (int i = 0; i < view2NGrams.getCount(ngram); i++) {
+                addField(jcas, getFieldNameView2(), ngram);
             }
         }
-        
+
         try {
             writeToIndex();
         }
         catch (IOException e) {
             throw new AnalysisEngineProcessException(e);
         }
-    }   
+    }
+
     protected abstract FrequencyDistribution<String> getNgramsFD(List<JCas> jcases)
-            throws TextClassificationException;
+        throws TextClassificationException;
 
     protected abstract FrequencyDistribution<String> getNgramsFDView1(JCas view1)
         throws TextClassificationException;
@@ -97,6 +99,7 @@ public abstract class LucenePMetaCollectorBase
         throws TextClassificationException;
 
     protected abstract String getFieldNameView1();
+
     protected abstract String getFieldNameView2();
 
 }
