022f930f8a718b01616a5db3d19dea9f18afd8f8
daxenberger@ukp.informatik.tu-darmstadt.de <daxenberger@ukp.informatik.tu-darmstadt.de@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-07-16 12:20:56 +0000

    test cases for unit and sequence demos.
    Update issue 164
    added test cases for Mallet demos to avoid problems like this in the future.

---
+++ trunk/dkpro-tc-examples/src/test/java/de/tudarmstadt/ukp/dkpro/tc/examples/single/sequence/NERSequenceDemoTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package de.tudarmstadt.ukp.dkpro.tc.examples.single.sequence;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import de.tudarmstadt.ukp.dkpro.lab.task.ParameterSpace;
+
+/**
+ * This is not exactly a unit test (yet). It just ensures that the experiments run without throwing
+ * any exception. Additional unit tests should test the inner workings of the experiments
+ * 
+ * 
+ */
+public class NERSequenceDemoTest
+{
+    NERSequenceDemo javaExperiment;
+    ParameterSpace pSpace;
+
+    @Before
+    public void setupLogging()
+    {
+        System.setProperty("org.apache.uima.logger.class",
+                "org.apache.uima.util.impl.Log4jLogger_impl");
+    }
+
+    @Before
+    public void setup()
+        throws Exception
+    {
+        String path = "target/repository/" + getClass().getSimpleName() + "/"
+                + name.getMethodName();
+        System.setProperty("DKPRO_HOME", new File(path).getAbsolutePath());
+
+        javaExperiment = new NERSequenceDemo();
+        pSpace = NERSequenceDemo.getParameterSpace();
+    }
+
+    @Test
+    public void testJavaCrossValidation()
+        throws Exception
+    {
+        // Java setup with automatic task wiring
+        javaExperiment.runCrossValidation(pSpace);
+    }
+
+    @Rule
+    public TestName name = new TestName();
+
+    @Before
+    public void printSeparator()
+    {
+        System.out.println("\n=== " + name.getMethodName() + " =====================");
+    }
+}
