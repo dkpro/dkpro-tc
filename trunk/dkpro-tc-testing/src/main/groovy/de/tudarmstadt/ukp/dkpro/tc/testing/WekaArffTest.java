711a7bf2873ba4ae9ac54dc72f8abd20e3826aea
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-09-03 05:48:07 +0000

    One more TaskUtils to WekaUtils move (that I forgot earlier) + finally removing the TaskUtils classes.

--- trunk/dkpro-tc-testing/src/main/groovy/de/tudarmstadt/ukp/dkpro/tc/testing/WekaArffTest.java
+++ trunk/dkpro-tc-testing/src/main/groovy/de/tudarmstadt/ukp/dkpro/tc/testing/WekaArffTest.java
@@ -24,7 +24,7 @@ import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
-import de.tudarmstadt.ukp.dkpro.tc.weka.util.TaskUtils;
+import de.tudarmstadt.ukp.dkpro.tc.weka.util.WekaUtils;
 
 /**
  * A test setup for native Weka access
@@ -42,8 +42,8 @@ public class WekaArffTest
         File train = new File("src/main/resources/arff/manyInstances/train.arff.gz");
         File test = new File("src/main/resources/arff/manyInstances/test.arff.gz");
 
-        Instances trainData = TaskUtils.getInstances(train, false);
-        Instances testData = TaskUtils.getInstances(test, false);
+        Instances trainData = WekaUtils.getInstances(train, false);
+        Instances testData = WekaUtils.getInstances(test, false);
 
         Classifier cl = new NaiveBayes();
         
