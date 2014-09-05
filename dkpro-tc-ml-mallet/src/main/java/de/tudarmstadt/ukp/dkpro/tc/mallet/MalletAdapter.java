798d664054cbb16b479f7b158b39e026e007f47b
Torsten Zesch <torsten.zesch@gmail.com>
2014-09-05 08:32:47 +0000

    Renaming other ML modules to follow new naming scheme.

---
+++ dkpro-tc-ml-mallet/src/main/java/de/tudarmstadt/ukp/dkpro/tc/mallet/MalletAdapter.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.tc.mallet;
+
+import java.util.Collection;
+
+import de.tudarmstadt.ukp.dkpro.lab.reporting.ReportBase;
+import de.tudarmstadt.ukp.dkpro.lab.task.Dimension;
+import de.tudarmstadt.ukp.dkpro.lab.task.impl.DimensionBundle;
+import de.tudarmstadt.ukp.dkpro.lab.task.impl.ExecutableTaskBase;
+import de.tudarmstadt.ukp.dkpro.tc.mallet.report.MalletBatchTrainTestReport;
+import de.tudarmstadt.ukp.dkpro.tc.mallet.report.MalletClassificationReport;
+import de.tudarmstadt.ukp.dkpro.tc.mallet.report.MalletOutcomeIDReport;
+import de.tudarmstadt.ukp.dkpro.tc.mallet.task.MalletTestTask;
+import de.tudarmstadt.ukp.dkpro.tc.mallet.util.MalletFoldDimensionBundle;
+import de.tudarmstadt.ukp.dkpro.tc.ml.TCMachineLearningAdapter;
+
+public class MalletAdapter 
+	implements TCMachineLearningAdapter
+{
+
+	public static TCMachineLearningAdapter getInstance() {
+		return new MalletAdapter();
+	}
+	
+	@Override
+	public ExecutableTaskBase getTestTask() {
+		return new MalletTestTask();
+	}
+
+	@Override
+	public Class<? extends ReportBase> getClassificationReportClass() {
+		return MalletClassificationReport.class;
+	}
+
+	@Override
+	public Class<? extends ReportBase> getOutcomeIdReportClass() {
+		return MalletOutcomeIDReport.class;
+	}
+
+	@Override
+	public Class<? extends ReportBase> getBatchTrainTestReportClass() {
+		return MalletBatchTrainTestReport.class;
+	}
+
+	@Override
+	public DimensionBundle<Collection<String>> getFoldDimensionBundle(
+			String[] files, int folds) {
+		return new MalletFoldDimensionBundle("files", Dimension.create("", files), folds);
+	}
+}
