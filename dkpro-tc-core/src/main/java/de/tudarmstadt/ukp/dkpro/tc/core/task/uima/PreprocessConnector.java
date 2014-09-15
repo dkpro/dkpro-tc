ca83bd71159d2af6fb6c05c9662507e698708faf
Johannes Daxenberger <daxenberger@ukp.informatik.tu-darmstadt.de>
2014-09-15 14:45:05 +0000

    adding missing license header

--- dkpro-tc-core/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/task/uima/PreprocessConnector.java
+++ dkpro-tc-core/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/task/uima/PreprocessConnector.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.tc.core.task.uima;
 
 import org.apache.uima.UimaContext;
@@ -8,35 +25,37 @@ import org.apache.uima.resource.ResourceInitializationException;
 
 import de.tudarmstadt.ukp.dkpro.tc.api.exception.TextClassificationException;
 
-public class PreprocessConnector 
-	extends JCasAnnotator_ImplBase
+public class PreprocessConnector
+    extends JCasAnnotator_ImplBase
 {
 
-	private int nrofProcessCalls;
-	
-	@Override
-	public void initialize(UimaContext context)
-			throws ResourceInitializationException
-	{
-		super.initialize(context);
-		
-		nrofProcessCalls = 0;
-	}
+    private int nrofProcessCalls;
 
+    @Override
+    public void initialize(UimaContext context)
+        throws ResourceInitializationException
+    {
+        super.initialize(context);
 
-	@Override
-	public void process(JCas aJCas) throws AnalysisEngineProcessException {
-		nrofProcessCalls++;
-	}
+        nrofProcessCalls = 0;
+    }
 
-	@Override
-	public void collectionProcessComplete()
-			throws AnalysisEngineProcessException
-	{
-		super.collectionProcessComplete();
-		// no documents?
-		if (nrofProcessCalls == 0) {
-			throw new AnalysisEngineProcessException(new TextClassificationException("There are no documents to process."));
-		}
-	}
+    @Override
+    public void process(JCas aJCas)
+        throws AnalysisEngineProcessException
+    {
+        nrofProcessCalls++;
+    }
+
+    @Override
+    public void collectionProcessComplete()
+        throws AnalysisEngineProcessException
+    {
+        super.collectionProcessComplete();
+        // no documents?
+        if (nrofProcessCalls == 0) {
+            throw new AnalysisEngineProcessException(new TextClassificationException(
+                    "There are no documents to process."));
+        }
+    }
 }
