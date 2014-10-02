f9bed823afa16f492a58d5ec00416a649b9fe000
ivan.habernal@gmail.com <ivan.habernal@gmail.com@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-10-02 13:46:56 +0000

    No issue. Added some JavaDoc to TCMachineLearningAdapter to explain the behavior of the methods.

--- trunk/dkpro-tc-ml/src/main/java/de/tudarmstadt/ukp/dkpro/tc/ml/TCMachineLearningAdapter.java
+++ trunk/dkpro-tc-ml/src/main/java/de/tudarmstadt/ukp/dkpro/tc/ml/TCMachineLearningAdapter.java
@@ -2,13 +2,13 @@
  * Copyright 2014
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,18 +17,20 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.tc.ml;
 
-import java.util.Collection;
-
 import de.tudarmstadt.ukp.dkpro.lab.reporting.ReportBase;
 import de.tudarmstadt.ukp.dkpro.lab.task.impl.DimensionBundle;
 import de.tudarmstadt.ukp.dkpro.lab.task.impl.ExecutableTaskBase;
 
+import java.util.Collection;
+
 /**
  * Interface for machine learning frameworks in TC
  */
-public interface TCMachineLearningAdapter {
+public interface TCMachineLearningAdapter
+{
 
-	public enum AdapterNameEntries {
+    public enum AdapterNameEntries
+    {
         /**
          * Intended for storing feature vectors (both in training and testing phases)
          */
@@ -37,44 +39,56 @@ public interface TCMachineLearningAdapter {
         /**
          * Intended for actual predictions on test data, i.e. label sequence
          */
-		predictionsFile,
+        predictionsFile,
 
         /**
          * Intended for storing evaluation of the task
          */
-		evaluationFile,
+        evaluationFile,
 
-		featureSelectionFile
-	}
+        featureSelectionFile
+    }
 
-	/**
-	 * @return The task that reads the ML feature store format, trains the classifier and stores the test results.
-	 */
-	public ExecutableTaskBase getTestTask();
-	
-	/**
-	 * @return The report that computes the classification results.
-	 */
-	public Class<? extends ReportBase> getClassificationReportClass();
-	
-	/**
-	 * @return The report that collects the outcomeId to prediction values.
-	 */
-	public Class<? extends ReportBase> getOutcomeIdReportClass();
-	
-	/**
-	 * @return The report that collects the results from the different folds.
-	 */
-	public Class<? extends ReportBase> getBatchTrainTestReportClass();
-	
-	/**
-	 * @return The fold dimension bundle for CV
-	 */
-	public<T extends DimensionBundle<Collection<String>>> T getFoldDimensionBundle(String[] files, int folds);
-	
-	/**
-	 * @param name Which name should be returned
-	 * @return Returns the filename that is used for specific files by the framework
-	 */
-	public String getFrameworkFilename(AdapterNameEntries name);
+    /**
+     * @return The task that reads the ML feature store format, trains the classifier and stores the test results.
+     */
+    public ExecutableTaskBase getTestTask();
+
+    /**
+     * This report is always added to {@code testTask} reports by default in
+     * {@linkplain de.tudarmstadt.ukp.dkpro.tc.ml.BatchTaskCrossValidation}
+     * and {@link de.tudarmstadt.ukp.dkpro.tc.ml.BatchTaskTrainTest}.
+     *
+     * @return The report that computes the classification results.
+     */
+    public Class<? extends ReportBase> getClassificationReportClass();
+
+    /**
+     * This report is always added to {@code testTask} reports by default in
+     * {@linkplain de.tudarmstadt.ukp.dkpro.tc.ml.BatchTaskCrossValidation}
+     * and {@link de.tudarmstadt.ukp.dkpro.tc.ml.BatchTaskTrainTest}.
+     *
+     * @return The report that collects the outcomeId to prediction values.
+     */
+    public Class<? extends ReportBase> getOutcomeIdReportClass();
+
+    /**
+     * This report is always added to {@code crossValidationTask} reports
+     * by default in {@linkplain de.tudarmstadt.ukp.dkpro.tc.ml.BatchTaskCrossValidation}.
+     *
+     * @return The report that collects the results from the different folds.
+     */
+    public Class<? extends ReportBase> getBatchTrainTestReportClass();
+
+    /**
+     * @return The fold dimension bundle for CV
+     */
+    public <T extends DimensionBundle<Collection<String>>> T getFoldDimensionBundle(String[] files,
+            int folds);
+
+    /**
+     * @param name Which name should be returned
+     * @return Returns the filename that is used for specific files by the framework
+     */
+    public String getFrameworkFilename(AdapterNameEntries name);
 }
