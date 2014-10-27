b6d8aa9886c588351f5542803e1f411bb96b7cbb
daxenberger@ukp.informatik.tu-darmstadt.de <daxenberger@ukp.informatik.tu-darmstadt.de@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-10-27 14:07:15 +0000

    Update issue 73
    Setting names for leave-one-out feature sets using Discriminable.
    Kudos to Richard for pointing this out.

--- trunk/dkpro-tc-core/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/util/ExperimentUtil.java
+++ trunk/dkpro-tc-core/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/util/ExperimentUtil.java
@@ -19,21 +19,27 @@ package de.tudarmstadt.ukp.dkpro.tc.core.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import de.tudarmstadt.ukp.dkpro.lab.task.Dimension;
+import de.tudarmstadt.ukp.dkpro.lab.task.Discriminable;
 import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
 
 public class ExperimentUtil {
 
-	/**
-	 * Returns a pre-defined dimension with feature extractor sets configured for an ablation test.
-	 * For example, if you specify four feature extractors A, B, C, and D, you will get
-	 * [A,B,C,D], [A,B,C], [A,B,D], [A,C,D], [B,C,D],
-	 * 
-	 * @param featureExtractorClassNames All the feature extractors that should be tested.
-	 * @return
-	 */
+    private static final String LEFTOUT_FE = "ABLATION_TEST_LEFTOUT_FEATURE_";
+
+	        /**
+     * Returns a pre-defined dimension with feature extractor sets configured for an ablation test.
+     * For example, if you specify four feature extractors A, B, C, and D, you will get [A,B,C,D],
+     * [A,B,C], [A,B,D], [A,C,D], [B,C,D],
+     * 
+     * @param featureExtractorClassNames
+     *            All the feature extractors that should be tested.
+     * @return a dimension with a list of feature extractor sets; named after the feature that is
+     *         left out
+     */
 	public static Dimension<List<String>> getAblationTestFeatures(String ... featureExtractorClassNames) {
 		@SuppressWarnings("unchecked")
 		List<String>[] featureSets = (ArrayList<String>[]) new ArrayList[featureExtractorClassNames.length + 1];
@@ -53,8 +59,53 @@ public class ExperimentUtil {
 	}
 	
 	private static List<String> getFeatureNamesMinusOne(String[] names, int i) {
-		List<String> nameList = new ArrayList<String>(Arrays.asList(names));
+        NamedArrayList<String> nameList = new NamedArrayList<String>(Arrays.asList(names));
+        nameList.setName(LEFTOUT_FE + names[i]);
 		nameList.remove(i);
 		return nameList;
 	}
+
+    /**
+     * A named list which can be used to label values which are lists (e.g. a list of feature
+     * extractors) in dimensions.
+     * 
+     * @param <T>
+     */
+    public static class NamedArrayList<T> extends ArrayList<T> implements Discriminable
+    {
+        private String name;
+
+        public NamedArrayList()
+        {
+            super();
+        }
+
+        public NamedArrayList(Collection<? extends T> c)
+        {
+            super(c);
+        }
+
+        public NamedArrayList(int initialCapacity)
+        {
+            super(initialCapacity);
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        @Override
+        public Object getDiscriminatorValue()
+        {
+            return name;
+        }
+
+        @Override
+        public Object getActualValue()
+        {
+            return this;
+        }
+    }
+
 }
