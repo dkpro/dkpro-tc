b490954ab2fae960be2b7c2f45ccfb4fd5e308db
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-10-29 15:43:58 +0000

    Adding a demo for ablation tests.

---
+++ trunk/dkpro-tc-examples/src/main/java/de/tudarmstadt/ukp/dkpro/tc/examples/single/document/ablation/AblationTestDemo.java
@@ -0,0 +1,191 @@
+/**
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package de.tudarmstadt.ukp.dkpro.tc.examples.single.document.ablation;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import weka.classifiers.functions.SMO;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.tudarmstadt.ukp.dkpro.lab.Lab;
+import de.tudarmstadt.ukp.dkpro.lab.task.Dimension;
+import de.tudarmstadt.ukp.dkpro.lab.task.ParameterSpace;
+import de.tudarmstadt.ukp.dkpro.lab.task.impl.BatchTask.ExecutionPolicy;
+import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
+import de.tudarmstadt.ukp.dkpro.tc.core.util.ExperimentUtil;
+import de.tudarmstadt.ukp.dkpro.tc.examples.io.TwentyNewsgroupsCorpusReader;
+import de.tudarmstadt.ukp.dkpro.tc.examples.util.DemoUtils;
+import de.tudarmstadt.ukp.dkpro.tc.features.length.NrOfTokensDFE;
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.LuceneNGramDFE;
+import de.tudarmstadt.ukp.dkpro.tc.features.ngram.base.NGramFeatureExtractorBase;
+import de.tudarmstadt.ukp.dkpro.tc.features.twitter.EmoticonRatioDFE;
+import de.tudarmstadt.ukp.dkpro.tc.features.twitter.NumberOfHashTagsDFE;
+import de.tudarmstadt.ukp.dkpro.tc.ml.task.BatchTaskCrossValidation;
+import de.tudarmstadt.ukp.dkpro.tc.ml.task.BatchTaskTrainTest;
+import de.tudarmstadt.ukp.dkpro.tc.weka.WekaClassificationAdapter;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.WekaBatchCrossValidationReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.WekaBatchOutcomeIDReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.WekaBatchRuntimeReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.WekaBatchTrainTestReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.WekaClassificationReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.report.WekaFeatureValuesReport;
+import de.tudarmstadt.ukp.dkpro.tc.weka.writer.WekaDataWriter;
+
+/**
+ * Shows how to use the ablation test feature sets.
+ */
+public class AblationTestDemo
+    implements Constants
+{
+    public static final String LANGUAGE_CODE = "en";
+
+    public static final int NUM_FOLDS = 3;
+
+    public static final String corpusFilePathTrain = "src/main/resources/data/twentynewsgroups/bydate-train";
+    public static final String corpusFilePathTest = "src/main/resources/data/twentynewsgroups/bydate-test";
+
+    public static void main(String[] args)
+        throws Exception
+    {
+    	
+    	// This is used to ensure that the required DKPRO_HOME environment variable is set.
+    	// Ensures that people can run the experiments even if they haven't read the setup instructions first :)
+    	// Don't use this in real experiments! Read the documentation and set DKPRO_HOME as explained there.
+    	DemoUtils.setDkproHome(AblationTestDemo.class.getSimpleName());
+    	
+        ParameterSpace pSpace = getParameterSpace();
+
+        AblationTestDemo experiment = new AblationTestDemo();
+        experiment.runCrossValidation(pSpace);
+        experiment.runTrainTest(pSpace);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static ParameterSpace getParameterSpace()
+    {
+        // configure training and test data reader dimension
+        // train/test will use both, while cross-validation will only use the train part
+        Map<String, Object> dimReaders = new HashMap<String, Object>();
+        dimReaders.put(DIM_READER_TRAIN, TwentyNewsgroupsCorpusReader.class);
+        dimReaders
+                .put(
+                        DIM_READER_TRAIN_PARAMS,
+                        Arrays.asList(TwentyNewsgroupsCorpusReader.PARAM_SOURCE_LOCATION,
+                                corpusFilePathTrain,
+                                TwentyNewsgroupsCorpusReader.PARAM_LANGUAGE, LANGUAGE_CODE,
+                                TwentyNewsgroupsCorpusReader.PARAM_PATTERNS,
+                                Arrays.asList(TwentyNewsgroupsCorpusReader.INCLUDE_PREFIX
+                                        + "*/*.txt")));
+        dimReaders.put(DIM_READER_TEST, TwentyNewsgroupsCorpusReader.class);
+        dimReaders.put(
+                DIM_READER_TEST_PARAMS,
+                Arrays.asList(TwentyNewsgroupsCorpusReader.PARAM_SOURCE_LOCATION,
+                        corpusFilePathTest, TwentyNewsgroupsCorpusReader.PARAM_LANGUAGE,
+                        LANGUAGE_CODE, TwentyNewsgroupsCorpusReader.PARAM_PATTERNS,
+                        TwentyNewsgroupsCorpusReader.INCLUDE_PREFIX + "*/*.txt"));
+
+        Dimension<List<String>> dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
+                Arrays.asList(new String[] { SMO.class.getName() })
+        );
+
+        Dimension<List<Object>> dimPipelineParameters = Dimension.create(
+                DIM_PIPELINE_PARAMS,
+                Arrays.asList(new Object[] {
+                		NGramFeatureExtractorBase.PARAM_NGRAM_USE_TOP_K, 500,
+                		NGramFeatureExtractorBase.PARAM_NGRAM_MIN_N, 1,
+                        NGramFeatureExtractorBase.PARAM_NGRAM_MAX_N, 3 })
+        );
+
+        // ##############
+        // this is where the feature set for the ablation test are created
+        Dimension<List<String>> dimFeatureSets = ExperimentUtil.getAblationTestFeatures(
+        		EmoticonRatioDFE.class.getName(),
+                NumberOfHashTagsDFE.class.getName(),
+                NrOfTokensDFE.class.getName(),
+                LuceneNGramDFE.class.getName()
+        );
+
+        ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle("readers", dimReaders),
+                Dimension.create(DIM_DATA_WRITER, WekaDataWriter.class.getName()),
+                Dimension.create(DIM_LEARNING_MODE, LM_SINGLE_LABEL), Dimension.create(
+                        DIM_FEATURE_MODE, FM_DOCUMENT), dimPipelineParameters, dimFeatureSets,
+                dimClassificationArgs);
+
+        return pSpace;
+    }
+
+    // ##### CV #####
+    protected void runCrossValidation(ParameterSpace pSpace)
+        throws Exception
+    {
+
+        BatchTaskCrossValidation batch = new BatchTaskCrossValidation("TwentyNewsgroupsCV", WekaClassificationAdapter.getInstance(),
+                getPreprocessing(), NUM_FOLDS);
+        batch.addInnerReport(WekaClassificationReport.class);
+        // add a second report to TestTask which creates a report about average feature values for
+        // each outcome label
+        batch.addInnerReport(WekaFeatureValuesReport.class);
+        batch.setParameterSpace(pSpace);
+        batch.setExecutionPolicy(ExecutionPolicy.RUN_AGAIN);
+        batch.addReport(WekaBatchCrossValidationReport.class);
+        batch.addReport(WekaBatchRuntimeReport.class);
+
+        // Run
+        Lab.getInstance().run(batch);
+    }
+
+    // ##### TRAIN-TEST #####
+    protected void runTrainTest(ParameterSpace pSpace)
+        throws Exception
+    {
+
+        BatchTaskTrainTest batch = new BatchTaskTrainTest("TwentyNewsgroupsTrainTest", WekaClassificationAdapter.getInstance(),
+                getPreprocessing());
+        batch.addInnerReport(WekaClassificationReport.class);
+        // add a second report to TestTask which creates a report about average feature values for
+        // each outcome label
+        batch.addInnerReport(WekaFeatureValuesReport.class);
+        batch.setParameterSpace(pSpace);
+        batch.setExecutionPolicy(ExecutionPolicy.RUN_AGAIN);
+        batch.addReport(WekaBatchTrainTestReport.class);
+        batch.addReport(WekaBatchOutcomeIDReport.class);
+        batch.addReport(WekaBatchRuntimeReport.class);
+
+        // Run
+        Lab.getInstance().run(batch);
+    }
+
+    protected AnalysisEngineDescription getPreprocessing()
+        throws ResourceInitializationException
+    {
+
+        return createEngineDescription(
+                createEngineDescription(BreakIteratorSegmenter.class),
+                createEngineDescription(OpenNlpPosTagger.class, OpenNlpPosTagger.PARAM_LANGUAGE,
+                        LANGUAGE_CODE));
+    }
+}
