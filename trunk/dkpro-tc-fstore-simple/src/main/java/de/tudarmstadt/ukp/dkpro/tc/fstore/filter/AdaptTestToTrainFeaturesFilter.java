8d60b66a54477fc024bbbae317a79541d5a91c8c
torsten.zesch <torsten.zesch@1594a34e-dc64-a6aa-c632-c87192b3ba5b>
2014-11-05 10:23:23 +0000

    Issue 210: Incomplete first version of filter.

---
+++ trunk/dkpro-tc-fstore-simple/src/main/java/de/tudarmstadt/ukp/dkpro/tc/fstore/filter/AdaptTestToTrainFeaturesFilter.java
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.tc.fstore.filter;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureStore;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.Instance;
+
+/**
+ *
+ */
+public class AdaptTestToTrainFeaturesFilter
+	implements FeatureStoreFilter
+{
+
+	private TreeSet<String> trainFeatureNames;
+	
+	public void setFeatureNames(TreeSet<String> featureNames) {
+		this.trainFeatureNames = featureNames;
+	}
+	
+	
+	@Override
+	public void applyFilter(FeatureStore store) {
+		
+//		// create mapping from outcomes to instance offsets in the feature store
+//		Map<String, List<Integer>> outcome2instanceOffset = new HashMap<String, List<Integer>>();
+//		for (int i=0; i<store.getNumberOfInstances(); i++) {
+//			Instance instance = store.getInstance(i);
+//			String outcome = instance.getOutcome();
+//			List<Integer> offsets;
+//			if (outcome2instanceOffset.containsKey(outcome)) {
+//				offsets = outcome2instanceOffset.get(outcome);
+//			}
+//			else {
+//				offsets = new ArrayList<>();
+//			}
+//			offsets.add(i);
+//			outcome2instanceOffset.put(outcome, offsets);
+//		}
+//		
+//		// find the smallest class
+//		int minClassSize = Integer.MAX_VALUE;
+//		for (String outcome : outcome2instanceOffset.keySet()) {
+//			int classSize = outcome2instanceOffset.get(outcome).size();
+//			if (classSize < minClassSize) {
+//				minClassSize = classSize;
+//			}
+//		}
+//		
+//		// resample all but the smallest class to the same size as the smallest class
+//		// return the offsets of the instances that should be deleted
+//		SortedSet<Integer> offsetsToDelete = new TreeSet<>();
+//		for (String outcome : outcome2instanceOffset.keySet()) {
+//			int classSize = outcome2instanceOffset.get(outcome).size();
+//			if (classSize != minClassSize) {
+//				offsetsToDelete.addAll(resample(outcome2instanceOffset.get(outcome), minClassSize));
+//			}
+//		}
+//		
+//		int nrOfDeleted = 0;
+//		for (int offsetToDelete : offsetsToDelete) {
+//			store.deleteInstance(offsetToDelete - nrOfDeleted);
+//			nrOfDeleted++;
+//		}
+	}
+
+	@Override
+	public boolean isApplicableForTraining() {
+		return false;
+	}
+
+	@Override
+	public boolean isApplicableForTesting() {
+		return true;
+	}
+}
