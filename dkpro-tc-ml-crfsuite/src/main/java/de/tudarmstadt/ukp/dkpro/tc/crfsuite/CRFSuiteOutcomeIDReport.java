90840571e65d1d102a687e67f08599c71f4a15f1
Tobias Horsmann <tobias.horsmann@gmail.com>
2015-02-01 20:29:04 +0000

    Issue 183:	Add CRFSuite ML adapter

--- dkpro-tc-ml-crfsuite/src/main/java/de/tudarmstadt/ukp/dkpro/tc/crfsuite/CRFSuiteOutcomeIDReport.java
+++ dkpro-tc-ml-crfsuite/src/main/java/de/tudarmstadt/ukp/dkpro/tc/crfsuite/CRFSuiteOutcomeIDReport.java
@@ -39,147 +39,146 @@ import de.tudarmstadt.ukp.dkpro.tc.crfsuite.task.CRFSuiteTestTask;
  * @author zesch
  * 
  */
-public class CRFSuiteOutcomeIDReport
-    extends ReportBase
-{
-    /**
-     * Name of the file where the instanceID / outcome pairs are stored
-     */
-    public static final String ID_OUTCOME_KEY = "id2outcome.txt";
-    /**
-     * Character that is used for separating fields in the output file
-     */
-    public static final String SEPARATOR_CHAR = ";";
+public class CRFSuiteOutcomeIDReport extends ReportBase {
+	/**
+	 * Name of the file where the instanceID / outcome pairs are stored
+	 */
+	public static final String ID_OUTCOME_KEY = "id2outcome.txt";
+	/**
+	 * Character that is used for separating fields in the output file
+	 */
+	public static final String SEPARATOR_CHAR = ";";
 
-    private static final String ID_CONSTANT_VALUE = Constants.ID_FEATURE_NAME + "=";
+	private static final String ID_CONSTANT_VALUE = Constants.ID_FEATURE_NAME
+			+ "=";
 
-    @Override
-    public void execute()
-        throws Exception
-    {
-        List<String> labelGoldVsActual = getGoldAndPredictions();
+	@Override
+	public void execute() throws Exception {
+		List<String> labelGoldVsActual = getGoldAndPredictions();
 
-        HashMap<String, Integer> mapping = createMappingLabel2Number(labelGoldVsActual);
+		HashMap<String, Integer> mapping = createMappingLabel2Number(labelGoldVsActual);
 
-        List<String> testData = getTestData();
+		List<String> testData = getTestData();
 
-        Properties props = generateProperties(mapping, labelGoldVsActual, testData);
+		Properties props = generateProperties(mapping, labelGoldVsActual,
+				testData);
 
-        // add "#labels' line with all labels
-        StringBuilder sb = new StringBuilder();
-        sb.append("labels");
-        for (String label : mapping.keySet()) {
-            sb.append(" " + mapping.get(label) + "=" + label);
-        }
+		// add "#labels' line with all labels
+		StringBuilder sb = new StringBuilder();
+		sb.append("labels");
+		for (String label : mapping.keySet()) {
+			sb.append(" " + mapping.get(label) + "=" + label);
+		}
 
-        getContext().storeBinary(
-                ID_OUTCOME_KEY,
-                new PropertiesAdapter(props, "ID=PREDICTION" + SEPARATOR_CHAR + "GOLDSTANDARD"
-                        + "\n" + sb.toString()));
-    }
+		getContext().storeBinary(
+				ID_OUTCOME_KEY,
+				new PropertiesAdapter(props, "ID=PREDICTION" + SEPARATOR_CHAR
+						+ "GOLDSTANDARD" + "\n" + sb.toString()));
+	}
 
-    private HashMap<String, Integer> createMappingLabel2Number(List<String> aLabelGoldVsActual)
-    {
-        HashSet<String> labels = new HashSet<String>();
+	private HashMap<String, Integer> createMappingLabel2Number(
+			List<String> aLabelGoldVsActual) {
+		HashSet<String> labels = new HashSet<String>();
 
-        int maxLines = aLabelGoldVsActual.size();
-        for (int i = 0; i < maxLines; i++) {
-            if (i == 0) {
-                // skip the headline row
-                continue;
-            }
-            String line = aLabelGoldVsActual.get(i);
-            String[] split = line.split("\t");
-            if (split.length == 0) {
-                continue;
-            }
-            if (split.length >= 1) {
-                labels.add(split[0]);
-            }
-            if (split.length >= 2) {
-                labels.add(split[1]);
-            }
-        }
+		int maxLines = aLabelGoldVsActual.size();
+		for (int i = 0; i < maxLines; i++) {
+			if (i == 0) {
+				// skip the headline row
+				continue;
+			}
+			String line = aLabelGoldVsActual.get(i);
+			String[] split = line.split("\t");
+			if (split.length == 0) {
+				continue;
+			}
+			if (split.length >= 1) {
+				labels.add(split[0]);
+			}
+			if (split.length >= 2) {
+				labels.add(split[1]);
+			}
+		}
 
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        int i = 0;
-        for (String label : labels) {
-        	if (label.isEmpty()){
-        		continue;
-        	}
-            map.put(label, i);
-            i++;
-        }
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		int i = 0;
+		for (String label : labels) {
+			if (label.isEmpty()) {
+				continue;
+			}
+			map.put(label, i);
+			i++;
+		}
 
-        return map;
-    }
+		return map;
+	}
 
-    private List<String> getTestData()
-        throws Exception
-    {
-        File storage = getContext().getStorageLocation(
-                CRFSuiteTestTask.TEST_TASK_INPUT_KEY_TEST_DATA, AccessMode.READONLY);
+	private List<String> getTestData() throws Exception {
+		File storage = getContext().getStorageLocation(
+				CRFSuiteTestTask.TEST_TASK_INPUT_KEY_TEST_DATA,
+				AccessMode.READONLY);
 
-        File testFile = new File(storage.getAbsolutePath()
-                + "/"
-                + CRFSuiteAdapter.getInstance().getFrameworkFilename(
-                        AdapterNameEntries.featureVectorsFile));
+		File testFile = new File(storage.getAbsolutePath()
+				+ "/"
+				+ CRFSuiteAdapter.getInstance().getFrameworkFilename(
+						AdapterNameEntries.featureVectorsFile));
 
-        List<String> readLines = FileUtils.readLines(testFile, "UTF-8");
+		List<String> readLines = FileUtils.readLines(testFile, "UTF-8");
 
-        return readLines;
-    }
+		return readLines;
+	}
 
-    private List<String> getGoldAndPredictions()
-        throws Exception
-    {
-        File storage = getContext().getStorageLocation(CRFSuiteTestTask.TEST_TASK_OUTPUT_KEY,
-                AccessMode.READONLY);
-        File predictionFile = new File(storage.getAbsolutePath()
-                + "/"
-                + CRFSuiteAdapter.getInstance().getFrameworkFilename(
-                        AdapterNameEntries.predictionsFile));
-        List<String> readLines = FileUtils.readLines(predictionFile, "UTF-8");
+	private List<String> getGoldAndPredictions() throws Exception {
+		File storage = getContext().getStorageLocation(
+				CRFSuiteTestTask.TEST_TASK_OUTPUT_KEY, AccessMode.READONLY);
+		File predictionFile = new File(storage.getAbsolutePath()
+				+ "/"
+				+ CRFSuiteAdapter.getInstance().getFrameworkFilename(
+						AdapterNameEntries.predictionsFile));
+		List<String> readLines = FileUtils.readLines(predictionFile, "UTF-8");
 
-        return readLines;
-    }
+		return readLines;
+	}
 
-    protected static Properties generateProperties(HashMap<String, Integer> aMapping,
-            List<String> predictions, List<String> testFeatures)
-        throws Exception
-    {
-        Properties props = new Properties();
+	protected static Properties generateProperties(
+			HashMap<String, Integer> aMapping, List<String> predictions,
+			List<String> testFeatures) throws Exception {
+		Properties props = new Properties();
 
-        int maxLines = predictions.size();
+		int maxLines = predictions.size();
 
-        for (int idx = 1; idx < maxLines; idx++) {
-            String entry = predictions.get(idx);
-            String[] split = entry.split("\t");
-            if (split.length != 2) {
-                continue;
-            }
+		for (int idx = 1; idx < maxLines; idx++) {
+			String entry = predictions.get(idx);
+			String[] split = entry.split("\t");
+			if (split.length != 2) {
+				continue;
+			}
+			if (idx-1 >= testFeatures.size()) {
+				// trailing whitespaces at the end of the file could become a
+				// problem otherwise
+				break;
+			}
+			String featureEntry = testFeatures.get(idx - 1);
+			String id = extractTCId(featureEntry);
+			int numGold = aMapping.get(split[0]);
+			int numPred = aMapping.get(split[1]);
+			String propEntry = numPred + SEPARATOR_CHAR + numGold;
+			props.setProperty(id, propEntry);
+		}
 
-            String id = extractTCId(testFeatures.get(idx - 1));
-            int numGold = aMapping.get(split[0]);
-            int numPred = aMapping.get(split[1]);
-            String propEntry = numPred + SEPARATOR_CHAR + numGold;
-            props.setProperty(id, propEntry);
-        }
+		return props;
+	}
 
-        return props;
-    }
+	private static String extractTCId(String line) {
+		int begin = line.indexOf(ID_CONSTANT_VALUE);
+		int end = line.indexOf("\t", begin + ID_CONSTANT_VALUE.length());
 
-    private static String extractTCId(String line)
-    {
-        int begin = line.indexOf(ID_CONSTANT_VALUE);
-        int end = line.indexOf("\t", begin + ID_CONSTANT_VALUE.length());
-
-        // Assuming the ID is at the end of the line:
-        String id = line.substring(begin + ID_CONSTANT_VALUE.length(), line.length());
-        // But in case it's not:
-        if (end != -1) {
-            id = line.substring(begin + ID_CONSTANT_VALUE.length(), end);
-        }
-        return id;
-    }
+		// Assuming the ID is at the end of the line:
+		String id = line.substring(begin + ID_CONSTANT_VALUE.length(),
+				line.length());
+		// But in case it's not:
+		if (end != -1) {
+			id = line.substring(begin + ID_CONSTANT_VALUE.length(), end);
+		}
+		return id;
+	}
 }
