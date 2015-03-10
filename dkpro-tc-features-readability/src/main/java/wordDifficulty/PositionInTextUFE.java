ddc2eeb31706907f73cb0cf483f32c9229fe860b
beinborn <beinborn@users.noreply.github.com>
2015-03-10 16:45:52 +0000

    Simple extractors for measuring word difficulty. Unit tests will follow later.

---
+++ dkpro-tc-features-readability/src/main/java/wordDifficulty/PositionInTextUFE.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package wordDifficulty;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.tc.api.exception.TextClassificationException;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.ClassificationUnitFeatureExtractor;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
+import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
+import de.tudarmstadt.ukp.dkpro.tc.api.type.TextClassificationUnit;
+
+public class PositionInTextUFE
+    extends FeatureExtractorResource_ImplBase
+    implements ClassificationUnitFeatureExtractor
+{
+    /**
+     * @author beinborn
+     * 
+     *         In this feature extractor we simply return the absolute position of the word by
+     *         counting the tokens. The relative position is then calculated by dividing by the
+     *         number of words). Alternative solutions could also take the sentences into account.
+     */
+    public static final String ABS_POSITION = "PositionOfWord";
+    public static final String REL_POSITION = "RelativePositionOfWord";
+
+    @Override
+    public List<Feature> extract(JCas jcas, TextClassificationUnit classificationUnit)
+        throws TextClassificationException
+    {
+        List<Feature> featList = new ArrayList<Feature>();
+
+        Collection<Token> tokens = JCasUtil.select(jcas, Token.class);
+        int position = 0;
+        for (Token tok : tokens) {
+            position++;
+            if (tok.getBegin() == classificationUnit.getBegin()) {
+                break;
+            }
+        }
+        double relPosition = position / (double) Math.max(tokens.size(), 1);
+        featList.add(new Feature(ABS_POSITION, position));
+        featList.add(new Feature(REL_POSITION, relPosition));
+        return featList;
+    }
+}
