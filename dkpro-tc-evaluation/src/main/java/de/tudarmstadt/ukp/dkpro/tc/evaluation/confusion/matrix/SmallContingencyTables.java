281fdaa74ae8a09a7740f9c25a512395a67904d7
Johannes Daxenberger <daxenberger@ukp.informatik.tu-darmstadt.de>
2015-03-17 16:13:17 +0000

    Fixing license headers

--- dkpro-tc-evaluation/src/main/java/de/tudarmstadt/ukp/dkpro/tc/evaluation/confusion/matrix/SmallContingencyTables.java
+++ dkpro-tc-evaluation/src/main/java/de/tudarmstadt/ukp/dkpro/tc/evaluation/confusion/matrix/SmallContingencyTables.java
@@ -1,27 +1,26 @@
-/*
+/*******************************************************************************
  * Copyright 2014
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.tc.evaluation.confusion.matrix;
 
 import java.util.Arrays;
 import java.util.Map;
 
-import de.tudarmstadt.ukp.dkpro.tc.evaluation.evaluator.EvaluatorFactory;
+import de.tudarmstadt.ukp.dkpro.tc.evaluation.Id2Outcome;
 
 public class SmallContingencyTables {
 
@@ -33,7 +32,7 @@ public class SmallContingencyTables {
 		if (classNames.length == 0) {
 			throw new IllegalArgumentException("Must at least specify on class name.");
 		}
-        this.class2Number = EvaluatorFactory.classNamesToMapping(Arrays.asList(classNames));
+        this.class2Number = Id2Outcome.classNamesToMapping(Arrays.asList(classNames));
 		this.smallContingencyTables = new double[class2Number.size()][2][2];
 	}
 	
--- dkpro-tc-evaluation/src/main/java/de/tudarmstadt/ukp/dkpro/tc/evaluation/measures/label/MacroRecall.java
+++ dkpro-tc-evaluation/src/main/java/de/tudarmstadt/ukp/dkpro/tc/evaluation/measures/label/MacroRecall.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright 2014
  * Ubiquitous Knowledge Processing (UKP) Lab
- * Technische Universit�t Darmstadt
+ * Technische Universität Darmstadt
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
