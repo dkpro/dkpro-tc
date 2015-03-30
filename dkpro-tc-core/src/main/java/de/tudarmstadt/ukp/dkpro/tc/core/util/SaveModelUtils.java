8546c7a6f0fbfddf5cea4ef6f40af05bfa13c2a8
Tobias Horsmann <tobias.horsmann@gmail.com>
2015-03-30 14:56:45 +0000

    Issue 234:	CRFSuite: Creating standalone runnable jars with self-trained models -feature parameter are tested if they point to a valid location in the file system, if yes the file/folder is copied to the output folder of the model

--- dkpro-tc-core/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/util/SaveModelUtils.java
+++ dkpro-tc-core/src/main/java/de/tudarmstadt/ukp/dkpro/tc/core/util/SaveModelUtils.java
@@ -20,7 +20,12 @@ package de.tudarmstadt.ukp.dkpro.tc.core.util;
 import static de.tudarmstadt.ukp.dkpro.tc.core.task.MetaInfoTask.META_KEY;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -88,17 +93,76 @@ public class SaveModelUtils
         for (Entry<String, String> entry : metaParameterKeyPairs.entrySet()) {
             File file = new File(aContext.getStorageLocation(META_KEY, AccessMode.READWRITE),
                     entry.getValue());
-            parameterProperties.put(entry.getKey(), file.getAbsolutePath());
+
+            String name = file.getName();
+            String subFolder = aOutputFolder.getAbsoluteFile() + "/" + name;
+            File targetFolder = new File(subFolder);
+            copyToTargetLocation(file, targetFolder);
+            parameterProperties.put(entry.getKey(), name);
+
+            // should never be reached
         }
 
         for (int i = 0; i < aFeatureParameters.size(); i = i + 2) {
-            parameterProperties.put((String) aFeatureParameters.get(i).toString(),
-                    aFeatureParameters.get(i + 1).toString());
+
+            String key = (String) aFeatureParameters.get(i).toString();
+            String value = aFeatureParameters.get(i + 1).toString();
+
+            if (valueExistAsFileOrFolderInTheFileSystem(value)) {
+                String name = new File(value).getName();
+                String destination = aOutputFolder + "/" + name;
+                copyToTargetLocation(new File(value), new File(destination));
+                parameterProperties.put(key, name);
+                continue;
+            }
+            parameterProperties.put(key, value);
         }
 
         FileWriter writer = new FileWriter(new File(aOutputFolder, MODEL_PARAMETERS));
         parameterProperties.store(writer, "");
         writer.close();
+    }
+
+    private static boolean valueExistAsFileOrFolderInTheFileSystem(String aValue)
+    {
+        return new File(aValue).exists();
+    }
+
+    private static void copyToTargetLocation(File source, File destination)
+        throws IOException
+    {
+
+        if (source.isDirectory()) {
+            if (!destination.exists()) {
+                destination.mkdir();
+            }
+
+            for (String file : source.list()) {
+                File src = new File(source, file);
+                File dest = new File(destination, file);
+                copyToTargetLocation(src, dest);
+            }
+
+        }
+        else {
+            copySingleFiles(source, destination);
+        }
+    }
+
+    private static void copySingleFiles(File source, File destination)
+        throws IOException
+    {
+        InputStream inputstream = new FileInputStream(source);
+        OutputStream outputstream = new FileOutputStream(destination);
+
+        int length;
+        byte[] buffer = new byte[1024];
+        while ((length = inputstream.read(buffer)) > 0) {
+            outputstream.write(buffer, 0, length);
+        }
+
+        inputstream.close();
+        outputstream.close();
     }
 
     public static void writeModelAdapterInformation(File aOutputFolder, String aModelMeta)
