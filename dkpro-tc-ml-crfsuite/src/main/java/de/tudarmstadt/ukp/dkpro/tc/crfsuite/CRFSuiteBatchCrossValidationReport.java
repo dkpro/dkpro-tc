563116b37dea3cc93a3ac28214b9efc0c28bd922
Tobias Horsmann <tobias.horsmann@gmail.com>
2015-04-24 12:13:56 +0000

    Issue 234:	CRFSuite: Creating standalone runnable jars with self-trained models

--- dkpro-tc-ml-crfsuite/src/main/java/de/tudarmstadt/ukp/dkpro/tc/crfsuite/CRFSuiteBatchCrossValidationReport.java
+++ dkpro-tc-ml-crfsuite/src/main/java/de/tudarmstadt/ukp/dkpro/tc/crfsuite/CRFSuiteBatchCrossValidationReport.java
@@ -41,7 +41,6 @@ import de.tudarmstadt.ukp.dkpro.lab.storage.StorageService;
 import de.tudarmstadt.ukp.dkpro.lab.storage.impl.PropertiesAdapter;
 import de.tudarmstadt.ukp.dkpro.lab.task.Task;
 import de.tudarmstadt.ukp.dkpro.lab.task.TaskContextMetadata;
-import de.tudarmstadt.ukp.dkpro.lab.task.impl.BatchTask;
 import de.tudarmstadt.ukp.dkpro.tc.core.Constants;
 import de.tudarmstadt.ukp.dkpro.tc.core.util.ReportConstants;
 import de.tudarmstadt.ukp.dkpro.tc.core.util.ReportUtils;
