7d14a95f1354cafef777ea3f7a01693e1ac20d00
Andriy Nadolskyy <nadolskyy@gmail.com>
2015-07-07 01:17:44 +0200

    fixes issue 83

--- dkpro-tc-features-ngram/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/FrequencyDistributionNGramDFE.java
+++ dkpro-tc-features-ngram/src/main/java/de/tudarmstadt/ukp/dkpro/tc/features/ngram/FrequencyDistributionNGramDFE.java
@@ -42,36 +42,46 @@ public class FrequencyDistributionNGramDFE
     public List<Feature> extract(JCas jcas)
         throws TextClassificationException
     {
-    	double totalCountDocumentNgrams = 0;
         List<Feature> features = new ArrayList<Feature>();
         FrequencyDistribution<String> documentNgrams = null;
         documentNgrams = NGramUtils.getDocumentNgrams(jcas, ngramLowerCase,
                 filterPartialStopwordMatches, ngramMinN, ngramMaxN, stopwords);
         
-        for (String ngram : documentNgrams.getKeys()) {
-        	totalCountDocumentNgrams += documentNgrams.getCount(ngram);
-        }
-
-        for (String topNgram : topKSet.getKeys()) {
-        	double tf = 0;
-        	double idf = 0;
-        	double tfIdf = 0;
-        	
-        	if (documentNgrams.getKeys().contains(topNgram)) {
-        		// calculate the TF value: the occurrences number of the current n-gram in the document
-            	// divided by the total number of n-gram occurrences in the document 
-        		tf = documentNgrams.getCount(topNgram) / totalCountDocumentNgrams;
-        		
-            	// calculate the IDF value: logarithm of dividing the total number of documents by 
-            	// the number of documents containing the current top n-gram
-            	idf = Math.log((double) dfStore.getDocumentCount() / dfStore.getDf(topNgram));
-            			
-    			// calculate the TF-IDF value
-            	tfIdf = tf * idf;	
+        if (tfIdfCalculation.equals("true")) {
+        	double countCurrentDocumentNgrams = 0;
+            for (String ngram : documentNgrams.getKeys()) {
+            	countCurrentDocumentNgrams += documentNgrams.getCount(ngram);
             }
         	
-        	features.add(new Feature(getFeaturePrefix() + "_" + topNgram, tfIdf));
-        }
+        	for (String topNgram : topKSet.getKeys()) {
+            	double tf = 0;
+            	double idf = 0;
+            	double tfIdf = 0;          	
+            	
+            	if (documentNgrams.getKeys().contains(topNgram)) {
+            		// calculate the TF value: the occurrences number of the current n-gram in the document
+                	// divided by the total number of n-gram occurrences in the document 
+            		tf = documentNgrams.getCount(topNgram) / countCurrentDocumentNgrams;
+            		
+                	// calculate the IDF value: natural logarithm of dividing the total number of documents 
+            		// by the number of documents containing the current top n-gram
+                	idf = Math.log((double) dfStore.getDocumentCount() / dfStore.getDf(topNgram));
+                			
+        			// calculate the TF-IDF value
+                	tfIdf = tf * idf;	
+                }            	
+            	features.add(new Feature(getFeaturePrefix() + "_" + topNgram, tfIdf));
+            }            
+    	} else if (tfIdfCalculation.equals("false")){
+    		for (String topNgram : topKSet.getKeys()) {
+                if (documentNgrams.getKeys().contains(topNgram)) {
+                    features.add(new Feature(getFeaturePrefix() + "_" + topNgram, 1));
+                }
+                else {
+                    features.add(new Feature(getFeaturePrefix() + "_" + topNgram, 0));
+                }
+            }
+    	}        
         return features;
     }
 }
