a5e060041a46cce9210d761b105f47e61f04af2d
Tobias Horsmann <tobias.horsmann@gmail.com>
2015-09-24 09:05:50 +0200

    Issue #95: Less arrays hopefully less memory consumption

--- dkpro-tc-fstore-simple/src/main/java/de/tudarmstadt/ukp/dkpro/tc/fstore/simple/DenseFeatureStore.java
+++ dkpro-tc-fstore-simple/src/main/java/de/tudarmstadt/ukp/dkpro/tc/fstore/simple/DenseFeatureStore.java
@@ -18,12 +18,8 @@
 package de.tudarmstadt.ukp.dkpro.tc.fstore.simple;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -42,11 +38,7 @@ import de.tudarmstadt.ukp.dkpro.tc.api.features.Instance;
 public class DenseFeatureStore
     implements FeatureStore
 {
-    private List<List<Object>> instanceList;
-    private List<List<String>> outcomeList;
-    private List<Double> weightList;
-    private List<Integer> sequenceIds;
-    private List<Integer> sequencePositions;
+	private List<Instance> instanceList;
     private TreeSet<String> featureNames;
 
     /**
@@ -54,11 +46,7 @@ public class DenseFeatureStore
      */
     public DenseFeatureStore()
     {
-        this.instanceList = new ArrayList<List<Object>>();
-        this.outcomeList = new ArrayList<List<String>>();
-        this.weightList = new ArrayList<Double>();
-        this.sequenceIds = new ArrayList<Integer>();
-        this.sequencePositions = new ArrayList<Integer>();
+    	this.instanceList = new ArrayList<Instance>();
         this.featureNames = null;
     }
 
@@ -91,81 +79,38 @@ public class DenseFeatureStore
                     "One or more, but not all of your instances return the following feature(s): "
                             + StringUtils.join(symDiff, " and "));
         }
+        
+        instanceList.add(instance);
 
-        // create map of feature names and offset in set
-        Map<String, Integer> sortedFeatureNameMap = new HashMap<String, Integer>();
-        int offset = 0;
-        Iterator<String> iterator = featureNames.iterator();
-        while (iterator.hasNext()) {
-            sortedFeatureNameMap.put(iterator.next(), offset);
-            offset++;
-        }
-
-        Object[] values = new Object[featureNames.size()];
-        for (Feature feature : instance.getFeatures()) {
-            values[sortedFeatureNameMap.get(feature.getName())] = feature.getValue();
-        }
-        this.instanceList.add(Arrays.asList(values));
-        this.outcomeList.add(instance.getOutcomes());
-        this.weightList.add(instance.getWeight());
-        this.sequenceIds.add(instance.getSequenceId());
-        this.sequencePositions.add(instance.getSequencePosition());
     }
 
     @Override
     public Instance getInstance(int i) 
     {
-        List<Feature> features = new ArrayList<Feature>();
-
-        List<Object> values = instanceList.get(i);
-
-        int offset = 0;
-        Iterator<String> sortedNames = getFeatureNames().iterator();
-        while (sortedNames.hasNext()) {
-            String name = sortedNames.next();
-            Feature feature = new Feature(name, values.get(offset));
-            features.add(feature);
-            offset++;
-        }
-
-        Instance instance = new Instance(features, outcomeList.get(i));
-        instance.setWeight(weightList.get(i));
-        instance.setSequenceId(sequenceIds.get(i));
-        instance.setSequencePosition(sequencePositions.get(i));
-        return instance;
+    	return instanceList.get(i);
     }
 
     @Override
     public List<String> getOutcomes(int i)
     {
-        return this.outcomeList.get(i);
+        return instanceList.get(i).getOutcomes();
     }
     
     @Override
     public Double getWeight(int i)
     {
-        return this.weightList.get(i);
+        return instanceList.get(i).getWeight();
     }
-
-    // public List<List<String>> getOutcomeLists()
-    // {
-    // return outcomeList;
-    // }
 
     @Override
     public SortedSet<String> getUniqueOutcomes()
     {
         SortedSet<String> uniqueOutcomes = new TreeSet<String>();
-        for (List<String> outcomes : outcomeList) {
-            uniqueOutcomes.addAll(outcomes);
+        for(Instance ins : instanceList){
+            uniqueOutcomes.addAll(ins.getOutcomes());
         }
         return uniqueOutcomes;
     }
-
-    // public void setOutcomeList(List<List<String>> outcomeList)
-    // {
-    // this.outcomeList = outcomeList;
-    // }
 
     @Override
     public int getNumberOfInstances()
@@ -195,10 +140,6 @@ public class DenseFeatureStore
     {
         return "SparseFeatureStore{" +
                 "instanceList=" + instanceList +
-                ", outcomeList=" + outcomeList +
-                ", weightList=" + weightList +
-                ", sequenceIds=" + sequenceIds +
-                ", sequencePositions=" + sequencePositions +
                 '}';
     }
 
