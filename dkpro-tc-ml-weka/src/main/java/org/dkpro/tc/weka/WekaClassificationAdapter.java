4e416661efd8b6f28dde34827b1c1db4d9bd1259
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-05-08 18:16:16 +0200

    Issue #50: imports

--- dkpro-tc-ml-weka/src/main/java/org/dkpro/tc/weka/WekaClassificationAdapter.java
+++ dkpro-tc-ml-weka/src/main/java/org/dkpro/tc/weka/WekaClassificationAdapter.java
@@ -25,7 +25,6 @@ import org.dkpro.lab.task.Dimension;
 import org.dkpro.lab.task.impl.DimensionBundle;
 import org.dkpro.lab.task.impl.ExecutableTaskBase;
 import org.dkpro.lab.task.impl.FoldDimensionBundle;
-
 import org.dkpro.tc.core.io.DataWriter;
 import org.dkpro.tc.core.ml.ModelSerialization_ImplBase;
 import org.dkpro.tc.core.ml.TCMachineLearningAdapter;
