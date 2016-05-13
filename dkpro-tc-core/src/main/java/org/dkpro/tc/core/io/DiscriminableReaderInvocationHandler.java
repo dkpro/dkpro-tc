f18fe8216fccd8ba7c97af199084d2d31113bffc
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-05-13 10:12:13 +0200

    Issue #353: discriminator value

--- dkpro-tc-core/src/main/java/org/dkpro/tc/core/io/DiscriminableReaderInvocationHandler.java
+++ dkpro-tc-core/src/main/java/org/dkpro/tc/core/io/DiscriminableReaderInvocationHandler.java
@@ -19,55 +19,101 @@ package org.dkpro.tc.core.io;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.resource.metadata.ConfigurationParameterSettings;
+import org.apache.uima.resource.metadata.NameValuePair;
+import org.apache.uima.resource.metadata.ResourceMetaData;
 import org.dkpro.lab.task.Discriminable;
 
-public class DiscriminableReaderInvocationHandler implements InvocationHandler, Discriminable, DiscriminableReaderDescription
+public class DiscriminableReaderInvocationHandler
+    implements InvocationHandler, Discriminable, DiscriminableReaderDescription
 {
-    
+
     private CollectionReaderDescription crd;
 
-
-    public DiscriminableReaderInvocationHandler(Object crd){
-        this.crd = (CollectionReaderDescription)crd;
+    public DiscriminableReaderInvocationHandler(Object crd)
+    {
+        this.crd = (CollectionReaderDescription) crd;
     }
-    
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args)
         throws Throwable
     {
-        
-        if(method.getName().equals("getDiscriminatorValue")){
+
+        if (method.getName().equals("getDiscriminatorValue")) {
             return getDiscriminatorValue();
         }
-        if(method.getName().equals("getActualValue")){
+        if (method.getName().equals("getActualValue")) {
             return getActualValue();
         }
-        if(method.getName().equals("getReaderDescription")){
+        if (method.getName().equals("getReaderDescription")) {
             return getReaderDescription();
         }
-        
+
         return method.invoke(crd, args);
     }
-
 
     @Override
     public Object getDiscriminatorValue()
     {
-        String string = crd.getCollectionReaderMetaData().toString();
-        return DigestUtils.md5Hex(string);
+        ResourceMetaData metaData = crd.getMetaData();
+        ConfigurationParameterSettings configurationParameterSettings = metaData
+                .getConfigurationParameterSettings();
+        NameValuePair[] parameterSettings = configurationParameterSettings.getParameterSettings();
+
+        String implementationName = crd.getImplementationName();
+
+        List<String> entries = new ArrayList<>();
+        entries.add(implementationName);
+        entries = addParameters(entries, parameterSettings);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < entries.size(); i++) {
+            sb.append(entries.get(i));
+            if (i + 1 < entries.size()) {
+                sb.append(", ");
+            }
+        }
+
+        String description = "MD5: " + DigestUtils.md5Hex(sb.toString()) + ", " + sb.toString();
+        return description;
     }
 
+    private List<String> addParameters(List<String> entries, NameValuePair[] parameterSettings)
+    {
+        for (int i = 0; i < parameterSettings.length; i++) {
+            NameValuePair nvp = parameterSettings[i];
+            Object value = nvp.getValue();
+            StringBuilder sb = new StringBuilder();
+            if (value instanceof Object[]) {
+                Object[] x = (Object[]) value;
+                for (int k = 0; k < x.length; k++) {
+                    Object object = x[k];
+                    sb.append(object);
+                    if (k + 1 < x.length) {
+                        sb.append(", ");
+                    }
+                }
+                entries.add(nvp.getName() + "=" + sb.toString());
+            }
+            else {
+                entries.add(nvp.getName() + "=" + nvp.getValue());
+            }
+        }
+
+        return entries;
+    }
 
     @Override
     public Object getActualValue()
     {
         return getDiscriminatorValue();
     }
-
 
     @Override
     public CollectionReaderDescription getReaderDescription()
