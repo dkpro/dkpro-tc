9db7dcdaa73cf62fadf8b21fb81309f92364c47a
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-06-05 22:16:32 +0200

    Issue #355: broken import

--- dkpro-tc-core/src/main/java/org/dkpro/tc/core/feature/NoopFeatureExtractor.java
+++ dkpro-tc-core/src/main/java/org/dkpro/tc/core/feature/NoopFeatureExtractor.java
@@ -21,19 +21,19 @@ import java.util.Collections;
 import java.util.Set;
 
 import org.apache.uima.jcas.JCas;
-
 import org.dkpro.tc.api.exception.TextClassificationException;
-import org.dkpro.tc.api.features.DocumentFeatureExtractor;
+import org.dkpro.tc.api.features.ClassificationUnitFeatureExtractor;
 import org.dkpro.tc.api.features.Feature;
 import org.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
+import org.dkpro.tc.api.type.TextClassificationUnit;
 
 public class NoopFeatureExtractor
 	extends FeatureExtractorResource_ImplBase
-	implements DocumentFeatureExtractor
+	implements ClassificationUnitFeatureExtractor
 {
 
 	@Override
-	public Set<Feature> extract(JCas view) throws TextClassificationException {
+	public Set<Feature> extract(JCas view, TextClassificationUnit target) throws TextClassificationException {
 		return Collections.<Feature>emptySet();
 	}
 
