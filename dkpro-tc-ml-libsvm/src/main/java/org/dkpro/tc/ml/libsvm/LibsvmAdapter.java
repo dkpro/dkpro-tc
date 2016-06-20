e48cd72676e8eed39888022b81d025ddacbe2e15
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-06-20 08:53:20 +0200

    Issue #293: moved documentation to another class

--- dkpro-tc-ml-libsvm/src/main/java/org/dkpro/tc/ml/libsvm/LibsvmAdapter.java
+++ dkpro-tc-ml-libsvm/src/main/java/org/dkpro/tc/ml/libsvm/LibsvmAdapter.java
@@ -30,8 +30,29 @@ import org.dkpro.tc.core.ml.TCMachineLearningAdapter;
 import org.dkpro.tc.core.task.ModelSerializationTask;
 import org.dkpro.tc.fstore.simple.SparseFeatureStore;
 import org.dkpro.tc.ml.libsvm.serialization.LibsvmModelSerializationDescription;
+import org.dkpro.tc.ml.libsvm.serialization.LoadModelConnectorLibsvm;
 import org.dkpro.tc.ml.report.InnerBatchUsingTCEvaluationReport;
 
+/**
+ * Libsvm offers various configuration switches for the SVM type and Kernel we defined constant
+ * values in this class with more detail explanations.
+ * 
+ * <pre>
+ * -s svm_type (see constants)
+ * -s kernel_type (see constants) 
+ * -d degree : set degree in kernel function (default 3) 
+ * -g gamma : set gamma in kernel function (default 1/num_features) 
+ * -r coef0 : set coef0 in kernel function (default 0)
+ * -c cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR (default 1)
+ * -n nu : set the parameter nu of nu-SVC, one-class SVM, and nu-SVR (default 0.5) 
+ * -p epsilon : set the epsilon in loss function of epsilon-SVR (default 0.1) 
+ * -m cachesize : set cache memory size in MB (default 100) 
+ * -e epsilon : set tolerance of termination criterion (default 0.001) 
+ * -h shrinking: whether to use the shrinking heuristics, 0 or 1 (default 1) 
+ * -b probability_estimates : whether to train a SVC or SVR model for probability estimates, 0 or 1 (default 0) 
+ * -wi weight : set the parameter C of class i to weight*C, for C-SVC (default 1)
+ * </pre>
+ */
 public class LibsvmAdapter
     implements TCMachineLearningAdapter
 {
@@ -111,7 +132,7 @@ public class LibsvmAdapter
     @Override
     public Class<? extends ModelSerialization_ImplBase> getLoadModelConnectorClass()
     {
-        return null;
+        return LoadModelConnectorLibsvm.class;
     }
 
     @Override
@@ -125,5 +146,30 @@ public class LibsvmAdapter
     {
         return SparseFeatureStore.class.getName();
     }
+
+
+    /** SVM type is set by switch [-s] */
+    public static final String PARAM_SVM_TYPE_C_SVC_MULTI_CLASS = "0";
+    /** SVM type is set by switch [-s] */
+    public static final String PARAM_SVM_TYPE_NU_SVC_MULTI_CLASS = "1";
+    /** SVM type is set by switch [-s] */
+    public static final String PARAM_SVM_TYPE_ONE_CLASS_SVM = "2";
+    /** SVM type is set by switch [-s] */
+    public static final String PARAM_SVM_TYPE_EPISLON_SVR_REGRESSION = "3";
+    /** SVM type is set by switch [-s] */
+    public static final String PARAM_SVM_TYPE_NU_SVR_REGRESSION = "4";
+
+    /** Polynomial kernel = u'*v set by switch [-t] (default) */
+    public static final String PARAM_KERNEL_LINEAR = "0";
+    /** Polynomial kernel = (gamma*u'*v + coef0)^degree set by switch [-t] */
+    public static final String PARAM_KERNEL_POLYNOMIAL = "1";
+    /** Radial based = exp(-gamma*|u-v|^2) set by switch [-t] */
+    public static final String PARAM_KERNEL_RADIAL_BASED = "2";
+    /** sigmoid = tanh(gamma*u'*v + coef0) set by switch [-t] */
+    public static final String PARAM_KERNEL_SIGMOID = "3";
+    /** precomputed kernel (kernel values in training_set_file) set by switch [-t] */
+    public static final String PARAM_KERNEL_PRE_COMPUTED = "4";
+
     
 }
+
--- dkpro-tc-ml-libsvm/src/main/java/org/dkpro/tc/ml/libsvm/LibsvmTestTask.java
+++ dkpro-tc-ml-libsvm/src/main/java/org/dkpro/tc/ml/libsvm/LibsvmTestTask.java
@@ -48,26 +48,6 @@ import org.dkpro.tc.ml.libsvm.api.LibsvmTrainModel;
 import libsvm.svm;
 import libsvm.svm_model;
 
-/**
- * Libsvm offers various configuration switches for the SVM type and Kernel we defined constant
- * values in this class with more detail explanations.
- * 
- * <pre>
- * -s svm_type (see constants)
- * -s kernel_type (see constants) 
- * -d degree : set degree in kernel function (default 3) 
- * -g gamma : set gamma in kernel function (default 1/num_features) 
- * -r coef0 : set coef0 in kernel function (default 0)
- * -c cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR (default 1)
- * -n nu : set the parameter nu of nu-SVC, one-class SVM, and nu-SVR (default 0.5) 
- * -p epsilon : set the epsilon in loss function of epsilon-SVR (default 0.1) 
- * -m cachesize : set cache memory size in MB (default 100) 
- * -e epsilon : set tolerance of termination criterion (default 0.001) 
- * -h shrinking: whether to use the shrinking heuristics, 0 or 1 (default 1) 
- * -b probability_estimates : whether to train a SVC or SVR model for probability estimates, 0 or 1 (default 0) 
- * -wi weight : set the parameter C of class i to weight*C, for C-SVC (default 1)
- * </pre>
- */
 public class LibsvmTestTask
     extends ExecutableTaskBase
     implements Constants
@@ -264,27 +244,5 @@ public class LibsvmTestTask
 
         return sb.toString();
     }
-
-    /** SVM type is set by switch [-s] */
-    public static final String PARAM_SVM_TYPE_C_SVC_MULTI_CLASS = "0";
-    /** SVM type is set by switch [-s] */
-    public static final String PARAM_SVM_TYPE_NU_SVC_MULTI_CLASS = "1";
-    /** SVM type is set by switch [-s] */
-    public static final String PARAM_SVM_TYPE_ONE_CLASS_SVM = "2";
-    /** SVM type is set by switch [-s] */
-    public static final String PARAM_SVM_TYPE_EPISLON_SVR_REGRESSION = "3";
-    /** SVM type is set by switch [-s] */
-    public static final String PARAM_SVM_TYPE_NU_SVR_REGRESSION = "4";
-
-    /** Polynomial kernel = u'*v, set by switch [-t] (default) */
-    public static final String PARAM_KERNEL_LINEAR = "0";
-    /** Polynomial kernel = (gamma*u'*v + coef0)^degree, set by switch [-t] */
-    public static final String PARAM_KERNEL_POLYNOMIAL = "1";
-    /** Radial based = exp(-gamma*|u-v|^2), set by switch [-t] */
-    public static final String PARAM_KERNEL_RADIAL_BASED = "2";
-    /** sigmoid = tanh(gamma*u'*v + coef0), set by switch [-t] */
-    public static final String PARAM_KERNEL_SIGMOID = "3";
-    /** precomputed kernel (kernel values in training_set_file), set by switch [-t] */
-    public static final String PARAM_KERNEL_PRE_COMPUTED = "4";
 
 }
