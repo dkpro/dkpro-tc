c6d3abca18b3f46b69586ece673b388c4b031517
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-06-26 17:03:35 +0200

    Issue #188: simplified annotator

--- dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/io/anno/SequenceTargetPosTaggingPreprocessing.java
+++ dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/io/anno/SequenceTargetPosTaggingPreprocessing.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
-import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.dkpro.tc.api.type.TextClassificationOutcome;
@@ -18,10 +17,6 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 public class SequenceTargetPosTaggingPreprocessing
     extends JCasAnnotator_ImplBase
 {
-    public static final String PARAM_USE_COARSE_GRAINED = "useCoarseGrained";
-    @ConfigurationParameter(name = PARAM_USE_COARSE_GRAINED, mandatory = true, defaultValue = "false")
-    protected boolean useCoarseGrained;
-
     int tcId = 0;
 
     @Override
@@ -36,8 +31,8 @@ public class SequenceTargetPosTaggingPreprocessing
             List<Token> tokens = JCasUtil.selectCovered(aJCas, Token.class, sent);
 
             for (Token token : tokens) {
-                TextClassificationTarget unit = new TextClassificationTarget(aJCas, token.getBegin(),
-                        token.getEnd());
+                TextClassificationTarget unit = new TextClassificationTarget(aJCas,
+                        token.getBegin(), token.getEnd());
                 unit.setId(tcId++);
                 unit.setSuffix(token.getCoveredText());
                 unit.addToIndexes();
@@ -54,15 +49,7 @@ public class SequenceTargetPosTaggingPreprocessing
     public String getTextClassificationOutcome(JCas jcas, TextClassificationTarget unit)
     {
         List<POS> posList = JCasUtil.selectCovered(jcas, POS.class, unit);
-
-        String outcome = "";
-        if (useCoarseGrained) {
-            outcome = posList.get(0).getClass().getSimpleName();
-        }
-        else {
-            outcome = posList.get(0).getPosValue();
-        }
-        return outcome;
+        return posList.get(0).getPosValue();
     }
 
 }
