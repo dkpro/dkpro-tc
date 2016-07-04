22e4d978e38f0bebb901de80d2811d258fe26bca
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-07-04 16:37:17 +0200

    Issue #369: Weka demo added

---
+++ dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/io/EssayScoreReader.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2016
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package org.dkpro.tc.examples.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.fit.component.JCasCollectionReader_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.util.Progress;
+import org.dkpro.tc.api.type.TextClassificationOutcome;
+
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
+
+public class EssayScoreReader
+    extends JCasCollectionReader_ImplBase
+
+{
+    public static final String PARAM_SOURCE_LOCATION = ComponentParameters.PARAM_SOURCE_LOCATION;
+    @ConfigurationParameter(name = PARAM_SOURCE_LOCATION, mandatory = true)
+    private File inputFile;
+
+    /**
+     * Character encoding of the input data
+     */
+    public static final String PARAM_ENCODING = ComponentParameters.PARAM_SOURCE_ENCODING;
+    @ConfigurationParameter(name = PARAM_ENCODING, mandatory = true, defaultValue = "UTF-8")
+    private String encoding;
+
+    /**
+     * Language of the input data
+     */
+    public static final String PARAM_LANGUAGE = ComponentParameters.PARAM_LANGUAGE;
+    @ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = true)
+    private String language;
+
+    String text = null;
+    String score = null;
+
+    private BufferedReader br;
+
+    int id=0;
+    @Override
+    public void getNext(JCas aJCas)
+        throws IOException, CollectionException
+    {
+        DocumentMetaData dmd = new DocumentMetaData(aJCas);
+        dmd.setDocumentTitle("");
+        dmd.setDocumentId(id++ + "");
+        dmd.addToIndexes();
+
+        aJCas.setDocumentText(text);
+
+        TextClassificationOutcome o = new TextClassificationOutcome(aJCas, 0,
+                aJCas.getDocumentText().length());
+        o.setOutcome(score);
+        o.addToIndexes();
+    }
+
+    @Override
+    public boolean hasNext()
+    {
+        try {
+            if (br == null) {
+                br = new BufferedReader(
+                        new InputStreamReader(new FileInputStream(inputFile), "utf-8"));
+            }
+
+            String entry = br.readLine();
+
+            if (entry == null || entry.isEmpty()) {
+                return false;
+            }
+
+            String[] split = entry.split("\t");
+            score = split[0];
+            text = split[1];
+            return true;
+        }
+        catch (Exception e) {
+            throw new UnsupportedOperationException(e);
+        }
+    }
+
+    @Override
+    public Progress[] getProgress()
+    {
+        return null;
+    }
+
+}
---
+++ dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/regression/WekaRegressionDemo.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright 2016
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package org.dkpro.tc.examples.regression;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.dkpro.lab.Lab;
+import org.dkpro.lab.task.Dimension;
+import org.dkpro.lab.task.ParameterSpace;
+import org.dkpro.tc.core.Constants;
+import org.dkpro.tc.examples.io.EssayScoreReader;
+import org.dkpro.tc.examples.util.DemoUtils;
+import org.dkpro.tc.features.length.NrOfSentences;
+import org.dkpro.tc.features.length.NrOfTokens;
+import org.dkpro.tc.features.length.NrOfTokensPerSentence;
+import org.dkpro.tc.ml.ExperimentTrainTest;
+import org.dkpro.tc.ml.report.BatchTrainTestReport;
+import org.dkpro.tc.weka.WekaClassificationAdapter;
+
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import weka.classifiers.functions.LinearRegression;
+
+public class WekaRegressionDemo
+    implements Constants
+{
+
+    public static void main(String[] args)
+        throws Exception
+    {
+
+        // This is used to ensure that the required DKPRO_HOME environment variable is set.
+        // Ensures that people can run the experiments even if they haven't read the setup
+        // instructions first :)
+        // Don't use this in real experiments! Read the documentation and set DKPRO_HOME as
+        // explained there.
+        DemoUtils.setDkproHome(WekaRegressionDemo.class.getSimpleName());
+
+        ParameterSpace pSpace = getParameterSpace();
+
+        WekaRegressionDemo experiment = new WekaRegressionDemo();
+        experiment.runTrainTest(pSpace);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static ParameterSpace getParameterSpace()
+        throws ResourceInitializationException
+    {
+        // configure training and test data reader dimension
+        // train/test will use both, while cross-validation will only use the train part
+        // The reader is also responsible for setting the labels/outcome on all
+        // documents/instances it creates.
+        Map<String, Object> dimReaders = new HashMap<String, Object>();
+
+        CollectionReaderDescription readerTrain = CollectionReaderFactory.createReaderDescription(
+                EssayScoreReader.class, EssayScoreReader.PARAM_SOURCE_LOCATION,
+                "src/main/resources/data/essays/train/essay_train.txt", EssayScoreReader.PARAM_LANGUAGE, "en"
+                );
+        dimReaders.put(DIM_READER_TRAIN, readerTrain);
+
+        CollectionReaderDescription readerTest = CollectionReaderFactory.createReaderDescription(
+                EssayScoreReader.class, EssayScoreReader.PARAM_SOURCE_LOCATION,
+                "src/main/resources/data/essays/test/essay_test.txt", EssayScoreReader.PARAM_LANGUAGE, "en");
+        dimReaders.put(DIM_READER_TEST, readerTest);
+
+        Dimension<List<String>> dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
+                Arrays.asList(new String[] { LinearRegression.class.getName() }));
+
+        Dimension<List<String>> dimFeatureSets = Dimension.create(DIM_FEATURE_SET,
+                Arrays.asList(new String[] { NrOfTokens.class.getName(), NrOfSentences.class.getName(), NrOfTokensPerSentence.class.getName() }));
+
+        ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle("readers", dimReaders),
+                Dimension.create(DIM_LEARNING_MODE, LM_REGRESSION),
+                Dimension.create(DIM_FEATURE_MODE, FM_DOCUMENT), dimFeatureSets,
+                dimClassificationArgs);
+
+        return pSpace;
+    }
+
+
+    // ##### TRAIN-TEST #####
+    protected void runTrainTest(ParameterSpace pSpace)
+        throws Exception
+    {
+        ExperimentTrainTest batch = new ExperimentTrainTest("RegressionDemo",
+                WekaClassificationAdapter.class);
+        batch.setPreprocessing(getPreprocessing());
+        batch.setParameterSpace(pSpace);
+        batch.addReport(BatchTrainTestReport.class);
+
+        // Run
+        Lab.getInstance().run(batch);
+    }
+
+    protected AnalysisEngineDescription getPreprocessing()
+        throws ResourceInitializationException
+    {
+        return createEngineDescription(createEngineDescription(BreakIteratorSegmenter.class));
+    }
+}
