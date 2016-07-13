22c09e2ce3d8fde0e5b08d5b57ffd5b9f74a731f
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-07-13 12:39:17 +0200

    Issue #378

--- dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/raw/JCasIdSetter.java
+++ dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/raw/JCasIdSetter.java
@@ -20,22 +20,27 @@ package org.dkpro.tc.examples.raw;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.dkpro.tc.api.type.JCasId;
 
-public class JCasIdSetter extends JCasAnnotator_ImplBase
+public class JCasIdSetter
+    extends JCasAnnotator_ImplBase
 {
 
     int jcasId;
-    
+
     @Override
     public void process(JCas arg0)
         throws AnalysisEngineProcessException
     {
-        JCasId id = new JCasId(arg0);
-        id.setId(jcasId++);
-        id.addToIndexes();
-        
+        boolean exists = JCasUtil.exists(arg0, JCasId.class);
+        if (!exists) {
+            JCasId id = new JCasId(arg0);
+            id.setId(jcasId++);
+            id.addToIndexes();
+        }
+
     }
 
 }
