093997d99c61e87f6d5853d481e467dbac67251c
Tobias Horsmann <tobias.horsmann@gmail.com>
2016-07-23 14:36:08 +0200

    Issue #39: demo

--- dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/single/pair/WekaPairTwentyNewsgroupsDemo.java
+++ dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/single/pair/WekaPairTwentyNewsgroupsDemo.java
@@ -25,15 +25,17 @@ import java.util.Map;
 
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.resource.ExternalResourceDescription;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.dkpro.lab.Lab;
 import org.dkpro.lab.task.BatchTask.ExecutionPolicy;
 import org.dkpro.lab.task.Dimension;
 import org.dkpro.lab.task.ParameterSpace;
 import org.dkpro.tc.core.Constants;
+import org.dkpro.tc.core.task.TcFeature;
+import org.dkpro.tc.core.util.TcFeatureFactory;
 import org.dkpro.tc.examples.io.PairTwentyNewsgroupsReader;
 import org.dkpro.tc.examples.util.DemoUtils;
-import org.dkpro.tc.features.ngram.base.FrequencyDistributionNGramFeatureExtractorBase;
 import org.dkpro.tc.features.pair.core.length.DiffNrOfTokensPairFeatureExtractor;
 import org.dkpro.tc.ml.ExperimentTrainTest;
 import org.dkpro.tc.weka.WekaClassificationAdapter;
@@ -48,11 +50,11 @@ import weka.classifiers.bayes.NaiveBayes;
  * file: see the files in src/main/resources/lists/ as examples.
  * <p>
  * PairTwentyNewsgroupsExperiment uses similar architecture as TwentyNewsgroupsGroovyExperiment (
- * {@link ExperimentTrainTest}) to automatically wire the standard tasks for a basic TrainTest setup.
- * To remind the user to be careful of information leak when training and testing on pairs of data
- * from similar sources, we do not provide a demo Cross Validation setup here. (Our sample train and
- * test datasets are from separate newsgroups.) Please see TwentyNewsgroupsGroovyExperiment for a
- * demo implementing a CV experiment.
+ * {@link ExperimentTrainTest}) to automatically wire the standard tasks for a basic TrainTest
+ * setup. To remind the user to be careful of information leak when training and testing on pairs of
+ * data from similar sources, we do not provide a demo Cross Validation setup here. (Our sample
+ * train and test datasets are from separate newsgroups.) Please see
+ * TwentyNewsgroupsGroovyExperiment for a demo implementing a CV experiment.
  */
 public class WekaPairTwentyNewsgroupsDemo
     implements Constants
@@ -66,57 +68,46 @@ public class WekaPairTwentyNewsgroupsDemo
     public static void main(String[] args)
         throws Exception
     {
-    	// This is used to ensure that the required DKPRO_HOME environment variable is set.
-    	// Ensures that people can run the experiments even if they haven't read the setup instructions first :)
-    	// Don't use this in real experiments! Read the documentation and set DKPRO_HOME as explained there.
-    	DemoUtils.setDkproHome(WekaPairTwentyNewsgroupsDemo.class.getSimpleName());
-    	
+        // This is used to ensure that the required DKPRO_HOME environment variable is set.
+        // Ensures that people can run the experiments even if they haven't read the setup
+        // instructions first :)
+        // Don't use this in real experiments! Read the documentation and set DKPRO_HOME as
+        // explained there.
+        DemoUtils.setDkproHome(WekaPairTwentyNewsgroupsDemo.class.getSimpleName());
+
         ParameterSpace pSpace = getParameterSpace();
         WekaPairTwentyNewsgroupsDemo experiment = new WekaPairTwentyNewsgroupsDemo();
         experiment.runTrainTest(pSpace);
     }
 
     @SuppressWarnings("unchecked")
-    public static ParameterSpace getParameterSpace() throws ResourceInitializationException
+    public static ParameterSpace getParameterSpace()
+        throws ResourceInitializationException
     {
         // configure training and test data reader dimension
         // train/test will use both, while cross-validation will only use the train part
         Map<String, Object> dimReaders = new HashMap<String, Object>();
-        
+
         CollectionReaderDescription readerTrain = CollectionReaderFactory.createReaderDescription(
                 PairTwentyNewsgroupsReader.class, PairTwentyNewsgroupsReader.PARAM_LISTFILE,
-                listFilePathTrain,
-                PairTwentyNewsgroupsReader.PARAM_LANGUAGE_CODE,
-                languageCode);
+                listFilePathTrain, PairTwentyNewsgroupsReader.PARAM_LANGUAGE_CODE, languageCode);
         dimReaders.put(DIM_READER_TRAIN, readerTrain);
 
         CollectionReaderDescription readerTest = CollectionReaderFactory.createReaderDescription(
                 PairTwentyNewsgroupsReader.class, PairTwentyNewsgroupsReader.PARAM_LISTFILE,
-                listFilePathTest,
-                PairTwentyNewsgroupsReader.PARAM_LANGUAGE_CODE,
-                languageCode);
+                listFilePathTest, PairTwentyNewsgroupsReader.PARAM_LANGUAGE_CODE, languageCode);
         dimReaders.put(DIM_READER_TEST, readerTest);
-
 
         Dimension<List<String>> dimClassificationArgs = Dimension.create(DIM_CLASSIFICATION_ARGS,
                 Arrays.asList(new String[] { NaiveBayes.class.getName() }));
 
-        Dimension<List<Object>> dimPipelineParameters = Dimension.create(
-                DIM_PIPELINE_PARAMS,
-                Arrays.asList(new Object[] {
-                        FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_USE_TOP_K,
-                        "50", FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MIN_N, 1,
-                        FrequencyDistributionNGramFeatureExtractorBase.PARAM_NGRAM_MAX_N, 3 })
-                );
-
-        // This feature is sensible and fast, but gives bad results on the demo data
-        Dimension<List<String>> dimFeatureSets = Dimension.create(
+        Dimension<List<TcFeature<ExternalResourceDescription>>> dimFeatureSets = Dimension.create(
                 DIM_FEATURE_SET,
-                Arrays.asList(new String[] { DiffNrOfTokensPairFeatureExtractor.class.getName() }));
+                Arrays.asList(TcFeatureFactory.create(DiffNrOfTokensPairFeatureExtractor.class)));
 
         ParameterSpace pSpace = new ParameterSpace(Dimension.createBundle("readers", dimReaders),
-                Dimension.create(DIM_LEARNING_MODE, LM_SINGLE_LABEL), Dimension.create(
-                        DIM_FEATURE_MODE, FM_PAIR), dimPipelineParameters, dimFeatureSets,
+                Dimension.create(DIM_LEARNING_MODE, LM_SINGLE_LABEL),
+                Dimension.create(DIM_FEATURE_MODE, FM_PAIR), dimFeatureSets,
                 dimClassificationArgs);
 
         return pSpace;
