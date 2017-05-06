628824c2ac6bc1c58f314c96d867c0deeb9f5957
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-05-06 12:28:48 +0200

    #403 - test case fixes

--- dkpro-tc-features-ngram/src/test/java/org/dkpro/tc/features/ngram/LucenePhoneticNGramFeatureExtractorTest.java
+++ dkpro-tc-features-ngram/src/test/java/org/dkpro/tc/features/ngram/LucenePhoneticNGramFeatureExtractorTest.java
@@ -21,7 +21,10 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
@@ -31,13 +34,12 @@ import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.factory.ExternalResourceFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.resource.ExternalResourceDescription;
-import org.dkpro.tc.api.features.FeatureStore;
+import org.dkpro.tc.api.features.Instance;
 import org.dkpro.tc.core.Constants;
 import org.dkpro.tc.core.io.JsonDataWriter;
 import org.dkpro.tc.core.util.TaskUtils;
 import org.dkpro.tc.features.ngram.io.TestReaderSingleLabel;
 import org.dkpro.tc.features.ngram.meta.LucenePhoneticNGramMetaCollector;
-import org.dkpro.tc.fstore.simple.DenseFeatureStore;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -47,63 +49,65 @@ import com.google.gson.Gson;
 
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 
-public class LucenePhoneticNGramFeatureExtractorTest
-{
-    @Rule
-    public TemporaryFolder folder = new TemporaryFolder();
+public class LucenePhoneticNGramFeatureExtractorTest {
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
 
-    @Before
-    public void setupLogging()
-    {
-        System.setProperty("org.apache.uima.logger.class",
-                "org.apache.uima.util.impl.Log4jLogger_impl");
-    }
+	@Before
+	public void setupLogging() {
+		System.setProperty("org.apache.uima.logger.class", "org.apache.uima.util.impl.Log4jLogger_impl");
+	}
 
-    @Test
-    public void lucenePhoneticNGramFeatureExtractorTest()
-        throws Exception
-    {
+	@Test
+	public void lucenePhoneticNGramFeatureExtractorTest() throws Exception {
 
-        File luceneFolder = folder.newFolder();
-        File outputPath = folder.newFolder();
+		File luceneFolder = folder.newFolder();
+		File outputPath = folder.newFolder();
 
-        Object[] parameters = new Object[] { LucenePhoneticNGram.PARAM_UNIQUE_EXTRACTOR_NAME, "123",
-                LucenePhoneticNGram.PARAM_NGRAM_USE_TOP_K, "10",
-                LucenePhoneticNGram.PARAM_SOURCE_LOCATION, luceneFolder.toString(),
-                LucenePhoneticNGramMetaCollector.PARAM_TARGET_LOCATION, luceneFolder.toString(), };
+		Object[] parameters = new Object[] { LucenePhoneticNGram.PARAM_UNIQUE_EXTRACTOR_NAME, "123",
+				LucenePhoneticNGram.PARAM_NGRAM_USE_TOP_K, "10", LucenePhoneticNGram.PARAM_SOURCE_LOCATION,
+				luceneFolder.toString(), LucenePhoneticNGramMetaCollector.PARAM_TARGET_LOCATION,
+				luceneFolder.toString(), };
 
-        ExternalResourceDescription featureExtractor = ExternalResourceFactory
-                .createExternalResourceDescription(LucenePhoneticNGram.class, parameters);
-        List<ExternalResourceDescription> fes = new ArrayList<>();
-        fes.add(featureExtractor);
+		ExternalResourceDescription featureExtractor = ExternalResourceFactory
+				.createExternalResourceDescription(LucenePhoneticNGram.class, parameters);
+		List<ExternalResourceDescription> fes = new ArrayList<>();
+		fes.add(featureExtractor);
 
-        CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(
-                TestReaderSingleLabel.class, TestReaderSingleLabel.PARAM_LANGUAGE, "en",
-                TestReaderSingleLabel.PARAM_SOURCE_LOCATION, "src/test/resources/data/text*.txt");
+		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(
+				TestReaderSingleLabel.class, TestReaderSingleLabel.PARAM_LANGUAGE, "en",
+				TestReaderSingleLabel.PARAM_SOURCE_LOCATION, "src/test/resources/data/text*.txt");
 
-        AnalysisEngineDescription segmenter = AnalysisEngineFactory
-                .createEngineDescription(BreakIteratorSegmenter.class);
+		AnalysisEngineDescription segmenter = AnalysisEngineFactory
+				.createEngineDescription(BreakIteratorSegmenter.class);
 
-        AnalysisEngineDescription metaCollector = AnalysisEngineFactory
-                .createEngineDescription(LucenePhoneticNGramMetaCollector.class, parameters);
+		AnalysisEngineDescription metaCollector = AnalysisEngineFactory
+				.createEngineDescription(LucenePhoneticNGramMetaCollector.class, parameters);
 
-        AnalysisEngineDescription featExtractorConnector = TaskUtils.getFeatureExtractorConnector(
-                outputPath.getAbsolutePath(), JsonDataWriter.class.getName(),
-                Constants.LM_SINGLE_LABEL, Constants.FM_DOCUMENT, DenseFeatureStore.class.getName(),
-                false, false, false, new ArrayList<>(), false, fes);
+		AnalysisEngineDescription featExtractorConnector = TaskUtils.getFeatureExtractorConnector(
+				outputPath.getAbsolutePath(), JsonDataWriter.class.getName(), Constants.LM_SINGLE_LABEL,
+				Constants.FM_DOCUMENT, false, false, false, false, false, Collections.emptyList(), fes);
 
-        // run meta collector
-        SimplePipeline.runPipeline(reader, segmenter, metaCollector);
+		// run meta collector
+		SimplePipeline.runPipeline(reader, segmenter, metaCollector);
 
-        // run FE(s)
-        SimplePipeline.runPipeline(reader, segmenter, featExtractorConnector);
+		// run FE(s)
+		SimplePipeline.runPipeline(reader, segmenter, featExtractorConnector);
 
-        Gson gson = new Gson();
-        FeatureStore fs = gson.fromJson(
-                FileUtils.readFileToString(new File(outputPath, JsonDataWriter.JSON_FILE_NAME)),
-                DenseFeatureStore.class);
-        System.out.println(fs);
-        assertEquals(2, fs.getNumberOfInstances());
-        assertEquals(1, fs.getUniqueOutcomes().size());
-    }
+		Gson gson = new Gson();
+		List<String> lines = FileUtils.readLines(new File(outputPath, JsonDataWriter.JSON_FILE_NAME));
+		List<Instance> instances = new ArrayList<>();
+		for (String l : lines) {
+			instances.add(gson.fromJson(l, Instance.class));
+		}
+
+		assertEquals(2, instances.size());
+		assertEquals(1, getUniqueOutcomes(instances));
+	}
+
+	private int getUniqueOutcomes(List<Instance> instances) {
+		Set<String> outcomes = new HashSet<String>();
+		instances.forEach(x -> outcomes.addAll(x.getOutcomes()));
+		return outcomes.size();
+	}
 }
--- dkpro-tc-features-pair-similarity/src/test/java/org/dkpro/tc/features/pair/similarity/PPipelineTestBase.java
+++ dkpro-tc-features-pair-similarity/src/test/java/org/dkpro/tc/features/pair/similarity/PPipelineTestBase.java
@@ -22,7 +22,9 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 
 import org.apache.commons.io.FileUtils;
@@ -33,12 +35,11 @@ import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.resource.ResourceInitializationException;
-import org.dkpro.tc.api.features.FeatureStore;
+import org.dkpro.tc.api.features.Feature;
 import org.dkpro.tc.api.features.Instance;
 import org.dkpro.tc.core.Constants;
 import org.dkpro.tc.core.io.JsonDataWriter;
 import org.dkpro.tc.core.task.uima.DocumentModeAnnotator;
-import org.dkpro.tc.fstore.simple.DenseFeatureStore;
 import org.dkpro.tc.testing.TestPairReader;
 import org.junit.ClassRule;
 import org.junit.rules.TemporaryFolder;
@@ -50,86 +51,104 @@ import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
 import de.tudarmstadt.ukp.dkpro.core.snowball.SnowballStemmer;
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 
+public abstract class PPipelineTestBase {
+	@ClassRule
+	public static TemporaryFolder folder = new TemporaryFolder();
 
-public abstract class PPipelineTestBase
-{
-    @ClassRule
-    public static TemporaryFolder folder = new TemporaryFolder();
-    
 	protected List<Instance> instanceList;
 	protected List<List<String>> outcomeList;
 	protected TreeSet<String> featureNames;
-	
-    protected File lucenePath;
-    protected File outputPath;
-    protected Object[] parameters;
-    protected AnalysisEngineDescription metaCollector;
-    protected AnalysisEngineDescription featExtractorConnector;
-    
-    protected void initialize() throws Exception{
-        lucenePath = folder.newFolder();
-        outputPath = folder.newFolder();
-        
-        instanceList = new ArrayList<Instance>();
-        outcomeList = new ArrayList<List<String>>();
-    }
-    protected String setTestPairsLocation(){
-    	return "src/test/resources/data/textpairs.txt";
-    }
 
-    protected void runPipeline()
-            throws Exception
-    {
-        List<Object> parameterList = new ArrayList<Object>(Arrays.asList(parameters));
-               
-        CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(
-                TestPairReader.class, 
-                TestPairReader.PARAM_INPUT_FILE, setTestPairsLocation()
-        );
-        
-        AnalysisEngineDescription segmenter = AnalysisEngineFactory.createEngineDescription(BreakIteratorSegmenter.class);
-        AnalysisEngineDescription stemmer = AnalysisEngineFactory.createEngineDescription(SnowballStemmer.class);
-        AnalysisEngineDescription lemmatizer = AnalysisEngineFactory.createEngineDescription(MorphaLemmatizer.class);
-        AnalysisEngineDescription posTagger = AnalysisEngineFactory.createEngineDescription(OpenNlpPosTagger.class);
-        AnalysisEngineDescription pairAnno = AnalysisEngineFactory.createEngineDescription(DocumentModeAnnotator.class, DocumentModeAnnotator.PARAM_FEATURE_MODE, Constants.FM_PAIR);
+	protected File lucenePath;
+	protected File outputPath;
+	protected Object[] parameters;
+	protected AnalysisEngineDescription metaCollector;
+	protected AnalysisEngineDescription featExtractorConnector;
 
-        AggregateBuilder builder = new AggregateBuilder();
-        builder.add(segmenter, Constants.INITIAL_VIEW, Constants.PART_ONE);
-        builder.add(segmenter, Constants.INITIAL_VIEW, Constants.PART_TWO);
-        builder.add(pairAnno, Constants.INITIAL_VIEW, Constants.PART_ONE);
-        builder.add(pairAnno, Constants.INITIAL_VIEW, Constants.PART_TWO);        
-        builder.add(stemmer, Constants.INITIAL_VIEW, Constants.PART_ONE);
-        builder.add(stemmer, Constants.INITIAL_VIEW, Constants.PART_TWO);
-        builder.add(lemmatizer, Constants.INITIAL_VIEW, Constants.PART_ONE);
-        builder.add(lemmatizer, Constants.INITIAL_VIEW, Constants.PART_TWO);
-        builder.add(posTagger, Constants.INITIAL_VIEW, Constants.PART_ONE);
-        builder.add(posTagger, Constants.INITIAL_VIEW, Constants.PART_TWO);
+	protected void initialize() throws Exception {
+		lucenePath = folder.newFolder();
+		outputPath = folder.newFolder();
 
-        getMetaCollector(parameterList);
+		instanceList = new ArrayList<Instance>();
+		outcomeList = new ArrayList<List<String>>();
+	}
 
-        getFeatureExtractorCollector(parameterList);
+	protected String setTestPairsLocation() {
+		return "src/test/resources/data/textpairs.txt";
+	}
 
-        // run meta collector
-        SimplePipeline.runPipeline(reader, builder.createAggregateDescription(), metaCollector);
+	protected void runPipeline() throws Exception {
+		List<Object> parameterList = new ArrayList<Object>(Arrays.asList(parameters));
 
-        // run FE(s)
-        SimplePipeline.runPipeline(reader, builder.createAggregateDescription(), featExtractorConnector);
+		CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(TestPairReader.class,
+				TestPairReader.PARAM_INPUT_FILE, setTestPairsLocation());
 
-        Gson gson = new Gson();
-        FeatureStore fs = gson.fromJson(FileUtils.readFileToString(new File(outputPath, JsonDataWriter.JSON_FILE_NAME)), DenseFeatureStore.class);
-        assertEquals(1, fs.getNumberOfInstances());
-        assertEquals(1, fs.getUniqueOutcomes().size());
-        
-        featureNames = fs.getFeatureNames();
+		AnalysisEngineDescription segmenter = AnalysisEngineFactory
+				.createEngineDescription(BreakIteratorSegmenter.class);
+		AnalysisEngineDescription stemmer = AnalysisEngineFactory.createEngineDescription(SnowballStemmer.class);
+		AnalysisEngineDescription lemmatizer = AnalysisEngineFactory.createEngineDescription(MorphaLemmatizer.class);
+		AnalysisEngineDescription posTagger = AnalysisEngineFactory.createEngineDescription(OpenNlpPosTagger.class);
+		AnalysisEngineDescription pairAnno = AnalysisEngineFactory.createEngineDescription(DocumentModeAnnotator.class,
+				DocumentModeAnnotator.PARAM_FEATURE_MODE, Constants.FM_PAIR);
 
-        for (int i=0; i<fs.getNumberOfInstances(); i++) {
-            instanceList.add(fs.getInstance(i));
-            outcomeList.add(fs.getOutcomes(i));
-        }
-    }
+		AggregateBuilder builder = new AggregateBuilder();
+		builder.add(segmenter, Constants.INITIAL_VIEW, Constants.PART_ONE);
+		builder.add(segmenter, Constants.INITIAL_VIEW, Constants.PART_TWO);
+		builder.add(pairAnno, Constants.INITIAL_VIEW, Constants.PART_ONE);
+		builder.add(pairAnno, Constants.INITIAL_VIEW, Constants.PART_TWO);
+		builder.add(stemmer, Constants.INITIAL_VIEW, Constants.PART_ONE);
+		builder.add(stemmer, Constants.INITIAL_VIEW, Constants.PART_TWO);
+		builder.add(lemmatizer, Constants.INITIAL_VIEW, Constants.PART_ONE);
+		builder.add(lemmatizer, Constants.INITIAL_VIEW, Constants.PART_TWO);
+		builder.add(posTagger, Constants.INITIAL_VIEW, Constants.PART_ONE);
+		builder.add(posTagger, Constants.INITIAL_VIEW, Constants.PART_TWO);
+
+		getMetaCollector(parameterList);
+
+		getFeatureExtractorCollector(parameterList);
+
+		// run meta collector
+		SimplePipeline.runPipeline(reader, builder.createAggregateDescription(), metaCollector);
+
+		// run FE(s)
+		SimplePipeline.runPipeline(reader, builder.createAggregateDescription(), featExtractorConnector);
+
+		Gson gson = new Gson();
+
+		List<String> lines = FileUtils.readLines(new File(outputPath, JsonDataWriter.JSON_FILE_NAME));
+		for (String l : lines) {
+			instanceList.add(gson.fromJson(l, Instance.class));
+		}
+
+		assertEquals(1, lines.size());
+		assertEquals(1, getUniqueOutcomes(instanceList).size());
+
+		featureNames = getFeatureNames(instanceList);
+
+		for (int i = 0; i < instanceList.size(); i++) {
+			outcomeList.add(instanceList.get(i).getOutcomes());
+		}
+	}
+
+	private TreeSet<String> getFeatureNames(List<Instance> instanceList) {
+		TreeSet<String> s = new TreeSet<>();
+		for (Instance i : instanceList) {
+			for (Feature f : i.getFeatures()) {
+				s.add(f.getName());
+			}
+		}
+
+		return s;
+	}
+
+	private static Set<String> getUniqueOutcomes(List<Instance> instances) {
+		Set<String> outcomes = new HashSet<String>();
+		instances.forEach(x -> outcomes.addAll(x.getOutcomes()));
+		return outcomes;
+	}
+
 	protected abstract void getFeatureExtractorCollector(List<Object> parameterList)
-		throws ResourceInitializationException;
+			throws ResourceInitializationException;
 
-	protected abstract void getMetaCollector(List<Object> parameterList)
-		throws ResourceInitializationException;
+	protected abstract void getMetaCollector(List<Object> parameterList) throws ResourceInitializationException;
 }
