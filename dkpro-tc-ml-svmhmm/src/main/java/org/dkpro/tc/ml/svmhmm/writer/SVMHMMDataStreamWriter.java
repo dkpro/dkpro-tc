78bba3ee7f023ec1f11d766e586d219a264babb4
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-05-15 10:08:41 +0200

    #403 - test case fixes and clean up

--- dkpro-tc-ml-svmhmm/src/main/java/org/dkpro/tc/ml/svmhmm/writer/SVMHMMDataStreamWriter.java
+++ dkpro-tc-ml-svmhmm/src/main/java/org/dkpro/tc/ml/svmhmm/writer/SVMHMMDataStreamWriter.java
@@ -71,10 +71,6 @@ public class SVMHMMDataStreamWriter implements DataStreamWriter {
 
 	private File outputDirectory;
 
-	private boolean useSparse;
-	private String learningMode;
-	private boolean applyWeighting;
-
 	private File classifierFormatOutputFile;
 
 	private BufferedWriter bw;
@@ -84,140 +80,6 @@ public class SVMHMMDataStreamWriter implements DataStreamWriter {
 
 	private Map<String, Integer> featureNameMap;
 
-	// @Override
-	// public void write(File aOutputDirectory, FeatureStore featureStore,
-	// boolean aUseDenseInstances,
-	// String aLearningMode, boolean applyWeighting)
-	// throws Exception
-	// {
-	// // map features to feature numbers
-	// BidiMap featureNameToFeatureNumberMapping = SVMHMMUtils
-	// .mapVocabularyToIntegers(featureStore.getFeatureNames());
-	//
-	// // prepare output file
-	// File outputFile = new File(aOutputDirectory, new
-	// SVMHMMAdapter().getFrameworkFilename(
-	// TCMachineLearningAdapter.AdapterNameEntries.featureVectorsFile));
-	//
-	// BufferedWriter bf = new BufferedWriter(new FileWriter(outputFile));
-	// PrintWriter pw = new PrintWriter(bf);
-	//
-	// log.info("Start writing features to file " +
-	// outputFile.getAbsolutePath());
-	//
-	// log.debug("Total instances: " + featureStore.getNumberOfInstances());
-	// log.debug("Feature vector size: " +
-	// featureStore.getFeatureNames().size());
-	//
-	// if (featureStore instanceof SparseFeatureStore) {
-	// SparseFeatureStore sparseFeatureStore = (SparseFeatureStore)
-	// featureStore;
-	// log.debug("Non-null feature sparsity ratio: "
-	// + sparseFeatureStore.getFeatureSparsityRatio());
-	// }
-	//
-	// for (int i = 0; i < featureStore.getNumberOfInstances(); i++) {
-	// Instance instance;
-	//
-	// instance = featureStore.getInstance(i);
-	//
-	// // placeholder for original token
-	// String originalToken = null;
-	//
-	// // other "features" - meta data features that will be stored in the
-	// comment
-	// SortedMap<String, String> metaDataFeatures = new TreeMap<>();
-	//
-	// // feature values
-	// SortedMap<Integer, Number> featureValues = new TreeMap<>();
-	// for (Feature f : instance.getFeatures()) {
-	// String featureName = f.getName();
-	// Object featureValue = f.getValue();
-	//
-	// // we ignore null feature values
-	// if (featureValue == null) {
-	// continue;
-	// }
-	//
-	// // get original token stored in OriginalToken feature
-	// if (OriginalTextHolderFeatureExtractor.ORIGINAL_TEXT.equals(featureName))
-	// {
-	// // if original token/text was multi line, join it to a single line
-	// // originalToken = ((String) featureValue).replaceAll("\\n", " ");
-	// originalToken = (String) featureValue;
-	// continue;
-	// }
-	//
-	// // handle other possible features as metadata?
-	// if (isMetaDataFeature(featureName)) {
-	// metaDataFeatures.put(featureName, (String) featureValue);
-	// continue;
-	// }
-	//
-	// // not allow other non-number features
-	// if (!(featureValue instanceof Number)) {
-	// log.debug("Only features with number values are allowed, but was " + f);
-	// continue;
-	// }
-	//
-	// // in case the feature store produced dense feature vector with zeros for
-	// // non-present features, we ignore zero value features here
-	// Number featureValueNumber = (Number) featureValue;
-	// if (Math.abs(featureValueNumber.doubleValue() - 0d) < EPS) {
-	// continue;
-	// }
-	//
-	// // get number and int value of the feature
-	// Integer featureNumber = (Integer) featureNameToFeatureNumberMapping
-	// .get(featureName);
-	//
-	// featureValues.put(featureNumber, featureValueNumber);
-	// }
-	//
-	// // print formatted output: label name and sequence id
-	// pw.printf(Locale.ENGLISH, "%s qid:%d ", instance.getOutcome(),
-	// getUniqueSequenceId(instance));
-	//
-	// // print sorted features
-	// for (Map.Entry<Integer, Number> entry : featureValues.entrySet()) {
-	// if (entry.getValue() instanceof Double) {
-	// // format double on 8 decimal places
-	// pw.printf(Locale.ENGLISH, "%d:%.8f ", entry.getKey(),
-	// entry.getValue().doubleValue());
-	// }
-	// else {
-	// // format as integer
-	// pw.printf(Locale.ENGLISH, "%d:%d ", entry.getKey(),
-	// entry.getValue().intValue());
-	// }
-	// }
-	//
-	// // print original token and label as comment
-	// pw.printf(Locale.ENGLISH, "# %s %d %s ", instance.getOutcome(),
-	// instance.getSequenceId(),
-	// (originalToken != null) ? (URLEncoder.encode(originalToken, "utf-8")) :
-	// "");
-	//
-	// // print meta-data features at the end
-	// for (Map.Entry<String, String> entry : metaDataFeatures.entrySet()) {
-	// pw.printf(" %s:%s", URLEncoder.encode(entry.getKey(), "utf-8"),
-	// URLEncoder.encode(entry.getValue(), "utf-8"));
-	// }
-	// // new line at the end
-	// pw.println();
-	// }
-	//
-	// IOUtils.closeQuietly(pw);
-	//
-	// // writing feature mapping
-	// File mappingFile = new File(aOutputDirectory,
-	// "featuresIntToNames_forDebug.txt");
-	// SVMHMMUtils.saveMappingTextFormat(featureNameToFeatureNumberMapping,
-	// mappingFile);
-	//
-	// log.info("Finished writing features to file " +
-	// outputFile.getAbsolutePath());
-	// }
 
 	private Integer getUniqueSequenceId(Instance instance) {
 		String key = instance.getJcasId() + "-" + instance.getSequenceId();
@@ -385,7 +247,7 @@ public class SVMHMMDataStreamWriter implements DataStreamWriter {
 		featureNameMap = new HashMap<String, Integer>();
 		List<String> fm = new ArrayList<>(featureNames);
 		for (int i = 0; i < fm.size(); i++) {
-			featureNameMap.put(fm.get(i), i + 2);
+			featureNameMap.put(fm.get(i), i + 1);
 		}
 	}
 
@@ -413,9 +275,6 @@ public class SVMHMMDataStreamWriter implements DataStreamWriter {
 	public void init(File outputDirectory, boolean useSparse, String learningMode, boolean applyWeighting)
 			throws Exception {
 		this.outputDirectory = outputDirectory;
-		this.useSparse = useSparse;
-		this.learningMode = learningMode;
-		this.applyWeighting = applyWeighting;
 		classifierFormatOutputFile = new File(outputDirectory,
 				new SVMHMMAdapter().getFrameworkFilename(AdapterNameEntries.featureVectorsFile));
 
