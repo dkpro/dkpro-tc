535f17b867cd6ce3d54ee4ad30606a8dab879703
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-05-28 13:05:04 +0200

    #388

--- dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/deeplearning/dl4j/seq/Dl4jUserCodeStub.java
+++ dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/deeplearning/dl4j/seq/Dl4jUserCodeStub.java
@@ -29,6 +29,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
@@ -36,7 +37,9 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
+import org.deeplearning4j.berkeley.Pair;
 import org.deeplearning4j.datasets.iterator.impl.ListDataSetIterator;
+import org.deeplearning4j.eval.EvaluationUtils;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
@@ -48,16 +51,20 @@ import org.deeplearning4j.nn.conf.layers.GravesLSTM;
 import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.dkpro.tc.examples.deeplearning.dl4j.seq.BinaryWordVectorSerializer.BinaryVectorizer;
 import org.dkpro.tc.ml.deeplearning4j.user.TcDeepLearning4jUser;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 public class Dl4jUserCodeStub implements TcDeepLearning4jUser {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		String root = "/Users/toobee/Desktop/org.dkpro.lab/repository/";
 		String trainVec = root + "/VectorizationTask-Train-DynetSeq2Seq-20170528101511705/output/instanceVectors.txt";
 		String trainOutc = root + "/VectorizationTask-Train-DynetSeq2Seq-20170528101511705/output/outcomeVectors.txt";
@@ -75,41 +82,74 @@ public class Dl4jUserCodeStub implements TcDeepLearning4jUser {
 
 		int featuresSize = getEmbeddingsSize(embedding);
 		int maxTagsetSize = getNumberOfOutcomes(trainOutcome);
-		int batchSize = 5;
+		int batchSize = 30;
 		int epochs = 1;
 		boolean shuffle = true;
 		int iterations = 1;
 		double learningRate = 0.1;
 
 		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .iterations(iterations)
-                .seed(12345l)
-                .updater(Updater.RMSPROP).regularization(true).l2(1e-5)
-                .weightInit(WeightInit.RELU)
-                .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue)
-                .gradientNormalizationThreshold(1.0)
-                .learningRate(learningRate)
-                .list()
-                .layer(0, new GravesLSTM.Builder()
-                            .activation("softsign")
-                            .nIn(featuresSize)
-                            .nOut(200)
-                            .build())
-                .layer(1, new RnnOutputLayer.Builder()
-                            .activation("softmax")
-                            .lossFunction(LossFunctions.LossFunction.MCXENT)
-                            .nIn(200)
-                            .nOut(maxTagsetSize)
-                            .build())
-                .pretrain(false).backprop(true).build();
+				.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).iterations(iterations).seed(12345l)
+				.updater(Updater.RMSPROP).regularization(true).l2(1e-5).weightInit(WeightInit.RELU)
+				.gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue)
+				.gradientNormalizationThreshold(1.0).learningRate(learningRate).list()
+				.layer(0, new GravesLSTM.Builder().activation(Activation.SOFTSIGN).nIn(featuresSize).nOut(200).build())
+				.layer(1,
+						new RnnOutputLayer.Builder().activation(Activation.SOFTMAX)
+								.lossFunction(LossFunctions.LossFunction.MCXENT).nIn(200).nOut(maxTagsetSize).build())
+				.pretrain(false).backprop(true).build();
 
-		
-		Collection<DataSet> dataSet = toDataSet(trainVec, trainOutcome, embedding);
-		DataSetIterator train = new ListDataSetIterator(dataSet, batchSize);
+		List<DataSet> dataSet = new ArrayList<DataSet>(toDataSet(trainVec, trainOutcome, embedding));
 		MultiLayerNetwork mln = new MultiLayerNetwork(conf);
 		mln.init();
-		mln.fit(train);
+		mln.setListeners(new ScoreIterationListener(1));
+
+		for (int i = 0; i < epochs; i++) {
+			System.out.println("Epoche " + (i + 1));
+			Collections.shuffle(dataSet);
+			DataSetIterator train = new ListDataSetIterator(dataSet, batchSize);
+			mln.fit(train);
+		}
+
+		DataSetIterator iTest = new ListDataSetIterator(dataSet, batchSize);
+		StringBuilder sb = new StringBuilder();
+		sb.append("#Gold\tPrediction" + System.lineSeparator());
+		// Run evaluation. This is on 25k reviews, so can take some time
+		while (iTest.hasNext()) {
+			DataSet t = iTest.next();
+			INDArray features = t.getFeatureMatrix();
+			INDArray lables = t.getLabels();
+			// System.out.println("labels : " + lables);
+			INDArray outMask = t.getLabelsMaskArray();
+			INDArray predicted = mln.output(features, false);
+			// System.out.println("predicted : " + predicted);
+			eval(lables, predicted, outMask, sb);
+		}
+		iTest.reset();
+
+		FileUtils.writeStringToFile(prediction, sb.toString(), "utf-8");
+	}
+
+	private static void eval(INDArray labels, INDArray p, INDArray outMask, StringBuilder sb) {
+		Pair<INDArray, INDArray> pair = EvaluationUtils.extractNonMaskedTimeSteps(labels, p, outMask);
+
+		INDArray realOutcomes = pair.getFirst();
+		INDArray guesses = pair.getSecond();
+
+		// Length of real labels must be same as length of predicted labels
+		if (realOutcomes.length() != guesses.length())
+			throw new IllegalArgumentException("Unable to evaluate. Outcome matrices not same length");
+
+		INDArray guessIndex = Nd4j.argMax(guesses, 1);
+		INDArray realOutcomeIndex = Nd4j.argMax(realOutcomes, 1);
+
+		int nExamples = guessIndex.length();
+		for (int i = 0; i < nExamples; i++) {
+			int actual = (int) realOutcomeIndex.getDouble(i);
+			int predicted = (int) guessIndex.getDouble(i);
+			sb.append(actual + "\t" + predicted + System.lineSeparator());
+			System.out.println(actual + "\t" + predicted);
+		}
 	}
 
 	private int getNumberOfOutcomes(File trainOutcome) throws IOException {
@@ -138,8 +178,14 @@ public class Dl4jUserCodeStub implements TcDeepLearning4jUser {
 		BinaryWordVectorSerializer.convertWordVectorsToBinary(wordVectors, f.toPath());
 		BinaryVectorizer bw = BinaryVectorizer.load(f.toPath());
 		List<DataSet> data = new ArrayList<>();
-		data.add(vectorize.vectorize(transformToList(sentences), transformToList(outcomes), bw, maxLen, labels.size(),
-				true));
+
+		for (int i = 0; i < sentences.size(); i++) {
+			// each sent/label pairing is in an own data set
+			List<String> singleSent = Arrays.asList(sentences.get(i));
+			List<String> singleOutcome = Arrays.asList(outcomes.get(i));
+			data.add(vectorize.vectorize(transformToList(singleSent), transformToList(singleOutcome), bw, maxLen,
+					labels.size(), true));
+		}
 
 		return data;
 	}
@@ -147,11 +193,11 @@ public class Dl4jUserCodeStub implements TcDeepLearning4jUser {
 	private List<List<String>> transformToList(List<String> sentences) {
 
 		List<List<String>> out = new ArrayList<>();
-		
-		for(String s : sentences){
+
+		for (String s : sentences) {
 			out.add(asList(s.split(" ")));
 		}
-		
+
 		return out;
 	}
 
