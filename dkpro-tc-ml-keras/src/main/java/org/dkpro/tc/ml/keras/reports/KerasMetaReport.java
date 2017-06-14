a09b17335d1da1879f2e2f2ce0d74284589c9a48
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-06-14 13:15:36 +0200

    #388

--- dkpro-tc-ml-keras/src/main/java/org/dkpro/tc/ml/keras/reports/KerasMetaReport.java
+++ dkpro-tc-ml-keras/src/main/java/org/dkpro/tc/ml/keras/reports/KerasMetaReport.java
@@ -18,21 +18,97 @@
 
 package org.dkpro.tc.ml.keras.reports;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 import org.dkpro.lab.reporting.ReportBase;
+import org.dkpro.lab.storage.StorageService.AccessMode;
 import org.dkpro.tc.core.DeepLearningConstants;
 import org.dkpro.tc.ml.keras.KerasTestTask;
+import org.dkpro.tc.ml.report.util.SortedKeyProperties;
 
-public class KerasMetaReport extends ReportBase {
+public class KerasMetaReport
+    extends ReportBase
+{
 
-	@Override
-	public void execute() throws Exception {
-	    String string = getDiscriminators()
-                .get(KerasTestTask.class.getName() + "|" + DeepLearningConstants.DIM_PYTHON_INSTALLATION);
-	    
-	    System.out.println(string);
-	    
-	    int a=0;
-	    a++;
-	}
+    @Override
+    public void execute()
+        throws Exception
+    {
+        String python = getDiscriminators().get(KerasTestTask.class.getName() + "|"
+                + DeepLearningConstants.DIM_PYTHON_INSTALLATION);
+
+        String kerasVersion = getKerasVersion(python);
+        String numpyVersion = getNumpyVersion(python);
+        String tensorFlowVersion = getTensorflowVersion(python);
+        String theanoVersion = getTheanoVersion(python);
+        System.out.println(kerasVersion+"\n" + numpyVersion + "\n"+tensorFlowVersion + "\n" + theanoVersion);
+
+        Properties p = new SortedKeyProperties();
+        p.setProperty("KerasVersion", kerasVersion);
+        p.setProperty("NumpyVersion", numpyVersion);
+        p.setProperty("TensorFlowVersion", tensorFlowVersion);
+        p.setProperty("TheanoFlowVersion", theanoVersion);
+        
+        File file = getContext().getFile("libaryVersion.txt", AccessMode.READWRITE);
+        FileOutputStream fos = new FileOutputStream(file);
+        p.store(fos, "Version information");
+        fos.close();
+    }
+    
+    private String getTheanoVersion(String python)
+            throws IOException, InterruptedException
+        {
+            return getVersion(python, "import theano; print(theano.__version__)");
+        }
+
+    private String getTensorflowVersion(String python)
+        throws IOException, InterruptedException
+    {
+        return getVersion(python, "import tensorflow; print(tensorflow.__version__)");
+    }
+
+    private String getKerasVersion(String python)
+        throws IOException, InterruptedException
+    {
+        return getVersion(python, "import keras; print(keras.__version__)");
+    }
+
+    private String getNumpyVersion(String python)
+        throws IOException, InterruptedException
+    {
+        return getVersion(python, "import numpy; print(numpy.__version__)");
+    }
+
+    private String getVersion(String python, String cmd)
+        throws IOException, InterruptedException
+    {
+        List<String> command = new ArrayList<>();
+        command.add(python);
+        command.add("-c");
+        command.add(cmd);
+
+        ProcessBuilder pb = new ProcessBuilder(command).command(command);
+        Process start = pb.start();
+        start.waitFor();
+
+        InputStream inputStream = start.getInputStream();
+        List<String> output = new ArrayList<>();
+        BufferedReader r = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+        String l = null;
+        while ((l = r.readLine()) != null) {
+            output.add(l);
+        }
+        r.close();
+
+        return output.get(output.size() - 1);
+    }
 
 }
