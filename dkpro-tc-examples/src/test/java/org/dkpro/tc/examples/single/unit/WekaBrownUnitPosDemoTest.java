529cf0a3cff8e07be5f50ec3ac1fa539af4c46ed
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-07-01 12:39:07 +0200

    #413

--- dkpro-tc-examples/src/test/java/org/dkpro/tc/examples/single/unit/WekaBrownUnitPosDemoTest.java
+++ dkpro-tc-examples/src/test/java/org/dkpro/tc/examples/single/unit/WekaBrownUnitPosDemoTest.java
@@ -61,6 +61,6 @@ public class WekaBrownUnitPosDemoTest extends JavaDemosTest_Base
         Id2Outcome o = new Id2Outcome(ContextMemoryReport.id2outcome, Constants.LM_SINGLE_LABEL);
         EvaluatorBase createEvaluator = EvaluatorFactory.createEvaluator(o, true, false);
         Double result = createEvaluator.calculateEvaluationMeasures().get(Accuracy.class.getSimpleName());
-        assertEquals(0.904, result, 0.0001);
+        assertEquals(0.744, result, 0.0001);
     }
 }
