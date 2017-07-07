888f6c00d078371332169e6a55b82c6529ec5f24
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-07-07 15:51:42 +0200

    #403 - escape feature names

--- dkpro-tc-ml-weka/src/main/java/org/dkpro/tc/ml/weka/writer/WekaDataWriterFast.java
+++ dkpro-tc-ml-weka/src/main/java/org/dkpro/tc/ml/weka/writer/WekaDataWriterFast.java
@@ -44,6 +44,8 @@ import org.dkpro.tc.ml.weka.WekaClassificationAdapter;
 
 import com.google.gson.Gson;
 
+import weka.core.Utils;
+
 /*
  * Datawriter for the Weka machine learning tool.
  */
@@ -58,7 +60,7 @@ public class WekaDataWriterFast
     boolean isRegression;
 
     private String[] outcomes;
-    
+
     private boolean isAlreadyInitialized;
     private StringBuilder headerFile;
 
@@ -67,13 +69,13 @@ public class WekaDataWriterFast
     private static final String ENUM = "enum";
     private static final String BOOLEAN = "boolean";
 
-    int numberFeatures=-1;
+    int numberFeatures = -1;
     Map<String, Integer> name2id;
     Map<String, String> name2featureType;
-    
+
     @Override
     public void init(File outputFolder, boolean useSparse, String learningMode,
-            boolean applyWeighting, String [] outcomes)
+            boolean applyWeighting, String[] outcomes)
                 throws Exception
     {
         this.outputFolder = outputFolder;
@@ -141,51 +143,51 @@ public class WekaDataWriterFast
         FileUtils.deleteQuietly(new File(outputFolder, GENERIC_FEATURE_FILE));
     }
 
-
     @Override
     public void writeClassifierFormat(Collection<Instance> instances, boolean compress)
         throws Exception
     {
         BufferedWriter writer = reOpenArffWriter();
         String header = initArffOutput();
-        if(header != null){
-            //first call only
+        if (header != null) {
+            // first call only
             writer.write(header);
         }
-        
-        for(Instance inst : instances){
-            String [] values = new String[numberFeatures];
-            for(Feature f : inst.getFeatures()){
+
+        for (Instance inst : instances) {
+            String[] values = new String[numberFeatures];
+            for (Feature f : inst.getFeatures()) {
                 Integer integer = name2id.get(f.getName());
                 values[integer] = transformValue(f.getName(), f.getValue());
             }
-            
-            for(int i =0; i < values.length; i++){
+
+            for (int i = 0; i < values.length; i++) {
                 String v = values[i];
-                if(v == null){
+                if (v == null) {
                     writer.write("?");
-                }else{
+                }
+                else {
                     writer.write(v);
                 }
-                if(i+1 < values.length){
+                if (i + 1 < values.length) {
                     writer.write(",");
                 }
             }
-            
+
             writer.write("," + inst.getOutcome());
-            
-            if(applyWeighting){
+
+            if (applyWeighting) {
                 double weight = inst.getWeight();
                 writer.write(",{");
-                writer.write(weight+"");
+                writer.write(weight + "");
                 writer.write("}");
             }
-            
+
             writer.write(System.lineSeparator());
         }
-        
+
         writer.close();
- 
+
     }
 
     private String transformValue(String name, Object object)
@@ -204,52 +206,61 @@ public class WekaDataWriterFast
         throw new IllegalArgumentException("Feature type of [" + name + "] is unknown");
     }
 
-    private BufferedWriter reOpenArffWriter() throws Exception
+    private BufferedWriter reOpenArffWriter()
+        throws Exception
     {
-        return new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(arffTarget, true), "utf-8"));
+        return new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream(arffTarget, true), "utf-8"));
     }
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
-    private String initArffOutput() throws IOException
+    private String initArffOutput()
+        throws IOException
     {
-        if(isAlreadyInitialized){
+        if (isAlreadyInitialized) {
             return null;
         }
-        
+
         name2featureType = new HashMap<>();
         headerFile = new StringBuilder();
         name2id = new HashMap<>();
-        
+
         List<String> lines = FileUtils.readLines(
                 new File(outputFolder, Constants.FILENAME_FEATURES_DESCRIPTION), "utf-8");
         Collections.sort(lines);
-        
+
         numberFeatures = lines.size();
-        
-        //build map
-        for(int i=0; i < lines.size(); i++){
+
+        // build map
+        for (int i = 0; i < lines.size(); i++) {
             String[] split = lines.get(i).split("\t");
             name2id.put(split[0], i);
         }
-        
-        //build header
+
+        // build header
         headerFile.append(
                 "@relation dkpro-tc-generated" + System.lineSeparator() + System.lineSeparator());
-        for(int i=0; i < lines.size(); i++){
+        for (int i = 0; i < lines.size(); i++) {
             String[] split = lines.get(i).split("\t");
-            
+
+            String escapedName = Utils.quote(split[0]);
+
             FeatureType ft = FeatureType.valueOf(split[1]);
-            if(ft == FeatureType.NUM || ft == FeatureType.NUM_FLOATING_POINT || ft == FeatureType.NUM_INTEGER){
-                headerFile.append("@attribute " + split[0] + " numeric" + System.lineSeparator());
+            if (ft == FeatureType.NUM || ft == FeatureType.NUM_FLOATING_POINT
+                    || ft == FeatureType.NUM_INTEGER) {
+                headerFile
+                        .append("@attribute " + escapedName + " numeric" + System.lineSeparator());
                 name2featureType.put(split[0], NUMERIC);
-            }else if (ft == FeatureType.BOOLEAN){
-                headerFile.append("@attribute " + split[0] + " numeric" + System.lineSeparator());
+            }
+            else if (ft == FeatureType.BOOLEAN) {
+                headerFile
+                        .append("@attribute " + escapedName + " numeric" + System.lineSeparator());
                 name2featureType.put(split[0], BOOLEAN);
-            }else if (ft == FeatureType.ENUM){
-                Class<Enum> c=null;
+            }
+            else if (ft == FeatureType.ENUM) {
+                Class<Enum> c = null;
                 try {
-                    headerFile.append("@attribute "+split[0]+" {");
+                    headerFile.append("@attribute " + escapedName + " {");
                     c = (Class<Enum>) Class.forName(split[2]);
                     Enum[] enumConstants = c.getEnumConstants();
                     for (int j = 0; j < enumConstants.length; j++) {
@@ -265,29 +276,28 @@ public class WekaDataWriterFast
                 catch (ClassNotFoundException e) {
                     throw new IOException(e);
                 }
-            }else { //String
-                headerFile.append("@attribute " + split[0] + " string" + System.lineSeparator());
+            }
+            else { // String
+                headerFile.append("@attribute " + escapedName + " string" + System.lineSeparator());
                 name2featureType.put(split[0], STRING);
             }
-            
+
         }
 
         headerFile.append("@attribute outcome {");
-        for(int j=0; j < outcomes.length; j++){
+        for (int j = 0; j < outcomes.length; j++) {
             headerFile.append(outcomes[j]);
-            if(j+1 < outcomes.length){
+            if (j + 1 < outcomes.length) {
                 headerFile.append(",");
             }
         }
-        headerFile.append("}" + System.lineSeparator()+ System.lineSeparator());
-        headerFile.append("@data"+ System.lineSeparator());
-        
-        
+        headerFile.append("}" + System.lineSeparator() + System.lineSeparator());
+        headerFile.append("@data" + System.lineSeparator());
+
         isAlreadyInitialized = true;
-        
+
         return headerFile.toString();
     }
-    
 
     @Override
     public boolean canStream()
