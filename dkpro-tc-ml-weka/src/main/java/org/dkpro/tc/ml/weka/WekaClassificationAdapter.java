dc731560c869a641c8c2ac98d0693e6f89778b73
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-07-07 15:02:56 +0200

    #403 - faster weka data writer prototype

--- dkpro-tc-ml-weka/src/main/java/org/dkpro/tc/ml/weka/WekaClassificationAdapter.java
+++ dkpro-tc-ml-weka/src/main/java/org/dkpro/tc/ml/weka/WekaClassificationAdapter.java
@@ -35,6 +35,7 @@ import org.dkpro.tc.ml.weka.task.WekaTestTask;
 import org.dkpro.tc.ml.weka.task.serialization.LoadModelConnectorWeka;
 import org.dkpro.tc.ml.weka.task.serialization.WekaModelSerializationDescription;
 import org.dkpro.tc.ml.weka.writer.WekaDataWriter;
+import org.dkpro.tc.ml.weka.writer.WekaDataWriterFast;
 
 public class WekaClassificationAdapter 
 	implements TcShallowLearningAdapter
@@ -70,7 +71,7 @@ public class WekaClassificationAdapter
 	public String getFrameworkFilename(AdapterNameEntries name) {
 
         switch (name) {
-            case featureVectorsFile:  return "training-data.arff.gz";
+            case featureVectorsFile:  return "training-data.arff";
             case predictionsFile      :  return "predictions.arff";
             case featureSelectionFile :  return "attributeEvaluationResults.txt";
         }
@@ -80,7 +81,7 @@ public class WekaClassificationAdapter
 	
 	@Override
 	public Class<? extends DataWriter> getDataWriterClass() {
-		return WekaDataWriter.class;
+		return WekaDataWriterFast.class;
 	}
 	
 	@Override
