61073190968938c99767579ecac1c24a4a870c8f
Tobias Horsmann <tobias.horsmann@gmail.com>
2017-12-24 14:51:13 +0100

    No issue.Find-bug message fixed.

--- dkpro-tc-api-features/src/main/java/org/dkpro/tc/api/features/TcFeature.java
+++ dkpro-tc-api-features/src/main/java/org/dkpro/tc/api/features/TcFeature.java
@@ -17,6 +17,7 @@
  ******************************************************************************/
 package org.dkpro.tc.api.features;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.uima.fit.factory.ExternalResourceFactory;
@@ -48,7 +49,7 @@ public class TcFeature
         this.classOfFeat = classOfFeat;
         this.id = id;
         this.fullFeatureName = classOfFeat.getName();
-        this.params = params;
+        this.params = Arrays.<Object>copyOf(params, params.length);
     }
 
     public void setConfig(Map<String, Object> aConfig)
