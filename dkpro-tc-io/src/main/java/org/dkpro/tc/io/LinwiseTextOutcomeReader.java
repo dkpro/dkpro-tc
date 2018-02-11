28218ac8923d62e9099c81392c7698c81102b615
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-02-11 11:55:57 +0100

    #444 - Reader for typical machine learning data formats

---
+++ dkpro-tc-io/src/main/java/org/dkpro/tc/io/LinwiseTextOutcomeReader.java
@@ -0,0 +1,144 @@
+/*******************************************************************************
+ * Copyright 2018
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.dkpro.tc.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.jcas.JCas;
+import org.dkpro.tc.api.type.TextClassificationOutcome;
+import org.dkpro.tc.api.type.TextClassificationTarget;
+
+import de.tudarmstadt.ukp.dkpro.core.api.io.JCasResourceCollectionReader_ImplBase;
+
+/**
+ * This is a line-wise reader which reads textual information and an associated
+ * outcome from a single line of text that is separated by a character, which
+ * allows separating both information. The text is annotated as
+ * {@link org.dkpro.tc.api.type.TextClassificationTarget} and the outcome is
+ * annotated as {@link org.dkpro.tc.api.type.TextClassificationOutcome}, which
+ * are the data types DKPro TC requires for processing the read data.
+ */
+public class LinwiseTextOutcomeReader extends JCasResourceCollectionReader_ImplBase {
+
+	/**
+	 * The separating character that separates textual information from its
+	 * outcome, i.e. a label or a numerical value. Defaults to TAB
+	 */
+	public static final String PARAM_SEPARATING_CHAR = "PARAM_SEPARATING_CHAR";
+	@ConfigurationParameter(name = PARAM_SEPARATING_CHAR, mandatory = true, defaultValue = "\t")
+	private String separatingChar;
+
+	public static final String PARAM_TEXT_INDEX = "PARAM_TEXT_INDEX";
+	@ConfigurationParameter(name = PARAM_TEXT_INDEX, mandatory = true, defaultValue = "0")
+	private Integer textIdx;
+
+	public static final String PARAM_OUTCOME_INDEX = "PARAM_OUTCOME_INDEX";
+	@ConfigurationParameter(name = PARAM_OUTCOME_INDEX, mandatory = true, defaultValue = "1")
+	private Integer outcomeIdx;
+
+	private BufferedReader reader;
+
+	String nextDocument = null;
+
+	@Override
+	public void getNext(JCas aJCas) throws IOException, CollectionException {
+		StringBuilder documentText = new StringBuilder();
+
+		String[] split = nextDocument.split(separatingChar);
+
+		String text = split[textIdx].trim();
+		String outcome = split[outcomeIdx].trim();
+
+		int entryStart = documentText.length();
+		int entryEnd = documentText.length() + text.length();
+
+		TextClassificationTarget classificationTarget = new TextClassificationTarget(aJCas, entryStart, entryEnd);
+		classificationTarget.addToIndexes();
+
+		TextClassificationOutcome classificationOutcome = new TextClassificationOutcome(aJCas, entryStart, entryEnd);
+		classificationOutcome.setOutcome(outcome);
+		classificationOutcome.addToIndexes();
+
+		documentText.append(text + " ");
+
+		aJCas.setDocumentText(documentText.toString().trim());
+	}
+
+	@Override
+	public boolean hasNext() {
+
+		if (reader == null) {
+			Resource nextFile = getNextResource();
+			if (nextFile == null) {
+				return false;
+			}
+			initReader(nextFile);
+		}
+
+		nextDocument = read();
+
+		if (nextDocument == null) {
+			close();
+			reader = null;
+			return hasNext();
+		}
+
+		return true;
+	}
+
+	private Resource getNextResource() {
+		Resource next = null;
+		try {
+			next = nextFile();
+		} catch (Exception e) {
+			// catch silently
+		}
+		return next;
+	}
+
+	private String read() {
+		try {
+			return reader.readLine();
+		} catch (IOException e) {
+			throw new UnsupportedOperationException(e);
+		}
+	}
+
+	private void initReader(Resource nextFile) {
+		try {
+			reader = new BufferedReader(new InputStreamReader(nextFile.getInputStream(), "utf-8"));
+		} catch (Exception e) {
+			throw new UnsupportedOperationException(e);
+		}
+	}
+
+	@Override
+	public void close() {
+		try {
+			reader.close();
+			super.close();
+		} catch (Exception e) {
+			throw new UnsupportedOperationException(e);
+		}
+	}
+
+}
---
+++ dkpro-tc-io/src/test/java/org/dkpro/tc/io/LinewiseTextOutcomeReaderTest.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright 2018
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.dkpro.tc.io;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.dkpro.tc.api.type.TextClassificationOutcome;
+import org.dkpro.tc.api.type.TextClassificationTarget;
+import org.junit.Test;
+
+public class LinewiseTextOutcomeReaderTest {
+
+	@Test
+	public void testReader() throws Exception {
+
+		CollectionReader reader = CollectionReaderFactory.createReader(LinwiseTextOutcomeReader.class,
+				LinwiseTextOutcomeReader.PARAM_TEXT_INDEX, 2, LinwiseTextOutcomeReader.PARAM_OUTCOME_INDEX, 1,
+				LinwiseTextOutcomeReader.PARAM_SOURCE_LOCATION, "src/test/resources/semEval2017Task4/",
+				LinwiseTextOutcomeReader.PARAM_PATTERNS, "*.txt");
+
+		List<String> readDocumentSpans = new ArrayList<>();
+		List<String> readOutcomes = new ArrayList<>();
+
+		while (reader.hasNext()) {
+			JCas emptyCas = JCasFactory.createJCas();
+			reader.getNext(emptyCas.getCas());
+
+			readDocumentSpans.add(JCasUtil.selectSingle(emptyCas, TextClassificationTarget.class).getCoveredText());
+			readOutcomes.add(JCasUtil.selectSingle(emptyCas, TextClassificationOutcome.class).getOutcome());
+		}
+
+		assertEquals(15, readDocumentSpans.size());
+		assertEquals(15, readOutcomes.size());
+	}
+
+}
