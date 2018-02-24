e207e3489a44ad7c0e0eb212f102eb4b3d74103f
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-02-24 21:40:27 +0100

    #444 - Reader for typical machine learning data formats

--- dkpro-tc-io/src/test/java/org/dkpro/tc/io/SequenceOutcomeReaderTest.java
+++ dkpro-tc-io/src/test/java/org/dkpro/tc/io/SequenceOutcomeReaderTest.java
@@ -17,7 +17,7 @@
  ******************************************************************************/
 package org.dkpro.tc.io;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Collection;
