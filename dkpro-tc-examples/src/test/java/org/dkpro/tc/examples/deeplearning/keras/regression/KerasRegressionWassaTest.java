c394194e5235d328e26941cc6ca17622670488e1
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-03-10 13:08:09 +0100

    #459 - Add Test-cases that check the created id2outcome file

--- dkpro-tc-examples/src/test/java/org/dkpro/tc/examples/deeplearning/keras/regression/KerasRegressionWassaTest.java
+++ dkpro-tc-examples/src/test/java/org/dkpro/tc/examples/deeplearning/keras/regression/KerasRegressionWassaTest.java
@@ -18,17 +18,17 @@
  */
 package org.dkpro.tc.examples.deeplearning.keras.regression;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 import org.dkpro.lab.task.ParameterSpace;
 import org.dkpro.tc.examples.deeplearning.PythonLocator;
 import org.dkpro.tc.examples.util.ContextMemoryReport;
 import org.dkpro.tc.examples.util.DemoUtils;
-import org.dkpro.tc.ml.report.util.Tc2LtlabEvalConverter;
 import org.junit.Test;
-
-import de.unidue.ltl.evaluation.core.EvaluationData;
-import de.unidue.ltl.evaluation.measures.correlation.PearsonCorrelation;
 
 public class KerasRegressionWassaTest extends PythonLocator {
 	@Test
@@ -48,11 +48,24 @@ public class KerasRegressionWassaTest extends PythonLocator {
 		if (testConditon) {
 			ParameterSpace ps = KerasRegressionWassa.getParameterSpace(python3);
 			KerasRegressionWassa.runTrainTest(ps);
-
-			EvaluationData<Double> data = Tc2LtlabEvalConverter.convertRegressionModeId2Outcome(ContextMemoryReport.id2outcomeFiles.get(0));
-			PearsonCorrelation p = new PearsonCorrelation(data);
 			
-			assertTrue(p.getResult() < 0.0);
+			assertEquals(1, ContextMemoryReport.id2outcomeFiles.size());
+
+			List<String> lines = FileUtils.readLines(ContextMemoryReport.id2outcomeFiles.get(0), "utf-8");
+			assertEquals(11, lines.size());
+
+			// line-wise compare
+			assertEquals("#ID=PREDICTION;GOLDSTANDARD;THRESHOLD", lines.get(0));
+			assertEquals("#labels", lines.get(1));
+			assertTrue(lines.get(3).matches("0=[0-9\\.]+;0.479;-1"));
+			assertTrue(lines.get(4).matches("1=[0-9\\.]+;0.458;-1"));
+			assertTrue(lines.get(5).matches("10=[0-9\\.]+;0.646;-1"));
+			assertTrue(lines.get(6).matches("11=[0-9\\.]+;0.726;-1"));
+			assertTrue(lines.get(7).matches("12=[0-9\\.]+;0.348;-1"));
+			assertTrue(lines.get(8).matches("13=[0-9\\.]+;0.417;-1"));
+			assertTrue(lines.get(9).matches("14=[0-9\\.]+;0.202;-1"));
+			assertTrue(lines.get(10).matches("15=[0-9\\.]+;0.557;-1"));
+
 		}
 	}
 }
