51c2c829a7462ac824358afc08bbf6607fea61b9
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-04-08 11:58:50 +0200

    #471 - Liblinear: Separate MLA code from DKPro Lab

--- dkpro-tc-ml-liblinear/src/main/java/org/dkpro/tc/ml/liblinear/core/LiblinearTrainer.java
+++ dkpro-tc-ml-liblinear/src/main/java/org/dkpro/tc/ml/liblinear/core/LiblinearTrainer.java
@@ -19,6 +19,7 @@ package org.dkpro.tc.ml.liblinear.core;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import de.bwaldvogel.liblinear.InvalidInputDataException;
 import de.bwaldvogel.liblinear.Linear;
@@ -29,6 +30,58 @@ import de.bwaldvogel.liblinear.SolverType;
 
 public class LiblinearTrainer
 {
+
+    public Model train(File data, File model, List<String> parameters)
+        throws IOException, InvalidInputDataException
+    {
+        SolverType s = getSolverFromParameters(parameters);
+        double c = getCvalue(parameters);
+        double eps = getEps(s, parameters);
+
+        return train(s, c, eps, data, model);
+    }
+
+    private double getEps(SolverType s, List<String> parameters)
+    {
+        for (int i = 0; i < parameters.size(); i += 2) {
+
+            if (parameters.get(i).equals("-e")) {
+                return Double.parseDouble(parameters.get(i + 1));
+            }
+
+        }
+
+        if (s.getId() == 1 || s.getId() == 3 || s.getId() == 4 || s.getId() == 7) {
+            return 0.1;
+        }
+
+        return 0.01;
+    }
+
+    private double getCvalue(List<String> parameters)
+    {
+        for (int i = 0; i < parameters.size(); i += 2) {
+
+            if (parameters.get(i).equals("-c")) {
+                return Double.parseDouble(parameters.get(i + 1));
+            }
+
+        }
+        return 1.0;
+    }
+
+    private SolverType getSolverFromParameters(List<String> parameters)
+    {
+        for (int i = 0; i < parameters.size(); i += 2) {
+
+            if (parameters.get(i).equals("-s")) {
+                return SolverType.getById(Integer.parseInt(parameters.get(i + 1)));
+            }
+
+        }
+
+        return SolverType.getById(1);
+    }
 
     public Model train(SolverType solver, double c, double eps, File data, File model)
         throws IOException, InvalidInputDataException
