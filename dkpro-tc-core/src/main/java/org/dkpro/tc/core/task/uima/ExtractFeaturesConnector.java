12de332ee639798ccb287abf3a9016c7df02926c
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-09-21 23:55:49 +0200

    #499 - Speed up feature name collection by using a mock CAS

--- dkpro-tc-core/src/main/java/org/dkpro/tc/core/task/uima/ExtractFeaturesConnector.java
+++ dkpro-tc-core/src/main/java/org/dkpro/tc/core/task/uima/ExtractFeaturesConnector.java
@@ -255,7 +255,7 @@ public class ExtractFeaturesConnector
 		
 		DocumentMetaData dmd = new DocumentMetaData(mockCas);
 		dmd.setLanguage(aDocMeta.getLanguage());
-		dmd.setDocumentId(aDocMeta.getDocumentId());
+		dmd.setDocumentId(System.currentTimeMillis() + "");
 		dmd.addToIndexes();
 		
 		//Create two views for Pair Mode
