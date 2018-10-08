9fa258f3793574aa88f7e300c50ba6913de8aa98
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-10-08 12:35:27 +0200

    #516 - Add a learning curve experiment

--- dkpro-tc-ml/src/main/java/org/dkpro/tc/ml/ExperimentLearningCurve.java
+++ dkpro-tc-ml/src/main/java/org/dkpro/tc/ml/ExperimentLearningCurve.java
@@ -286,7 +286,7 @@ public class ExperimentLearningCurve
         reports.add(new BasicResultReport());
 
         testTask = new DKProTcShallowTestTask(extractFeaturesTrainTask, extractFeaturesTestTask,
-                collectionTask, new ArrayList<>(), experimentName);
+                collectionTask, reports, experimentName);
         testTask.setType(testTask.getType() + "-" + experimentName);
         testTask.setAttribute(TC_TASK_TYPE, TcTaskType.FACADE_TASK.toString());
 
