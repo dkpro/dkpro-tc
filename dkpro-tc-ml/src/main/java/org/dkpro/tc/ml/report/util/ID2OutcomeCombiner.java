a89be9ee8d471ff287b764008f194b4214830195
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-11-27 18:39:18 +0100

    #524 - CV id2outcome labels differs to the one in train test

--- dkpro-tc-ml/src/main/java/org/dkpro/tc/ml/report/util/ID2OutcomeCombiner.java
+++ dkpro-tc-ml/src/main/java/org/dkpro/tc/ml/report/util/ID2OutcomeCombiner.java
@@ -24,11 +24,12 @@ import java.io.InputStreamReader;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.Set;
 
 import org.dkpro.tc.core.Constants;
@@ -45,6 +46,11 @@ public class ID2OutcomeCombiner<T>
 
     Set<String> uniqueNames = new HashSet<>();
     Map<String, String> multilabelIndexMapping;
+    
+    // the start index for the mapping of the combined file. This is zero for most classifiers but
+    // some use zero as reserved value for internal usage. To be consistent to the actual input file
+    // where the lowest index might be > 0, we track the lowest value we see explicitly.
+    int startIdx=0;
     
     static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
@@ -90,47 +96,48 @@ public class ID2OutcomeCombiner<T>
     private void processRegression(File id2OutcomeFile) throws Exception
     {
 
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(new FileInputStream(id2OutcomeFile), "utf-8"));
-        reader.readLine(); // pop first line
-        reader.readLine(); // mapping - does not exist in regression
-        reader.readLine(); // time stamp
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(id2OutcomeFile), "utf-8"))) {
+            reader.readLine(); // pop first line
+            reader.readLine(); // mapping - does not exist in regression
+            reader.readLine(); // time stamp
 
-        /*
-         * Careful with skippig lines starting with a '#' - this might be actual information content
-         */
-        
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            if (line.isEmpty()) {
-                continue;
+            /*
+             * Careful with skippig lines starting with a '#' - this might be actual information
+             * content
+             */
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                String[] split = splitAtEqualFromRight(line);
+                String docName = split[0];
+                String values = split[1];
+
+                String[] valSplit = values.split(";");
+                String p = valSplit[0];
+                String g = valSplit[1];
+                String threshold = valSplit[2];
+
+                uniqueNames.add(p);
+                uniqueNames.add(g);
+
+                names.add(docName);
+                thresholds.add(Double.valueOf(threshold));
+
+                List<T> pl = new ArrayList<>();
+                pl.add((T) p);
+                prediction.add(pl);
+
+                List<T> gl = new ArrayList<>();
+                gl.add((T) g);
+                gold.add(gl);
+
             }
-
-            String[] split = splitAtEqualFromRight(line);
-            String docName = split[0];
-            String values = split[1];
-
-            String[] valSplit = values.split(";");
-            String p = valSplit[0];
-            String g = valSplit[1];
-            String threshold = valSplit[2];
-
-            uniqueNames.add(p);
-            uniqueNames.add(g);
-
-            names.add(docName);
-            thresholds.add(Double.valueOf(threshold));
-
-            List<T> pl = new ArrayList<>();
-            pl.add((T) p);
-            prediction.add(pl);
-
-            List<T> gl = new ArrayList<>();
-            gl.add((T) g);
-            gold.add(gl);
-
         }
-        reader.close();
     }
 
     @SuppressWarnings("unchecked")
@@ -143,52 +150,63 @@ public class ID2OutcomeCombiner<T>
         // e.g. label id 3 might be C in the first fold but become 2 in the
         // second one because there is one label less
 
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(new FileInputStream(id2OutcomeFile), "utf-8"));
-        reader.readLine(); // pop first line with header
-        String header = reader.readLine();
-        Map<String, String> map = buildMappingFromHeader(header);
-        reader.readLine(); // time stamp
-
-        /*
-         * Careful with skippig lines starting with a '#' - this might be actual information content
-         */
-        
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            if (line.isEmpty()) {
-                continue;
-            }
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(id2OutcomeFile), "utf-8"))) {
+            reader.readLine(); // pop first line with header
+            String header = reader.readLine();
+            Map<String, String> map = buildMappingFromHeader(header);
+            reader.readLine(); // time stamp
             
-            String[] split = splitAtEqualFromRight(line);
-            String docName = split[0];
-            String values = split[1];
+            updateSmallestIdValueForOutputMapping(map);
 
-            String[] valSplit = values.split(";");
-            String p = map.get(valSplit[0]);
-            String g = map.get(valSplit[1]);
-            String threshold = valSplit[2];
+            /*
+             * Careful with skippig lines starting with a '#' - this might be actual information
+             * content
+             */
 
-            uniqueNames.add(p);
-            uniqueNames.add(g);
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()) {
+                    continue;
+                }
 
-            names.add(docName);
-            thresholds.add(Double.valueOf(threshold));
+                String[] split = splitAtEqualFromRight(line);
+                String docName = split[0];
+                String values = split[1];
 
-            List<T> pl = new ArrayList<>();
-            pl.add((T) p);
-            prediction.add(pl);
+                String[] valSplit = values.split(";");
+                String p = map.get(valSplit[0]);
+                String g = map.get(valSplit[1]);
+                String threshold = valSplit[2];
 
-            List<T> gl = new ArrayList<>();
-            gl.add((T) g);
-            gold.add(gl);
+                uniqueNames.add(p);
+                uniqueNames.add(g);
 
+                names.add(docName);
+                thresholds.add(Double.valueOf(threshold));
+
+                List<T> pl = new ArrayList<>();
+                pl.add((T) p);
+                prediction.add(pl);
+
+                List<T> gl = new ArrayList<>();
+                gl.add((T) g);
+                gold.add(gl);
+
+            }
         }
-        reader.close();
     }
 
 
-	private String[] splitAtEqualFromRight(String line) {
+	private void updateSmallestIdValueForOutputMapping(Map<String, String> map)
+    {
+	    Optional<Integer> minimal = map.keySet().stream().map(Integer::valueOf).reduce(Integer::min);
+        if(minimal.isPresent() && minimal.get() > startIdx) {
+            startIdx = minimal.get();
+        }
+    }
+
+    private String[] splitAtEqualFromRight(String line) {
     	
     	int indexOf = line.lastIndexOf("=");
     	
@@ -202,51 +220,53 @@ public class ID2OutcomeCombiner<T>
 		return new String [] {left, right};
 	}
 
-	@SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     private void processMultilabel(File id2OutcomeFile) throws Exception
     {
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(new FileInputStream(id2OutcomeFile), "utf-8"));
-        reader.readLine(); // pop first line
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(id2OutcomeFile), "utf-8"))) {
+            reader.readLine(); // pop first line
 
-        Map<String, String> map = buildMappingFromHeader(reader.readLine());
-        multilabelIndexMapping.putAll(map);
-        
-        reader.readLine(); // time stamp
+            Map<String, String> map = buildMappingFromHeader(reader.readLine());
+            multilabelIndexMapping.putAll(map);
+            
+            updateSmallestIdValueForOutputMapping(map);
 
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            if (line.isEmpty()) {
-                continue;
+            reader.readLine(); // time stamp
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                String[] split = splitAtEqualFromRight(line);
+                String docName = split[0];
+                String values = split[1];
+
+                String[] valSplit = values.split(";");
+                String[] p = valSplit[0].split(",");
+                String[] g = valSplit[1].split(",");
+                String threshold = valSplit[2];
+
+                names.add(docName);
+                thresholds.add(Double.valueOf(threshold));
+
+                List<T> pl = new ArrayList<>();
+                for (String x : p) {
+                    pl.add((T) x);
+                }
+                prediction.add(pl);
+
+                List<T> gl = new ArrayList<>();
+                for (String x : g) {
+                    gl.add((T) x);
+                }
+                gold.add(gl);
+
             }
-
-            String[] split = splitAtEqualFromRight(line);
-            String docName = split[0];
-            String values = split[1];
-
-            String[] valSplit = values.split(";");
-            String[] p = valSplit[0].split(",");
-            String[] g = valSplit[1].split(",");
-            String threshold = valSplit[2];
-
-            names.add(docName);
-            thresholds.add(Double.valueOf(threshold));
-
-            List<T> pl = new ArrayList<>();
-            for (String x : p) {
-                pl.add((T) x);
-            }
-            prediction.add(pl);
-
-            List<T> gl = new ArrayList<>();
-            for (String x : g) {
-                gl.add((T) x);
-            }
-            gold.add(gl);
 
         }
-
-        reader.close();
     }
 
     private static Map<String, String> buildMappingFromHeader(String header)
@@ -271,9 +291,10 @@ public class ID2OutcomeCombiner<T>
     {
 
         Map<String, Integer> map = new HashMap<>();
-        int id = 0;
-
-        for (String l : uniqueNames) {
+        int id = startIdx;
+        List<String> sortedUniqueNames = new ArrayList<>(uniqueNames);
+        Collections.sort(sortedUniqueNames);
+        for (String l : sortedUniqueNames) {
             map.put(l, id++);
         }
 
@@ -349,9 +370,12 @@ public class ID2OutcomeCombiner<T>
 
         StringBuilder sb = new StringBuilder();
         sb.append("#labels ");
+        
+        List<String> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
 
-        for (Entry<String, Integer> e : map.entrySet()) {
-            sb.append(e.getValue() + "=" + e.getKey() + " ");
+        for (String key : keys) {
+            sb.append(map.get(key) + "=" + key + " ");
         }
 
         return sb.toString().trim();
