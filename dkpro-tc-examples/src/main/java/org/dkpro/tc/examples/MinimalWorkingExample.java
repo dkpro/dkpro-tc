8a4e94a6bdcf704564581a7ad80ce95e62b61145
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-12-02 13:08:38 +0100

    #531 - Drop dkpro-tc-simple module

--- dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/MinimalWorkingExample.java
+++ dkpro-tc-examples/src/main/java/org/dkpro/tc/examples/MinimalWorkingExample.java
@@ -18,20 +18,22 @@ package org.dkpro.tc.examples;
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.resource.ResourceInitializationException;
 import org.dkpro.tc.api.features.TcFeatureFactory;
-import org.dkpro.tc.api.features.TcFeatureSet;
 import org.dkpro.tc.core.Constants;
 import org.dkpro.tc.examples.util.DemoUtils;
-import org.dkpro.tc.features.maxnormalization.TokenRatioPerDocument;
 import org.dkpro.tc.features.ngram.WordNGram;
 import org.dkpro.tc.io.FolderwiseDataReader;
 import org.dkpro.tc.ml.builder.FeatureMode;
 import org.dkpro.tc.ml.builder.LearningMode;
+import org.dkpro.tc.ml.builder.MLBackend;
+import org.dkpro.tc.ml.experiment.builder.ExperimentBuilder;
+import org.dkpro.tc.ml.experiment.builder.ExperimentType;
 import org.dkpro.tc.ml.libsvm.LibsvmAdapter;
-import org.dkpro.tc.simple.builder.TcTrainTestExperiment;
 
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 
@@ -45,7 +47,7 @@ public class MinimalWorkingExample
 
     public static void main(String[] args) throws Exception
     {
-    	System.setProperty("java.util.logging.config.file", "logging.properties");
+        System.setProperty("java.util.logging.config.file", "logging.properties");
         DemoUtils.setDkproHome("target/");
         runExperiment();
     }
@@ -63,20 +65,22 @@ public class MinimalWorkingExample
                 corpusFilePathTest, FolderwiseDataReader.PARAM_LANGUAGE, LANGUAGE_CODE,
                 FolderwiseDataReader.PARAM_PATTERNS, "*/*.txt");
 
-        
-        TcTrainTestExperiment exp = new TcTrainTestExperiment(readerTrain, 
-                                                              readerTest,
-                                                              LearningMode.SINGLE_LABEL,
-                                                              FeatureMode.DOCUMENT, 
-                                                              new LibsvmAdapter(),
-                                                              new TcFeatureSet(TcFeatureFactory.create(TokenRatioPerDocument.class),
-                                                              TcFeatureFactory.create(WordNGram.class, 
-                                                                                      WordNGram.PARAM_NGRAM_USE_TOP_K, 20, 
-                                                                                      WordNGram.PARAM_NGRAM_MIN_N, 1, 
-                                                                                      WordNGram.PARAM_NGRAM_MAX_N, 3)),
-                                                              AnalysisEngineFactory.createEngineDescription(BreakIteratorSegmenter.class)
-                                                              );
-        exp.run();
-        
+        ExperimentBuilder builder = new ExperimentBuilder();
+        builder.experiment(ExperimentType.TRAIN_TEST, "trainTest")
+                .dataReaderTrain(readerTrain)
+                .dataReaderTest(readerTest)
+                .preprocessing(getPreprocessing())
+                .features(TcFeatureFactory.create(WordNGram.class))
+                .learningMode(LearningMode.SINGLE_LABEL)
+                .featureMode(FeatureMode.DOCUMENT)
+                .machineLearningBackend(
+                        new MLBackend(new LibsvmAdapter())
+                        )
+                .run();
+    }
+
+    private static AnalysisEngineDescription getPreprocessing() throws ResourceInitializationException
+    {
+        return AnalysisEngineFactory.createEngineDescription(BreakIteratorSegmenter.class);
     }
 }
